b5b2925d9dc5d6768a1a7f65288cbcb41bf92d2e
amirrezaask <raskarpour@gmail.com>
2019-05-17 12:10:54 +0430

    Powershell Profile Added

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+﻿Import-Module posh-git
+function Prompt { 
+     $prompt = Write-Prompt ''
+     $prompt += & $GitPromptScriptBlock
+     return $prompt
+ }
+
+function paygearPythonProj() {
+     Set-Location -Path $("D:\sources\paygear")
+}
+
+function paygearGoProj () {
+     Set-Location -Path $($env:GOPATH + "\src\git.raad.cloud\cloud")
+}
+function amirrezaGoProj() {
+     Set-Location -Path $($env:GOPATH + "\src\github.com\amirrezaask")
+}
+function vimRunner {
+	wsl -e vim (wsl wslpath "'$args'")
+}
+
+
+
+Set-Alias -Name ppp -Value paygearPythonProj
+Set-Alias -Name pgp -Value paygearGoProj
+Set-Alias -Name vim -Value vimRunner
+Set-Alias -Name agp -Value amirrezaGoProj
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+
+
