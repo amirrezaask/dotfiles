cc92e191ce78f573fe41d03033026407b2f9d62a
amirrezaask <raskarpour@gmail.com>
2020-02-28 04:58:44 +0330

    add emacs config to this repo as well

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,8 @@
 ;; Author Amirrezaask <raskarpour@gmail.com>
-;; Alwaus remember effient over fancy
+;; Always remember effient over fancy
 
 ;; (setq debug-on-error 1)
+(setq start (float-time)) ;; to measure emacs startup time
 ;; initial setup
 (require 'package)
 (setq custom-file "~/.__custom.el")
@@ -13,8 +14,6 @@
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
 (global-linum-mode t)
-
-
 (package-initialize)
 (setq package-archives '(("org"       . "http://orgmode.org/elpa/")
                          ("gnu"       . "http://elpa.gnu.org/packages/")
@@ -27,30 +26,51 @@
   (package-install 'use-package))
 
 (require 'use-package) 
-(setq use-package-verbose t)
-(setq use-package-minimum-reported-time 0.000001)
+;; (setq use-package-verbose t)
+;; (setq use-package-minimum-reported-time 0.000001)
 ;; initial setup done ;)
 
 
+;; UI stuff
+
+(defconst lisp--prettify-symbols-alist
+  '(("lambda"  . ?λ)))
+(global-prettify-symbols-mode 1)
+
+
+;; Font Setup
 (setq font "Jetbrains Mono")
 (setq font-size 11)
 (add-to-list 'default-frame-alist (cons 'font (format "%s-%d" font font-size)))
 (set-face-attribute 'default nil
 		    :family font
 		    :height (* 10 font-size))
-
-;; UI stuff
+;; Font ends here
+;; Themes
 (use-package dracula-theme :ensure t :defer t)
+(use-package solarized-theme :ensure t :defer t)
 (use-package spacemacs-theme :ensure t :defer t)
 (use-package doom-themes :ensure t :defer t)
-(load-theme 'doom-one t)
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-center-content t))
+;; Themes end here
+(defun light-it-up ()
+  (interactive)
+  (load-theme 'solarized-light t))
 
+(load-theme 'solarized-dark t)
+;; (use-package dashboard
+;;   :ensure t
+;;   :config
+;;   (dashboard-setup-startup-hook)
+;;   (setq dashboard-startup-banner 'logo)
+;;   (setq dashboard-center-content t)
+;;   (setq dashboard-banner-logo-title "Welcome To GNU Emacs")
+;;   (setq dashboard-items '(
+;; 			  (projects . 5)
+;; 			  (bookmarks . 5)
+;; 			  (agenda . 5)
+;; 			  (registers . 5))))
+(use-package emojify :ensure t :config (emojify-mode 1))
+;; (use-package spaceline :ensure t :config (spaceline-spacemacs-theme))
 ;; (use-package doom-modeline :ensure t :config (doom-modeline-mode 1))
 ;; UI stuff ends here
 
@@ -61,7 +81,7 @@
   (setq ido-everywhere t)
   (ido-mode t)
   (ido-vertical-mode t))
-(use-package swiper :ensure t :commands swiper)
+(use-package swiper :ensure t :defer t :commands swiper)
 (use-package counsel :ensure t :commands counsel-M-x)
 ;; Completion framework ends here
 
@@ -69,22 +89,32 @@
 (use-package org :ensure t :defer t)
 (use-package org-bullets :ensure t :hook org-mode :config (lambda () (org-bullets-mode 1)))
 ;; Org ends here
+
 ;; editor
 (use-package yaml-mode :ensure t :mode "\\.ya?ml\\'")
 (use-package json-mode :ensure t :mode "\\.json\\'"
   :config
   (add-hook 'before-save-hook 'json-mode-beautify))
-(use-package evil :ensure t :config (evil-mode t))
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-search-module 'swiper)
+  :config
+  (evil-mode t))
+
 (use-package which-key :ensure t :config (which-key-mode 1))
 (use-package markdown-mode :ensure t :mode "\\.md\\'")
 ;; editor ends here
 
 ;; IDE
+;; (use-package dap-mode :ensure t :hook ((go-mode python-mode php-mode) . dap-mode))
 (use-package flycheck :ensure t :hook ((python-mode go-mode php-mode emacs-lisp-mode) . flycheck-mode))
+;; (use-package neotree :ensure t)
 (use-package magit :ensure t :defer t)
-(use-package projectile :ensure t)
+(use-package forge :ensure t :defer t :after magit)
+;; (use-package projectile :ensure t)
 (use-package lsp-mode :ensure t :defer t)
-(use-package lsp-ui :ensure t)
+(use-package lsp-ui :ensure t :defer t)
 (use-package company-lsp :ensure t :defer t)
 (use-package company
   :ensure t
@@ -94,10 +124,7 @@
   (setq company-echo-delay 0)
   (global-company-mode))
 
-;; IDE ends here
-
-
-
+;; IDE end
 ;; python setup
 (use-package python-mode
   :ensure t
@@ -108,14 +135,14 @@
   (lsp))
 ;; (use-package elpy :ensure t :init (elpy-enable)) ;; a little too much i think
 
-(use-package py-autopep8 :ensure t :hook python-mode)
+(use-package py-autopep8 :ensure t :defer t :hook python-mode)
 ;; python setup ends here
 
 
 ;; Lisp setup
 (use-package paredit :ensure t :hook (emacs-lisp-mode . paredit-mode))
 (use-package parinfer :ensure t :hook (emacs-lisp-mode . parinfer-mode))
-(use-package rainbow-delimiters :ensure :hook (emacs-lisp-mode . rainbow-delimiters-mode))
+(use-package rainbow-delimiters :ensure :hook ((emacs-lisp-mode python-mode go-mode php-mode) . rainbow-delimiters-mode))
 ;; Lisp ends here
 
 
@@ -123,7 +150,6 @@
 (use-package php-mode :ensure t :defer :init (add-hook 'php-mode-hook #'lsp))
 (use-package js2-mode :ensure t :hook js-mode)
 ;; not important languages :) ends here
-
 
 
 
@@ -153,7 +179,6 @@
 			    ))
   :config
   (add-to-list 'exec-path (concat (go-path) "/bin")))
-
 (use-package go-add-tags :ensure t)
 (use-package go-stacktracer :ensure t)
 (use-package go-eldoc :ensure t)
@@ -171,21 +196,53 @@
    ".." 'xref-find-definitions
    "/" 'undo-tree-undo
    "xx" 'counsel-M-x
-   "bl" 'switch-to-buffer
    "SPC" 'find-file
    "ff" 'find-file
+   ;; line actions 
    "ls" #'move-beginning-of-line
    "le" #'move-end-of-line
+   "lc" 'comment-line
+   ;; line actions ends here
+   ;; buffer actions
+   "bl" 'switch-to-buffer
    "bs" 'save-buffer
+   "bk" 'kill-buffer
+   "bn" #'buffer-next
+   "bp" #'buffer-previous
+   "be" 'eval-buffer
+   ;; buffer actions ends here
+   ;; window actions
+   "wk" 'evil-window-top
+   "wj" 'evil-window-down
+   "wh" 'evil-window-left
+   "wl" 'evil-window-right
    "wo" 'other-window
    "wc" 'delete-window
-   "sv" 'split-window-vertically
    "sh" 'split-window-horizontally
-   "bk" 'kill-buffer
+   "sv" 'split-window-vertically
+   ;; windows actions ends here
+   ;; Project actions
+   "pf" 'counsel-git
+   ;; Project ends here
    "ss" 'swiper
    "ee" 'eval-last-sexp
-   "eb" 'eval-buffer
-   "cl" 'comment-line
    "dk" 'describe-key
    "df" 'describe-function))
+;; keybindings ends here
+
+;; IRC setup
+;; (erc-autojoin-mode)
+;; (defun irc ()
+;;   (interactive)
+;;   (erc :server "irc.freenode.net" :nick "amirrezaask" :password nil))
+;; IRC ends here
+
+
+;; Twitter setup
+(use-package twittering-mode :ensure t :defer t :config (setq twittering-icon-mode t) :commands twittering-mode)
+;; Twitter ends here
+
+
 ;; init.el ends here
+(message "Startup Time %f" (- (float-time) start))
+(find-file "~/.TODO.org")
