09cc567b68acd3a9b954cb5c9a416f65fd96db44
amirrezaask <raskarpour@gmail.com>
2020-02-28 03:41:43 +0330

    make zshrc link and seperate vars

---
+++ zshrc/ashell.sh
@@ -0,0 +1,10 @@
+# Contains all my general shell vars and configs
+
+export GOPATH=/home/amirreza/go
+export PYTHONPATH=/home/amirreza/.local/bin
+export PATH=$HOME/.config/composer/vendor/bin:/usr/local/go/bin:$GOPATH/bin:$PYTHONPATH:/usr/local/Postman:$PATH
+alias agp="cd $GOPATH/src/github.com/amirrezaask"
+alias pgp="cd $GOPATH/src/git.raad.cloud/cloud"
+alias ppp="cd ~/src/paygear"
+alias pinstall="sudo pacman -S"
+alias psearch="sudo pacman -Ss"
