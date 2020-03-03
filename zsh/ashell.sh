a633059f1637dd1a529ca019d3a2b68513bbe83a
amirrezaask <raskarpour@gmail.com>
2020-03-03 09:47:27 +0330

    add rust support

--- zsh/ashell.sh
+++ zsh/ashell.sh
@@ -2,7 +2,7 @@
 
 export GOPATH=/home/amirreza/go
 export PYTHONPATH=/home/amirreza/.local/bin
-export PATH=$HOME/.config/composer/vendor/bin:/usr/local/go/bin:$GOPATH/bin:$PYTHONPATH:/usr/local/Postman:$PATH
+export PATH=$HOME/.config/composer/vendor/bin:/usr/local/go/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONPATH:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH
 alias agp="cd $GOPATH/src/github.com/amirrezaask"
 alias pgp="cd $GOPATH/src/git.raad.cloud/cloud"
 alias ppp="cd ~/src/paygear"
