e9a98f1ffb07a99a02329279782588229ccdcc35
amirrezaask <raskarpour@gmail.com>
2020-03-03 19:56:02 +0330

    add indent guide emacs, some i3 updates

--- zsh/ashell.sh
+++ zsh/ashell.sh
@@ -1,5 +1,7 @@
 # Contains all my general shell vars and configs
-
+reload_ashell() {
+	source ~/.ashell
+}
 export GOPATH=/home/amirreza/go
 export PYTHONPATH=/home/amirreza/.local/bin
 export PATH=$HOME/.config/composer/vendor/bin:/usr/local/go/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONPATH:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH
@@ -9,3 +11,5 @@ alias ppp="cd ~/src/paygear"
 alias pinstall="sudo pacman -S"
 alias psearch="sudo pacman -Ss"
 alias dots="cd ~/projects/mine/dotfiles"
+alias vim=nvim
+alias ocdp="oc delete pod"
