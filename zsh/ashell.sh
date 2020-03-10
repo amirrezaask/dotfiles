3fe70823b1e43508f40f854d2758b6d77e2b2f28
amirrezaask <raskarpour@gmail.com>
2020-03-10 06:22:05 +0330

    moving to dark side

--- zsh/ashell.sh
+++ zsh/ashell.sh
@@ -17,4 +17,4 @@ alias ocdp="oc delete pod"
 # replace caps with escape
 # setxkbmap -option "caps:swapescape"
 # replace caps with ctrl
-setxkbmap -layout us,ir -option ctrl:nocaps 
+setxkbmap -layout us,ir -option caps:swapescape 
