f0a03b6b39d69aa6abaae357cb107fa4f5de1588
amirrezaask <raskarpour@gmail.com>
2020-03-15 13:39:19 +0330

    hard to believe but I'm not that uncomfortable with Emacs default keybindings

--- zsh/ashell.sh
+++ zsh/ashell.sh
@@ -15,6 +15,6 @@ alias dots="cd ~/projects/mine/dotfiles"
 alias vim=nvim
 alias ocdp="oc delete pod"
 # replace caps with escape
-# setxkbmap -option "caps:swapescape"
+# setxkbmap -option "caps:swapescape ctrl:nocaps"
 # replace caps with ctrl
 setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle" 
