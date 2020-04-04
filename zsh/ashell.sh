80afe4bc29326c2ce9703668a2d07539932c823c
amirrezaask <raskarpour@gmail.com>
2020-04-04 23:55:26 +0430

    GOROOT, fix zshrc

--- zsh/ashell.sh
+++ zsh/ashell.sh
@@ -4,6 +4,7 @@ reload_ashell() {
 	source ~/.ashell
 }
 export GOPATH=/home/amirreza/go
+export GOROOT=/usr/local/go
 export PYTHONPATH=/home/amirreza/.local/bin
 export PATH=$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONPATH:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH
 alias vim=nvim
@@ -12,5 +13,5 @@ export EDITOR=nvim
 # reset setxkbmap
 setxkbmap -option
 # Caps remapping
-setxkbmap -layout us,ir -option "caps:swapescape" -option "grp:alt_shift_toggle"
-# setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle"
+# setxkbmap -layout us,ir -option "caps:swapescape" -option "grp:alt_shift_toggle"
+setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle"
