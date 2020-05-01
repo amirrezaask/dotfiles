0143323be744a955eeba641e1435270d8ee1c296
amirrezaask <raskarpour@gmail.com>
2020-05-01 21:58:59 +0430

    add fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+set -x EMACSCONFIG '~/.emacs.d'
+set -x GO111MODULE 'on'
+set -x GOPATH '/home/amirreza/go'
+set -x GOROOT '/usr/local/go'
+set -x PYTHONPATH '/home/amirreza/.local/bin'
+set -x PATH "$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONPATH:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH"
+set -x EMACSTERMINAL "emacsclient -t"
+set -x EDITOR "$EMACSTERMINAL"
+set -x SHELL "/bin/zsh"
+
+alias vim "nvim"
+alias e "$EMACSTERMINAL"
+alias open "xdg-open"
+
+# reset setxkbmap
+setxkbmap -option
+
+setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle"
