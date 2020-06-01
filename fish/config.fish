c1d5d5adb77f240aa7bacaf0c14b60ed8c25ae60
amirrezaask <raskarpour@gmail.com>
2020-06-01 23:12:00 +0430

    update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,8 @@ set -x PYTHONPATH '/home/amirreza/.local/bin'
 set -x PATH "$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONPATH:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH"
 set -x EMACSTERMINAL "emacsclient -t"
 set -x EDITOR nvim 
-set -x SHELL "/bin/zsh"
+set -x GOPRIVATE "gitlab.snapp.ir"
+set -x GOPROXY "goproxy.io"
 
 alias e "$EMACSTERMINAL"
 alias open "xdg-open"
@@ -15,4 +16,7 @@ alias vi "nvim"
 alias g "git"
 alias gs "git status"
 # setxkbmap -layout us,ir -option "caps:swapescape" -option "grp:alt_shift_toggle"
-setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle"
+# setxkbmap -layout us,ir -option "ctrl:nocaps" -option "grp:alt_shift_toggle"
+
+
+omf theme batman
