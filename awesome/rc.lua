d459756dc2d5fcbdfa4d24c9e687ecb9604aad01
amirrezaask <raskarpour@gmail.com>
2020-08-30 19:36:50 +0430

    change Super+r to run dmenu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -329,7 +329,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },            "r",     function () awful.util.spawn("dmenu_run") end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
