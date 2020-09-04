dc0392b0d67dbe4e9b2506d6fb08da6e7f1adf72
amirrezaask <raskarpour@gmail.com>
2020-09-04 06:20:13 +0430

    more simplification

--- emacs/init.el
+++ emacs/init.el
@@ -23,6 +23,22 @@
 ;; 
 
 ;;; Code:
+
+;;;; Setup package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+
 (org-babel-load-file "~/.emacs.d/README.org")
 
 ;;(load-file "~/.emacs.d/README.el")
