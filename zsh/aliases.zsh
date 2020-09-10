f2cca24feeb7be549b935fe90c2b4bc30afae47d
amirrezaask <raskarpour@gmail.com>
2020-09-11 02:53:10 +0430

    add dotfiles editiing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+export DOTFILES="$HOME/w/dotfiles"
 export GO111MODULE='on'
 export GOPATH="$HOME/go"
 export GOROOT="/usr/local/go" 
@@ -9,7 +10,6 @@ export GOPRIVATE='gitlab.snapp.ir'
 export GOPROXY='goproxy.io'
 export PLAN9="$HOME/.local/plan9"
 export _JAVA_AWT_WM_NONREPARENTING=1
-
 export PATH="$PLAN9/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONBINS:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH"
 
 alias open='xdg-open'
@@ -17,5 +17,6 @@ alias snappvpn="sudo openfortivpn -c $HOME/snappDC.conf"
 alias v=nvim
 alias vim=nvim
 alias vi=nvim
-alias g=git status
+alias g='git status'
+alias vd="cd $DOTFILES && vim"
 setxkbmap -layout "us,ir" -option "grp:shifts_toggle" -option "ctrl:nocaps"
