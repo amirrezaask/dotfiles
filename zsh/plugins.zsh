7e568128cad65cddefe06ffd272afe9c84c3368c
amirrezaask <raskarpour@gmail.com>
2020-09-11 01:59:38 +0430

    better git prompt

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 PLUGINS="$HOME/w/dotfiles/zsh/plugins"
 # loading plugins
+source "$PLUGINS/git-prompt.zsh/git-prompt.zsh"
 source "$PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
