f6be4b1427f81f09395fa46f6cc4fcde4650268d
amirrezaask <raskarpour@gmail.com>
2020-09-13 02:38:31 +0430

    add xrandr script

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -14,6 +14,7 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
+local xrandr = require('xrandr')
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
