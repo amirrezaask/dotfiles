c6ebecadfb7865c187683d964c5fc81a6885d2b1
amirrezaask <raskarpour@gmail.com>
2020-10-01 01:28:40 +0330

    use antigen for simpler setup

--- zsh/README.md
+++
@@ -1,2 +0,0 @@
-# AmirrezaAsk ZSH Bundle
-My custom bundle for ZSH. Why use heavy oh-my-zsh ??
--- zsh/plugins.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-PLUGINS="$HOME/w/dotfiles/zsh/plugins"
-# loading plugins
-source "$PLUGINS/git-prompt.zsh/git-prompt.zsh"
-source "$PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-source "$PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
