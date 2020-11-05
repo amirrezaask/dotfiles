37b4bb9b5e82c1d94dba1556bc6e6a15b0fa4fb6
amirrezaask <raskarpour@gmail.com>
2020-11-05 09:34:31 +0330

    fix lsp keybindings

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -19,15 +19,15 @@ local function on_attach()
 end
 
 -- 
-vim.cmd [[nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>]]
-vim.cmd [[nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>]]
-vim.cmd [[nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>]]
-vim.cmd [[nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>]]
-vim.cmd [[nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>]]
-vim.cmd [[nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>]]
-vim.cmd [[nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>]]
-vim.cmd [[nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>]]
-vim.cmd [[nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>]]
+vim.cmd [[ nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR> ]]
+vim.cmd [[ nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR> ]]
+vim.cmd [[ nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR> ]]
+vim.cmd [[ nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR> ]]
+vim.cmd [[ nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR> ]]
+vim.cmd [[ nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR> ]] 
+vim.cmd [[ nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR> ]]
+vim.cmd [[ nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR> ]]
+vim.cmd [[ nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR> ]]
 
 vim.cmd [[autocmd BufEnter * lua require'lsp'.on_attach()]]
 
