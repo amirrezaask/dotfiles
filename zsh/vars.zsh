008d0c214ac0d03df8c89d11b5f05f023642d328
amirrezaask <raskarpour@gmail.com>
2021-03-17 12:55:54 +0330

    back to oh-my-zsh

---
+++ zsh/vars.zsh
@@ -0,0 +1,44 @@
+# Variables
+export GO111MODULE='on'
+export GOPATH="$HOME"
+export PYTHONBINS="$HOME/.local/bin"
+export EDITOR='nvim'
+export GOPRIVATE='devheroes.codes,gitlab.com,gitlab.espadev.ir'
+export PLAN9="$HOME/.local/plan9"
+export PATH="/usr/local/go/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONBINS:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH"
+
+# Aliases
+alias open='xdg-open'
+alias g='git status'
+alias ga='git add'
+alias gcl='git clone'
+alias gpl='git pull'
+alias gd='git diff'
+alias gc='git commit'
+alias gp='git push'
+alias gcm='git commit -m'
+alias gf='git fetch'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gpsup='gp --set-upstream origin $(git_current_branch)'
+alias kcl='kubectl'
+alias dots="cd ${DOTFILES}"
+alias prj="cd ${GOPATH}/src/github.com/amirrezaask"
+alias freenet="echo ${VPN_PASSWORD} | sudo openconnect --no-dtls --passwd-on-stdin -u ${VPN_USERNAME} ${VPN_SERVER}"
+alias lock='i3lock -c000000'
+alias tf='terraform'
+alias tg='terragrunt'
+alias reload='source ~/.zshrc'
+alias sik='pkill'
+alias gg='git push'
+alias ez='git commit -m'
+alias goland='~/jetbrains/GoLand-2020.3/bin/goland.sh 2>/dev/null  &'
+if type nvim > /dev/null 2>&1; then
+    alias vim='nvim'
+    alias vi='nvim'
+fi
+if type bat > /dev/null 2>&1; then
+    alias cat='bat'
+fi
+
+
