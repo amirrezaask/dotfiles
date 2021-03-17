c57a4b6fbda2a98316dd1660aaedba21717f1ab6
amirrezaask <raskarpour@gmail.com>
2021-03-17 12:55:34 +0330

    change default terminal for awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -50,7 +50,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
