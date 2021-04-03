b7c8aff379d25a5a52a2d64ee3111fcbbea8ab1e
amirrezaask <raskarpour@gmail.com>
2021-04-03 08:29:16 +0430

    add dap configuration for go

--- nvim/lua/dap_config.lua
+++ nvim/lua/dap_config.lua
@@ -1,10 +1,10 @@
 local dap = require'dap'
 
 dap.adapters.go = function(callback, config)
-    local handle
-    local pid_or_err
-    local port = 38697
-    handle, pid_or_err =
+  local port = 38697
+  local handle
+  local pid_or_err
+  handle, pid_or_err =
       vim.loop.spawn(
       "dlv",
       {
@@ -16,22 +16,27 @@ dap.adapters.go = function(callback, config)
         print("Delve exited with exit code: " .. code)
       end
     )
-     ----should we wait for delve to start???
-    --vim.defer_fn(
-    --function()
-      --dap.repl.open()
-      --callback({type = "server", host = "127.0.0.1", port = port})
-    --end,
-    --100)
-
-      callback({type = "server", host = "127.0.0.1", port = port})
-  end
-  -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-  dap.configurations.go = {
-    {
-      type = "go",
-      name = "Debug",
-      request = "launch",
-      program = "${file}"
-    }
+    vim.defer_fn(
+      function()
+        dap.repl.open()
+        callback({type = "server", host = "127.0.0.1", port = port})
+      end,
+    100)
+  callback({type = "server", host = "127.0.0.1", port = port})
+end
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}"
   }
+}
+
+vim.cmd [[nnoremap <silent> <F3> :lua require'dap'.toggle_breakpoint()<CR>]]
+vim.cmd [[nnoremap <silent> <F4> :lua require'dap'.repl.open()<CR>]]
+vim.cmd [[nnoremap <silent> <F5> :lua require'dap'.continue()<CR>]]
+vim.cmd [[nnoremap <silent> <F7> :lua require'dap'.step_into()<CR>]]
+vim.cmd [[nnoremap <silent> <F8> :lua require'dap'.step_over()<CR>]]
+vim.cmd [[nnoremap <silent> <F9> :lua require'dap'.step_out()<CR>]]
+vim.cmd [[nnoremap <silent> <F10> :lua require('dap.ui.variables').hover()<CR>]]
