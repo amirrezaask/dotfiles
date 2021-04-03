1a735bfea7191fe145aae956ec79a57ff9553c43
amirrezaask <raskarpour@gmail.com>
2021-04-03 09:02:00 +0430

    add lsp api as commands for simpler usage

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -9,16 +9,30 @@ for _, server in pairs(servers) do
 end
 
 vim.cmd [[ autocmd BufEnter * lua require'completion'.on_attach() ]]
--- Keybindings 
-vim.cmd [[ nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR> ]]
-vim.cmd [[ nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR> ]]
-vim.cmd [[ nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR> ]]
-vim.cmd [[ nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR> ]]
-vim.cmd [[ nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR> ]]
-vim.cmd [[ nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR> ]] 
-vim.cmd [[ nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR> ]]
-vim.cmd [[ nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR> ]]
-vim.cmd [[ nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR> ]]
+
+-- Commands
+vim.cmd [[ command! LspDef lua vim.lsp.buf.definition() ]]
+vim.cmd [[ command! LspHover lua vim.lsp.buf.hover() ]]
+vim.cmd [[ command! LspImpl lua vim.lsp.buf.implementation() ]]
+vim.cmd [[ command! LspSignatureHelp lua vim.lsp.buf.signature_help() ]]
+vim.cmd [[ command! LspTypeDef lua vim.lsp.buf.type_definition() ]]
+vim.cmd [[ command! LspRefs lua vim.lsp.buf.references() ]]
+vim.cmd [[ command! LspDocSyms lua vim.lsp.buf.document_symbol() ]]
+vim.cmd [[ command! LspWorkSyms lua vim.lsp.buf.workspace_symbol() ]]
+vim.cmd [[ command! LspDecl lua vim.lsp.buf.declaration() ]]
+vim.cmd [[ command! LspRename lua vim.lsp.buf.rename() ]]
+
+-- Keybindings
+vim.cmd [[ nnoremap <silent> gd    <cmd>LspDef<CR> ]]
+vim.cmd [[ nnoremap <silent> K     <cmd>LspHover<CR> ]]
+vim.cmd [[ nnoremap <silent> gI    <cmd>LspImpl<CR> ]]
+vim.cmd [[ nnoremap <silent> <c-k> <cmd>LspSignatureHelp<CR> ]]
+vim.cmd [[ nnoremap <silent> 1gD   <cmd>LspTypeDef<CR> ]]
+vim.cmd [[ nnoremap <silent> gR    <cmd>LspRefs<CR> ]]
+vim.cmd [[ nnoremap <silent> g0    <cmd>LspDocSyms<CR> ]]
+vim.cmd [[ nnoremap <silent> gW    <cmd>LspWorkSyms<CR> ]]
+vim.cmd [[ nnoremap <silent> gD    <cmd>LspDecl<CR> ]]
+vim.cmd [[ nnoremap <silent> r     <cmd>LspRename<CR> ]]
 
 -- Use <Tab> and <S-Tab> to navigate through popup menu
 vim.cmd [[inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"]]
