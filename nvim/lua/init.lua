3994cc313dac631be30a2fe04f2a5f8c18c6e686
amirrezaask <raskarpour@gmail.com>
2021-04-17 23:42:07 +0430

    add lua auto format

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,115 +1,115 @@
 -- init.lua
-local nvim = require("nvim")
+local nvim = require('nvim')
 
 -- Install Plugins
-require("plugins")
+require('plugins')
 
-require("plugin.fuzzy")
--- require'plugin.telescope'
+require('plugin.fuzzy')
+require'plugin.telescope'
 
 nvim.with_options({
-	ignorecase = true,
-	modeline = true,
-	autoread = true,
-	compatible = false,
-	encoding = "utf-8",
-	hlsearch = true,
-	history = 700,
-	tabpagemax = 100,
-	ruler = true,
-	mouse = "a",
-	wrap = true,
-	autoindent = true,
-	termguicolors = true,
-	tabstop = 4,
-	shiftwidth = 4,
-	softtabstop = 4,
-	expandtab = true,
-	backup = false,
-	writebackup = false,
-	swapfile = false,
-	clipboard = "unnamedplus",
-	splitright = true,
-	splitbelow = true,
-	cursorline = true,
+  ignorecase = true,
+  modeline = true,
+  autoread = true,
+  compatible = false,
+  encoding = 'utf-8',
+  hlsearch = true,
+  history = 700,
+  tabpagemax = 100,
+  ruler = true,
+  mouse = 'a',
+  wrap = true,
+  autoindent = true,
+  termguicolors = true,
+  tabstop = 4,
+  shiftwidth = 4,
+  softtabstop = 4,
+  expandtab = true,
+  backup = false,
+  writebackup = false,
+  swapfile = false,
+  clipboard = 'unnamedplus',
+  splitright = true,
+  splitbelow = true,
+  cursorline = true,
 })
 
-local listchars = require("listchars")
+local listchars = require('listchars')
 listchars:update()
 
 vim.cmd([[ set cursorline ]])
 local global_maps = {
-	-- Easier window navigation
-	["<C-j>"] = "<C-w>j",
-	["<C-k>"] = "<C-w>k",
-	["<C-l>"] = "<C-w>l",
-	["<C-h>"] = "<C-w>h",
-	Q = "<nop>",
-	[";"] = ":",
-	["<f5>"] = "<cmd> Run<CR>",
+  -- Easier window navigation
+  ['<C-j>'] = '<C-w>j',
+  ['<C-k>'] = '<C-w>k',
+  ['<C-l>'] = '<C-w>l',
+  ['<C-h>'] = '<C-w>h',
+  Q = '<nop>',
+  [';'] = ':',
+  ['<f5>'] = '<cmd> Run<CR>',
 }
 
 local insert_maps = {
-	-- Easily exit normal mode
-	["jk"] = "<esc>",
-	["jj"] = "<esc>",
-	["kk"] = "<esc>",
-	["kj"] = "<esc>",
+  -- Easily exit normal mode
+  ['jk'] = '<esc>',
+  ['jj'] = '<esc>',
+  ['kk'] = '<esc>',
+  ['kj'] = '<esc>',
 }
 
 local normal_maps = {
-	["<Space>/"] = ":nohl<CR>",
-	["tn"] = ":tabnext<CR>",
-	["tp"] = ":tabprevious<CR>",
-	["tc"] = ":tabclose<CR>",
-	["tt"] = ":tabnew<CR>",
-	["<Space>v"] = "<cmd>vnew<CR>",
-	j = "gj",
-	k = "gk",
+  ['<Space>/'] = ':nohl<CR>',
+  ['tn'] = ':tabnext<CR>',
+  ['tp'] = ':tabprevious<CR>',
+  ['tc'] = ':tabclose<CR>',
+  ['tt'] = ':tabnew<CR>',
+  ['<Space>v'] = '<cmd>vnew<CR>',
+  j = 'gj',
+  k = 'gk',
 }
 
 -- Snippets
-insert_maps["<c-k>"] = '<cmd> lua return require"snippets".expand_or_advance(1)<CR>'
-insert_maps["<c-k>"] = '<cmd> lua return require"snippets".advance_snippet(-1)<CR>'
+insert_maps['<c-k>'] = '<cmd> lua return require"snippets".expand_or_advance(1)<CR>'
+insert_maps['<c-k>'] = '<cmd> lua return require"snippets".advance_snippet(-1)<CR>'
 
 -- Git Blame
-normal_maps["<Space>gm"] = "<cmd>GitMessenger<CR>"
+normal_maps['<Space>gm'] = '<cmd>GitMessenger<CR>'
 
 -- Netrw settings
 vim.g.netrw_banner = 0
 
 nvim.augroup({
-	lua = {
-		"BufEnter",
-		"*.lua",
-		"set ts=2 sw=2 sts=2 expandtab",
-	},
+  lua = {
+    'BufEnter',
+    '*.lua',
+    'set ts=2 sw=2 sts=2 expandtab',
+  },
 })
 
 -- compe.nvim
-require("plugin.compe")
+require('plugin.compe')
 
 -- Side tree
-normal_maps["<Space>s"] = '<cmd>lua require"sidetree".open_side_file_browser()<CR>'
+normal_maps['<Space>s'] = '<cmd>lua require"sidetree".open_side_file_browser()<CR>'
 
 -- Git signs
-require("gitsigns").setup()
+require('gitsigns').setup()
 
 -- Nvim dev helpers
-require("lua")
+require('lua')
 
 -- Register keymaps
 nvim.map(global_maps)
 
 nvim.mode_map({
-	n = normal_maps,
-	i = insert_maps,
+  n = normal_maps,
+  i = insert_maps,
 })
-require("plugin.expressline")
-require("lsp")
-vim.api.nvim_set_option("statusline", "[%l:%L] %m%f")
+require('plugin.expressline')
+require('lsp')
+vim.api.nvim_set_option('statusline', '[%l:%L] %m%f')
 
 -- Register commands
-nvim.command("Base16Editor", [[lua require'base16.editor'.open(require'base16'.themes["<args>"])]], 1)
-nvim.command("VTerm", [[ vnew | term ]])
-nvim.command("Term", [[ new | term ]])
+nvim.command('Base16Editor', [[lua require'base16.editor'.open(require'base16'.themes["<args>"])]], 1)
+nvim.command('VTerm', [[ vnew | term ]])
+nvim.command('Term', [[ new | term ]])
