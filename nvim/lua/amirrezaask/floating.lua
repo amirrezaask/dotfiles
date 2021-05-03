f4897352cc42e1eda6354794a8a56531f52b810c
amirrezaask <raskarpour@gmail.com>
2021-05-03 18:51:33 +0430

    add VRun command

--- nvim/lua/amirrezaask/floating.lua
+++ nvim/lua/amirrezaask/floating.lua
@@ -2,6 +2,12 @@ local floating = {}
 floating.__index = floating
 floating.__call = floating.new
 
+local function setup_buf(buf)
+  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>bd!<cr>', {})
+end
+
 local center = function(win_height, win_width)
   local width = vim.api.nvim_get_option('columns')
   local height = vim.api.nvim_get_option('lines')
@@ -10,6 +16,26 @@ local center = function(win_height, win_width)
   return row, col
 end
 
+local function job_to_buf(cmd, buf)
+  vim.fn.jobstart(cmd, {
+    on_exit = function(_, code, _)
+      print('process exited with code ' .. code)
+    end,
+    on_stdout = function(_, data, _)
+      vim.schedule(function() 
+        local count = vim.api.nvim_buf_line_count(buf)
+        vim.api.nvim_buf_set_lines(buf, count, -1, false, data)
+      end)
+    end,
+    on_stderr = function(_, data, _)
+      vim.schedule(function() 
+        local count = vim.api.nvim_buf_line_count(buf)
+        vim.api.nvim_buf_set_lines(buf, count, -1, false, data)
+      end)
+    end
+  })
+
+end
 
 function floating:new(opts)
   opts = opts or {}
@@ -22,10 +48,7 @@ function floating:new(opts)
   local row, col = center(win_height, win_width)
 
   local buf = vim.api.nvim_create_buf(true, true)
-  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>bd!<cr>', {})
-
+  setup_buf(buf)
   local win = vim.api.nvim_open_win(buf, true, {
     relative = 'editor',
     width = win_width,
@@ -36,27 +59,18 @@ function floating:new(opts)
   })
   vim.api.nvim_win_set_option(win, 'winhl', 'Normal:Normal')
   if type(opts.source) == 'string' then
-    vim.fn.jobstart(opts.source, {
-      on_exit = function(_, code, _)
-        print('process exited with code ' .. code)
-      end,
-      on_stdout = function(_, data, _)
-        vim.schedule(function() 
-          local count = vim.api.nvim_buf_line_count(buf)
-          vim.api.nvim_buf_set_lines(buf, count, -1, false, data)
-        end)
-      end,
-      on_stderr = function(_, data, _)
-        vim.schedule(function() 
-          local count = vim.api.nvim_buf_line_count(buf)
-          vim.api.nvim_buf_set_lines(buf, count, -1, false, data)
-        end)
-      end
-    })
+   job_to_buf(opts.source, buf)
   elseif type(opts.source) == 'table' then
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, opts.source)
   end
 end 
+
+function floating:vnew(command)
+  vim.cmd [[ vnew ]]
+  local buf = vim.api.nvim_get_current_buf()
+  setup_buf(buf)
+  job_to_buf(command or vim.fn.input('cmd: '), buf)
+end
 
 function floating:command()
   local cmd = vim.fn.input('command: ')
--- nvim/plugin/commands.vim
+++ nvim/plugin/commands.vim
@@ -1,2 +1,3 @@
-command! Run lua require'amirrezaask.floating':command()
-nnoremap <leader>r <cmd>Run<CR>
+command! FRun lua require'amirrezaask.floating':command()
+command! VRun lua require'amirrezaask.floating':vnew()
+nnoremap <leader>r <cmd>FRun<CR>
