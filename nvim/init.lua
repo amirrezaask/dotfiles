39b386fed555d726ebca09c259b7f610548a6e50
amirrezaask <raskarpour@gmail.com>
2021-05-20 16:27:41 +0430

    fix clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 -- Basic Neovim stuff
 require('options')
 require('mappings')
+
 -- Personal Modules
 require('ftplugin.lua')
 require('amirrezaask.terminal')
 require('amirrezaask.quickfix')
 require('amirrezaask.listchars')
 require('amirrezaask.statusline')
-vim.g.transparent = true
+
 -- require'amirrezaask.nvim'.colorscheme('gruvbox')
+-- vim.g.transparent = true
 require'amirrezaask.nvim'.colorscheme('gruvbuddy')
--- require('colorbuddy').colorscheme('gruvbuddy')
 
 -- Load plugins
 require('plugin')
-
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -31,16 +31,4 @@ nvim.with_options({
 })
 
 vim.g.netrw_banner = 0
-vim.g.go_fmt_command = 'goimports'
-vim.g.go_test_show_name = 1
-vim.g.go_list_type = "quickfix"
-vim.g.go_autodetect_gopath = 1
--- vim.g.go_metalinter_autosave_enabled = ['vet', 'golint']
--- vim.g.go_metalinter_enabled = ['vet', 'golint']
-vim.g.go_gopls_complete_unimported = 1
-vim.g.go_diagnostics_level = 2 
-vim.g.go_doc_popup_window = 1
-vim.g.go_imports_mode="gopls"
-vim.g.go_imports_autosave=1
-vim.g.go_highlight_build_constraints = 1
-vim.g.go_highlight_operators = 1
+vim.cmd [[ set clipboard=unnamedplus ]]
