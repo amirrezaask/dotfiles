ade39ddd423307e583766e8b6320895ea2459f55
amirrezaask <raskarpour@gmail.com>
2021-05-28 01:32:32 +0430

    go current test

--- nvim/lua/plugin/actions.lua
+++ nvim/lua/plugin/actions.lua
@@ -2,6 +2,10 @@ local actions = require('actions')
 local floating = require('floating')
 local lsp = require('plugin.lsp')
 
+-- function go_current_function_call()
+--   local line = vim.fn.getline('.')
+-- end
+
 local function floating_window_opts(opts)
   local base = {
     height_pct = 50,
@@ -19,7 +23,8 @@ end
 actions:setup {
   mappings = {
     ['n ,ab'] = 'build',
-    ['n ,at'] = 'test_all'
+    ['n ,at'] = 'test_all',
+    ['n ,tt'] = 'test_this'
   },
   filetypes = {
     go = {
@@ -35,7 +40,27 @@ actions:setup {
           }
         })
       end,
+      test_this = function(_)
+        local function go_current_test()
+          local linenr = vim.fn.search("func \\(Test\\|Example\\)", "bcnW")
+          if linenr == 0 then return end
+          local linetext = vim.fn.getline(linenr)
+          local test_name = vim.split(linetext, ' ')[2]
+          local start, _ = string.find(test_name, '%(')
+          test_name = string.sub(test_name, 1, start-1)
+          return test_name
+        end
+        local current_test = go_current_test()
+        floating:command(('go test -v -run %s'):format(current_test), floating_window_opts {
+         jobstart = {
+            cwd = lsp.go_root()
+          }
+        })
+      end,
+      -- this_doc = function(bufnr)
+
+      --   floating:command(('go doc '))
+      -- end
     }
   }
 }
-
