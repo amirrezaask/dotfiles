a48eb82c4a800f2e89a2cd254611339fc4025a71
amirrezaask <raskarpour@gmail.com>
2021-05-28 14:25:01 +0430

    fix listchars

--- nvim/lua/plugin/listchars.lua
+++ nvim/lua/plugin/listchars.lua
@@ -1,25 +1,40 @@
-local M = {}
+local listchars = require('listchars')
+local default_listchars = {
+  eol = '↲',
+  tab = '»\\ ',
+  trail = '·',
+  extends = '<',
+  precedes = '>',
+  conceal = '┊',
+  nbsp = '␣',
+}
 
-__Current_List_Chars = {}
+local yaml_listchars = {
+  lead ='·',
+  trail='*',
+  eol = '↲',
+  tab = '->\\ ',
+  extends = '<',
+  precedes = '>',
+  conceal = '┊',
+  nbsp = '␣',
+}
 
-function M:update(chars)
-  vim.opt.list = true
-  local list_chars_tuple = {}
-  for k, v in pairs(chars) do
-    if v ~= '' then
-      table.insert(list_chars_tuple, string.format('%s:%s', k, v))
-    end
+vim.autocmd {
+  "BufEnter",
+  "*.yaml,*.yml",
+  function()
+    print("yaml mode")
+    listchars(yaml_listchars)
   end
-  vim.opt.listchars = list_chars_tuple
-end
+}
 
-return setmetatable(M, {
-  __newindex = function(t, k, v)
-    __Current_List_Chars[k] = v
-    t:update(__Current_List_Chars)
-  end,
-  __call = function(t, chars) 
-    __Current_List_Chars = chars
-    t:update(chars)
+vim.autocmd {
+  "BufLeave",
+  "*.yaml,*.yml",
+  function()
+    listchars(default_listchars)
   end
-})
+}
+
+listchars(default_listchars)
