d85d731c475aaa62e52ccee825264affd981fc23
amirrezaask <raskarpour@gmail.com>
2021-05-29 18:11:46 +0430

    some fixes

--- nvim/lua/plugin/gitsigns.lua
+++ nvim/lua/plugin/gitsigns.lua
@@ -6,12 +6,6 @@ require('gitsigns').setup({
     topdelete = {  text = '‾', numhl = 'GitSignsDeleteNr' },
     changedelete = {  text = '~-', numhl = 'GitSignsChangeNr' },
   },
+
 })
 
-
--- nnoremap <M-j> :m .+1<CR>==
--- nnoremap <M-k> :m .-2<CR>==
--- inoremap <M-j> <Esc>:m .+1<CR>==gi
--- inoremap <M-k> <Esc>:m .-2<CR>==gi
--- vnoremap <M-j> :m '>+1<CR>gv=gv
--- vnoremap <M-k> :m '<-2<CR>gv=gv
