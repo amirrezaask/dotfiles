686ada9b92878000a875828e3ba66f4ed25932ad
amirrezaask <raskarpour@gmail.com>
2021-05-30 23:26:24 +0430

    fix <M-`>

--- nvim/lua/plugin/ctrltab.lua
+++ nvim/lua/plugin/ctrltab.lua
@@ -29,15 +29,19 @@ vim.map {
     local actions = require('telescope.actions')
     local action_state = require('telescope.actions.state')
     local buffers = {}
+    local buffers_rev = {}
     for _, b in ipairs(MNIM_BUFFERS) do
       table.insert(buffers, string.format('%s: %s', b, vim.api.nvim_buf_get_name(b)))
+    end
+    for i=#buffers, 1, -1 do
+      table.insert(buffers_rev, buffers[i])
     end
     pickers.new({}, {
     prompt_title = "> MNIM BUFFER SWITCH <",
     -- TODO: add previewer
     previewer = false,
     finder = finders.new_table({
-      results = buffers,
+      results = buffers_rev,
     }),
     sorter = conf.generic_sorter(),
     attach_mappings = function(prompt_bufnr, map)
@@ -45,7 +49,14 @@ vim.map {
         local line = action_state.get_selected_entry(prompt_bufnr)[1]
         local bufnr = vim.split(line, ': ')[1]
         actions.close(prompt_bufnr)
-        --TODO: update MNIM_BUFFERS
+        local n
+        for idx, b in ipairs(MNIM_BUFFERS) do
+          if b == tonumber(bufnr) then
+            n = idx
+          end
+        end
+        table.remove(MNIM_BUFFERS, n)
+        table.insert(MNIM_BUFFERS, tonumber(bufnr))
         vim.api.nvim_set_current_buf(bufnr)
       end
       map('i', '<CR>', jump_to)
