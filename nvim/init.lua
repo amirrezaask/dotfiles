17f01f9bb0e8a6c85dec30520f4c7feddd472502
amirrezaask <raskarpour@gmail.com>
2021-06-01 00:41:00 +0430

    default theme is always gruvbuddy

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,10 @@ require('nvim')
 
 vim.g.mapleader = ' '
 -- colorscheme
--- require('colors.gruvbuddy')
+require('colors.gruvbuddy')
 -- require('colors.gruvbox')
-vim.colorscheme('tokyonight')
+-- vim.colorscheme('tokyonight')
+-- vim.colorscheme('ayu')
 
 -- Load plugins and configuration
 require('plugins')
