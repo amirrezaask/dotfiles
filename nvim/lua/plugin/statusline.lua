0aefbf67ce7519bcb2ac43f15027199288d68c51
amirrezaask <raskarpour@gmail.com>
2021-06-03 00:28:31 +0430

    nline updates

--- nvim/lua/plugin/statusline.lua
+++ nvim/lua/plugin/statusline.lua
@@ -6,14 +6,15 @@ end
 local vim = require('nline.parts.vim')
 local git = require('nline.parts.git')
 local lsp = require('nline.parts.lsp')
+local icons = require('nline.parts.icons')
 local wrappers = require('nline.wrappers')
 
 nline.make {
   vim.mode(),
   vim.space(),
-  vim.icons.git, vim.space(), git.branch(),
+  icons.git, vim.space(), git.branch(),
   vim.space(), vim.seperator(),
-  vim.icons.file,
+  icons.file,
   vim.space(),
   vim.filename(),
   vim.modified(),
