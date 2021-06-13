2db1b5e9bc46e8451eafba13582ee0a0008aed97
amirrezaask <raskarpour@gmail.com>
2021-06-13 23:17:26 +0430

    new setup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -553,6 +553,7 @@ client.connect_signal('unfocus', function(c)
 end)
 
 -- Setup xrandr
-awful.spawn('xrandr --output eDP-1 --right-of DP-1 --output eDP-1 --left-of HDMI-2 --output DP-1 --left-of eDP-1')
+-- awful.spawn('xrandr --output eDP-1 --right-of DP-1 --output eDP-1 --left-of HDMI-2 --output DP-1 --left-of eDP-1')
+awful.spawn("xrandr --output eDP-1 --below HDMI-2")
 awful.spawn('compton')
 awful.spawn('vmware-user')
