e33c5ccfce531f0014250aa3c630a0b0f867c27f
amirrezaask <raskarpour@gmail.com>
2021-07-10 23:05:11 +0430

    remove autoformat

--- nvim/lua/plugin/go.lua
+++ nvim/lua/plugin/go.lua
@@ -1,12 +1,1 @@
-vim.g.go_imports_on_save = true
-vim.autocmd {
-  "BufWritePost",
-  "*",
-  function()
-    if vim.fn.executable("goimports") == 1 and vim.g.go_imports_on_save then
-      local filename = vim.api.nvim_buf_get_name(0)
-      vim.fn.system(string.format("goimports -w %s", filename))
-      vim.c.edit()
-    end
-  end
-}
+
