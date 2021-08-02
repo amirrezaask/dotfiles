64e16aca200a91d4aeaa3dbd7665d5cefe5e90c3
amirrezaask <raskarpour@gmail.com>
2021-08-03 03:22:14 +0430

    back to gruvbuddy

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,12 +4,12 @@ require('nvim')
 vim.g.mapleader = ' '
 
 -- If you want transparency support
-vim.g.transparent = true 
+vim.g.transparent = false
 
 -- Theme
--- require('colors.gruvbuddy')
+require('colors.gruvbuddy')
 -- require('base16').set('norcalli')
-require('colors.gruvbox')
+-- require('colors.gruvbox')
 
 -- Load plugins and configuration
 require('plugins')
--- nvim/lua/plugin/statusline.lua
+++ nvim/lua/plugin/statusline.lua
@@ -9,69 +9,68 @@ local lsp = require('nline.parts.lsp')
 local icons = require('nline.parts.icons')
 local wrappers = require('nline.wrappers')
 
--- nline.make {
---   vim.mode {
---     texts = {
---       normal = 'Normal',
---       visual = 'Visual',
---       visual_block = 'VisualBlock',
---       insert = 'Insert',
---       insert_complete = 'IComplete',
---       command = 'Command',
---       terminal = 'Terminal'
---     }
---   },
---   vim.space(), icons.git_branch, vim.space(), git.branch(),
-
---   vim.seperator(),
-
---   icons.file, vim.space(), vim.filename({ shorten = true }), vim.modified(),
-
---   vim.seperator(),
-
---   wrappers.square_brackets(lsp.current_function()),
---   wrappers.square_brackets(git.changes()),
---   wrappers.square_brackets(lsp.diagnostics()),
---   lsp.progress(),
---   wrappers.square_brackets(vim.line()..vim.space()..vim.colon()..vim.col()),
---   vim.filetype(),
--- }
-
-
-nline.make({
-   vim.mode {
-    hls = {
-      normal = 'StatusLine',
-      visual = 'StatusLine',
-      visual_block = 'StatusLine',
-      insert = 'StatusLine',
-      insert_complete = 'StatusLine',
-      command = 'StatusLine',
-      terminal = 'StatusLine'
-    },
+nline.make {
+  vim.mode {
     texts = {
-       normal = 'Normal',
-       visual = 'Visual',
-       visual_block = 'VisualBlock',
-       insert = 'Insert',
-       insert_complete = 'IComplete',
-       command = 'Command',
-       terminal = 'Terminal'
-     }
-   },
-  git.branch(),
-  vim.filename({ shorten = false }),
-  git.changes(),
-  lsp.diagnostics({
-    icons = {
-      error = 'E',
-      warning = 'W',
-      info = 'I',
-      hint = 'H',
-      ok = '',
-      ['function'] = '',
-      },
-    }),
-    vim.line()..vim.space()..vim.colon()..vim.col(),
-    lsp.progress()
-}, {delimiter = ' | '})
+      normal = 'Normal',
+      visual = 'Visual',
+      visual_block = 'VisualBlock',
+      insert = 'Insert',
+      insert_complete = 'IComplete',
+      command = 'Command',
+      terminal = 'Terminal'
+    }
+  },
+  vim.space(), icons.git_branch, vim.space(), git.branch(),
+
+  vim.seperator(),
+
+  icons.file, vim.space(), vim.filename({ shorten = true }), vim.modified(),
+
+  vim.seperator(),
+
+  wrappers.square_brackets(lsp.current_function()),
+  wrappers.square_brackets(git.changes()),
+  wrappers.square_brackets(lsp.diagnostics()),
+  lsp.progress(),
+  wrappers.square_brackets(vim.line()..vim.space()..vim.colon()..vim.col()),
+  vim.filetype(),
+}
+
+-- nline.make({
+--    vim.mode {
+--     hls = {
+--       normal = 'StatusLine',
+--       visual = 'StatusLine',
+--       visual_block = 'StatusLine',
+--       insert = 'StatusLine',
+--       insert_complete = 'StatusLine',
+--       command = 'StatusLine',
+--       terminal = 'StatusLine'
+--     },
+--     texts = {
+--        normal = 'Normal',
+--        visual = 'Visual',
+--        visual_block = 'VisualBlock',
+--        insert = 'Insert',
+--        insert_complete = 'IComplete',
+--        command = 'Command',
+--        terminal = 'Terminal'
+--      }
+--    },
+--   git.branch(),
+--   vim.filename({ shorten = false }),
+--   git.changes(),
+--   lsp.diagnostics({
+--     icons = {
+--       error = 'E',
+--       warning = 'W',
+--       info = 'I',
+--       hint = 'H',
+--       ok = '',
+--       ['function'] = '',
+--       },
+--     }),
+--     vim.line()..vim.space()..vim.colon()..vim.col(),
+--     lsp.progress()
+-- }, {delimiter = ' | '})
