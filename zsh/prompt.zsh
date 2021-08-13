363300e189f4dde3eccda6d0294c4271764fc487
amirrezaask <raskarpour@gmail.com>
2021-08-13 05:27:31 +0430

    fish like prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -13,10 +13,10 @@ get_branch(){
     if [ -z "$branch" ]; then
         echo ""
     else
-        echo "%F{33}git:%f %F{9}$branch%f"
+        echo "$branch"
     fi
 }
 
 setopt PROMPT_SUBST
-# PROMPT='%F{51}%2~%f $(get_branch) '
-PROMPT='%2~ $(get_branch) %F{green}>%f '
+
+PROMPT='%F{green}%~%f $(get_branch) > '
