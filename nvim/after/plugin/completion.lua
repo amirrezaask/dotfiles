03a11f937b345788590a67bc5a0fcafb41fa9ed1
amirrezaask <raskarpour@gmail.com>
2021-08-26 07:32:01 +0430

    move to nvim-cmp

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -1,44 +1,51 @@
-local has_compe, compe = pcall(require, "compe")
-if not has_compe then
+-- Set completeopt to have a better completion experience
+-- vim.opt.completeopt = { "menuone", "noselect" }
+
+-- -- Avoid showing message extra message when using completion
+-- vim.opt.shortmess:append "c"
+
+-- vim.imap {
+--   ["<expr> <S-Tab>"] = [[ pumvisible() ? "\<C-p>" : "\<S-Tab>"]],
+--   ["<expr> <Tab>"] = [[ pumvisible() ? "\<C-n>" : "\<Tab>" ]],
+--   ["<expr> <C-Space>"] = "compe#complete()",
+--   ["<expr> <CR>"] = "compe#confirm()",
+--   ["<expr> <C-e>"] = "compe#close('<C-e>')",
+--   ["<expr> <C-f>"] = "compe#scroll( {'delta': +4} )",
+--   ["<expr> <C-d>"] = "compe#scroll( {'delta': -4} )",
+-- }
+
+local has_cmp, cmp = pcall(require, "cmp")
+if not has_cmp then
   return
 end
 
-compe.setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 1,
-  preselect = "enable",
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = true,
-
-  source = {
-    path = true,
-    buffer = true,
-    calc = false,
-    nvim_lsp = true,
-    nvim_lua = true,
-    snippets_nvim = true,
-  },
-}
-
--- Set completeopt to have a better completion experience
 vim.opt.completeopt = { "menuone", "noselect" }
 
--- Avoid showing message extra message when using completion
+-- Don't show the dumb matching stuff.
 vim.opt.shortmess:append "c"
 
-vim.imap {
-  ["<expr> <S-Tab>"] = [[ pumvisible() ? "\<C-p>" : "\<S-Tab>"]],
-  ["<expr> <Tab>"] = [[ pumvisible() ? "\<C-n>" : "\<Tab>" ]],
-  ["<expr> <C-Space>"] = "compe#complete()",
-  ["<expr> <CR>"] = "compe#confirm()",
-  ["<expr> <C-e>"] = "compe#close('<C-e>')",
-  ["<expr> <C-f>"] = "compe#scroll( {'delta': +4} )",
-  ["<expr> <C-d>"] = "compe#scroll( {'delta': -4} )",
+cmp.setup {
+  snippet = {},
+
+  -- You can set mapping if you want.
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+  },
+
+  -- You should specify your *installed* sources.
+  sources = {
+    { name = "buffer" },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "nvim_lua" },
+  },
 }
