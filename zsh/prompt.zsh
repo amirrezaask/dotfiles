9cee1f18e33aea33d81cddb94d12df879ae1cee3
amirrezaask <raskarpour@gmail.com>
2021-09-17 17:27:39 +0430

    some updates

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,6 +7,13 @@ if [ "$PROMPT_ENGINE" = "starship" ]; then
         sh -c "$(curl -fsSL https://starship.rs/install.sh)"
     fi
     eval "$(starship init zsh)"
+elif [ "$PROMPT_ENGINE" = "oh-my-posh" ]; then
+    if ! command -v oh-my-posh &> /dev/null
+    then
+        sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+        sudo chmod +x /usr/local/bin/oh-my-posh
+    fi
+    eval "$(oh-my-posh --init --shell zsh --config ~/src/github.com/amirrezaask/dotfiles/powershell/amirreza.omp.json)"
 else
     get_branch(){
         branch=$(parse_git_branch)
