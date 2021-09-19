2c4604427d3686f07d20a7ea62b14ec96819b310
amirrezaask <raskarpour@gmail.com>
2021-09-20 00:06:42 +0430

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -43,57 +43,6 @@ function snappvpn
     sudo openfortivpn -c ~/snappDC.conf
 end
 
-# install starship if not installed
-if not command -v starship &> /dev/null
-then
-    curl -fsSL 'https://starship.rs/install.sh'
-end
+set -x PROMPT_ENGINE "none" # Also starship, oh-my-posh, none 
 
-set -x PROMPT_ENGINE "none" # Also starship, none available
-
-# install oh-my-posh
-if not command -v oh-my-posh &> /dev/null
-then
-    sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-    sudo chmod +x /usr/local/bin/oh-my-posh
-end
-
-# Greeting message from fish
-function fish_greeting
-    # welcome
-end
-
-if [ $PROMPT_ENGINE = "starship" ]
-    starship init fish | source
-end
-
-if [ $PROMPT_ENGINE = "oh-my-posh" ]
-    oh-my-posh --init --shell fish --config ~/src/github.com/amirrezaask/dotfiles/powershell/amirreza.omp.json | source
-end
-
-if [ $PROMPT_ENGINE = "none" ]
-    # Fish git prompt
-    set __fish_git_prompt_showuntrackedfiles 'yes'
-    set __fish_git_prompt_showdirtystate 'yes'
-    set __fish_git_prompt_showstashstate ''
-    set __fish_git_prompt_showupstream 'none'
-    set -g fish_prompt_pwd_dir_length 3
-
-    function fish_prompt
-        set_color brblack
-        echo -n "["(date "+%H:%M")"] "
-        set_color brblue
-        echo -n (hostname)
-        if [ $PWD != $HOME ]
-            set_color brblack
-            echo -n ':'
-            set_color yellow
-            echo -n (basename $PWD)
-        end
-        set_color green
-        printf '%s ' (__fish_git_prompt)
-        set_color red
-        echo -n '| '
-        set_color normal
-    end
-end
+source ~/.config/fish/prompt.fish
---
+++ fish/prompt.fish
@@ -0,0 +1,46 @@
+# install oh-my-posh
+if [ $PROMPT_ENGINE = "starship" ]
+    # install if not exists
+    if not command -v starship &> /dev/null
+    then
+        curl -fsSL 'https://starship.rs/install.sh'
+    end
+
+    starship init fish | source
+end
+
+if [ $PROMPT_ENGINE = "oh-my-posh" ]
+    if not command -v oh-my-posh &> /dev/null
+    then
+        sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+        sudo chmod +x /usr/local/bin/oh-my-posh
+    end
+    oh-my-posh --init --shell fish --config ~/src/github.com/amirrezaask/dotfiles/powershell/amirreza.omp.json | source
+end
+
+if [ $PROMPT_ENGINE = "none" ]
+    # Fish git prompt
+    set __fish_git_prompt_showuntrackedfiles 'yes'
+    set __fish_git_prompt_showdirtystate 'yes'
+    set __fish_git_prompt_showstashstate ''
+    set __fish_git_prompt_showupstream 'none'
+    set -g fish_prompt_pwd_dir_length 3
+
+    function fish_prompt
+        set_color brblack
+        echo -n "["(date "+%H:%M")"] "
+        set_color brblue
+        echo -n (hostname)
+        if [ $PWD != $HOME ]
+            set_color brblack
+            echo -n ':'
+            set_color yellow
+            echo -n (basename $PWD)
+        end
+        set_color green
+        printf '%s ' (__fish_git_prompt)
+        set_color red
+        echo -n '| '
+        set_color normal
+    end
+end
