d66b2f2461120e9e798c0511c83a2ef099f84837
amirrezaask <raskarpour@gmail.com>
2021-09-21 00:33:46 +0430

    some fish updates

--- fish/config.fish
+++ fish/config.fish
@@ -5,13 +5,11 @@ set -x PYTHONBINS "$HOME/.local/bin"
 set -x EDITOR 'nvim'
 set -x GOROOT '/usr/local/go'
 set -x GOPRIVATE 'gitlab.snapp.ir'
-set -x PLAN9 "$HOME/.local/plan9"
 set -x ZIGPATH "/usr/local/zig"
 set -x RUSTUP "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
 set -x PATH "$RUSTUP:$ZIGPATH:/usr/local/go/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PYTHONBINS:/usr/local/Postman:$HOME/.local/elixir-ls/:$PATH"
+
 alias open='xdg-open'
-alias gs='git status'
-alias gpsup='gp --set-upstream origin (git_current_branch)'
 alias kcl='kubectl'
 alias oss='cd ~/src/github.com/amirrezaask'
 set -x DOTFILES "~/src/github.com/amirrezaask/dotfiles"
@@ -30,10 +28,20 @@ alias webstorm="webstorm 2>/dev/null &"
 alias pycharm="pycharm 2>/dev/null &"
 alias intelij="intelij 2>/dev/null &"
 alias datagrip="datagrip 2>/dev/null &"
+
 alias gdoc='godoc -http=:6060'
+
+# Editor
 alias vim='nvim'
 alias vi='nvim'
 alias v='nvim'
+
+
+# Git
+alias gs='git status'
+alias gd='git diff'
+alias gpsup='gp --set-upstream origin (git_current_branch)'
+
 # FZF stuff
 setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
 setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
