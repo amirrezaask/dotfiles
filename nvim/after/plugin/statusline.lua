2885de41be0a640dc57cfaeeaad9280d350fb80f
amirrezaask <raskarpour@gmail.com>
2021-10-03 22:05:32 +0330

    statusline update

--- nvim/after/plugin/statusline.lua
+++ nvim/after/plugin/statusline.lua
@@ -7,7 +7,41 @@ local vim = require "nline.parts.vim"
 local git = require "nline.parts.git"
 local lsp = require "nline.parts.lsp"
 local wrappers = require "nline.wrappers"
+local icons = require "nline.parts.icons"
 
+local tj = {
+  vim.mode {
+    texts = {
+      normal = "Normal",
+      visual = "Visual",
+      visual_block = "VisualBlock",
+      insert = "Insert",
+      insert_complete = "IComplete",
+      command = "Command",
+      terminal = "Terminal",
+    },
+  },
+  vim.space(),
+  icons.git_branch,
+  vim.space(),
+  git.branch(),
+
+  vim.seperator(),
+
+  icons.file,
+  vim.space(),
+  vim.filename { shorten = true },
+  vim.modified(),
+
+  vim.seperator(),
+
+  -- wrappers.square_brackets(lsp.current_function()),
+  wrappers.square_brackets(git.changes()),
+  wrappers.square_brackets(lsp.diagnostics()),
+  -- lsp.progress(),
+  wrappers.square_brackets(vim.line() .. vim.space() .. vim.colon() .. vim.col()),
+  vim.filetype(),
+}
 local me = {
   vim.modified(),
   vim.filename { shorten = false } { hl = "NormalMode" },
@@ -22,4 +56,4 @@ local me = {
   wrappers.square_brackets(vim.line() .. vim.space() .. vim.colon() .. vim.col()),
   vim.filetype(),
 }
-nline.make(me)
+nline.make(tj)
