4217c8df06fe36a284aabca143ddccd69d3036ba
amirrezaask <raskarpour@gmail.com>
2021-10-19 18:41:28 +0330

    nvim

--- nvim/lua/amirrezaask/telescope.lua
+++ nvim/lua/amirrezaask/telescope.lua
@@ -14,7 +14,7 @@ local dropdown = require("telescope.themes").get_dropdown
 local notheme = function(opts)
   return opts
 end
-local current_theme = ivy
+local current_theme = notheme
 
 telescope.setup {
   defaults = {
