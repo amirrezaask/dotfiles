ba4d1ecaf49d36a59757835e68703b170a9f734d
amirrezaask <raskarpour@gmail.com>
2021-12-02 02:43:21 +0330

    remove old doc stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,22 +4,6 @@
 --  / ___ |/ / / / / / / /  / /  /  __/ / /_/ /_/ / ___ |(__  ) ,<
 -- /_/  |_/_/ /_/ /_/_/_/  /_/   \___/ /___/\__,_/_/  |_/____/_/|_|
 
--- A simple note to people reading my configuration stuff
---
--- ./lua/plugins.lua
--- is where all 3rd party plugins are installed using `packer.nvim`
---
--- ./lua/amirrezaask/*.lua
--- is were I put my new plugins or extensions for existing plugins.
---
--- ./plugin/*
--- This is were I put neovim itself configuration like keymaps.
---
--- ./after/*
--- This is were I put configurations for 3rd party plugins I installed.
-
--- Extensions for neovim on vim metatable, most of them are WIP in neovim core and when they are merged I will remove them from this.
-
 -- Install package manager
 local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
