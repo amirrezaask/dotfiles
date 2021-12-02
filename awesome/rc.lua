66d792626b23f4ac43a06f5669571683d18fa646
amirrezaask <raskarpour@gmail.com>
2021-12-02 20:48:21 +0330

    remove menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -22,10 +22,6 @@ local hotkeys_popup = require "awful.hotkeys_popup"
 -- when client with a matching name is opened:
 require "awful.hotkeys_popup.keys"
 
--- Load Debian menu entries
-local has_debian, debian = pcall(require, "debian.menu")
-local has_fdo, freedesktop = pcall(require, "freedesktop")
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -93,47 +89,6 @@ awful.layout.layouts = {
   -- awful.layout.suit.corner.se,
 }
 -- }}}
-
--- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-  {
-    "hotkeys",
-    function()
-      hotkeys_popup.show_help(nil, awful.screen.focused())
-    end,
-  },
-  { "manual", TERMINAL .. " -e man awesome" },
-  { "edit config", EDITOR_CMD .. " " .. awesome.conffile },
-  { "restart", awesome.restart },
-  {
-    "quit",
-    function()
-      awesome.quit()
-    end,
-  },
-}
-
-local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
-local menu_terminal = { "open terminal", TERMINAL }
-
-if has_fdo then
-  mymainmenu = freedesktop.menu.build {
-    before = { menu_awesome },
-    after = { menu_terminal },
-  }
-end
-if has_debian then
-  mymainmenu = awful.menu {
-    items = {
-      menu_awesome,
-      { "Debian", debian.menu.Debian_menu.Debian },
-      menu_terminal,
-    },
-  }
-end
-
-mylauncher = awful.widget.launcher { image = beautiful.awesome_icon, menu = mymainmenu }
 
 -- Menubar configuration
 menubar.utils.terminal = TERMINAL -- Set the terminal for applications that require it
