7ce154041f3b3b1cac55633c20acab909ff7347d
amirrezaask <raskarpour@gmail.com>
2021-12-04 00:50:56 +0330

    fix i3 language switch

--- nvim/lua/amirrezaask/go.lua
+++ nvim/lua/amirrezaask/go.lua
@@ -26,3 +26,6 @@ dap.configurations.go = {
 }
 vim.g.go_fmt_autosave = 1
 vim.g.go_fmt_command = "goimports"
+Go_telescope_picker = require"amirrezaask.telescope".make_command_picker("Go", {})
+
+vim.cmd [[ command! GoPicker lua Go_telescope_picker() ]]
