9e383588f759291ab59841e93025d036d0c5553e
amirrezaask <raskarpour@gmail.com>
2021-12-03 23:42:07 +0330

    wallpapers

--- nvim/lua/amirrezaask/go.lua
+++ nvim/lua/amirrezaask/go.lua
@@ -29,6 +29,6 @@ dap.configurations.go = {
 vim.cmd [[ command! GoCommands lua GO_telescope_picker() ]]
 
 vim.cmd [[ augroup Go ]]
-vim.cmd [[ autocmd BufWritePre *.go :silent! lua require('go.format').gofmt() ]]
+-- vim.cmd [[ autocmd BufWritePre *.go :silent! lua require('go.format').gofmt() ]]
 vim.cmd [[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]]
 vim.cmd [[ augroup END ]]
