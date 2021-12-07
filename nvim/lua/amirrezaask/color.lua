25c506e2cd28cc60b95a6ca659c7e1a2c6ad36b9
amirrezaask <raskarpour@gmail.com>
2021-12-07 21:19:39 +0330

    jedi language server for python

--- nvim/lua/amirrezaask/color.lua
+++ nvim/lua/amirrezaask/color.lua
@@ -2,7 +2,7 @@
 vim.cmd [[ colorscheme gruvbox ]]
 
 -- transparency
-vim.cmd [[ hi Normal guibg=none ]]
+-- vim.cmd [[ hi Normal guibg=none ]]
 
 vim.cmd [[ highlight LineNr guifg=#5eacd3 ]]
 vim.cmd [[ highlight netrwDir guifg=#5eacd3 ]]
--- nvim/lua/amirrezaask/lsp.lua
+++ nvim/lua/amirrezaask/lsp.lua
@@ -48,7 +48,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local servers = { "clangd", "rust_analyzer", "gopls", "intelephense" }
+local servers = { "clangd", "rust_analyzer", "gopls", "intelephense", "jedi_language_server" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
