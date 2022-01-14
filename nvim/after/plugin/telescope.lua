a754c09b96978e53412284cbdc07e60d96a26bea
amirrezaask <raskarpour@gmail.com>
2022-01-14 17:05:05 +0330

    simplify

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,3 @@
-local M = {}
-
 local telescope_actions = require "telescope.actions"
 
 -- Telescope
@@ -38,4 +36,20 @@ require("telescope").setup {
 }
 
 require("telescope").load_extension "harpoon"
-return M
+
+vim.cmd [[
+  nnoremap <leader><leader> <cmd>Telescope find_files theme=ivy<CR>
+
+  nnoremap <leader>fp <cmd>Telescope find_files theme=ivy hidden=true cwd=~/.local/share/nvim/site/pack/packer<CR>
+
+  nnoremap <leader>ps <cmd>Telescope find_files theme=ivy hidden=true cwd=~/src/gitlab.snapp.ir<CR>
+
+  nnoremap <C-q> <cmd>Telescope quickfix theme=ivy<CR>
+
+  nnoremap ?? <cmd>Telescope live_grep theme=ivy<CR>
+
+  nnoremap <leader>en <cmd>Telescope find_files theme=ivy<CR>
+
+  nnoremap <leader><leader> <cmd>Telescope find_files theme=ivy<CR>
+]]
+
