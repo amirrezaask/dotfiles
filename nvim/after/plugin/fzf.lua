bf75e606f731d6dcbe1d4c7ae79a0697ce3b8995
amirrezaask <raskarpour@gmail.com>
2022-03-18 23:54:27 +0330

    updates

--- nvim/after/plugin/fzf.lua
+++ nvim/after/plugin/fzf.lua
@@ -1,2 +1,12 @@
-vim.g.fzf_layout = { ['down'] = '40%' }
+-- vim.g.fzf_layout = { ['down'] = '40%' }
+vim.g.fzf_preview_window = {}
+vim.cmd [[
+  nnoremap <leader><leader> <cmd>Files<CR>
+  nnoremap <leader>fp <cmd>Files ~/.local/share/nvim/site/pack/packer<CR>
+  nnoremap <leader>ps <cmd>Files ~/src/gitlab.snapp.ir<CR>
+  nnoremap <leader>en <cmd>Files ~/.config/nvim<CR>
+  nnoremap ?? <cmd>Rg<CR>
+]]
+
+
 
