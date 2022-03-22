ff078307703d22e72d7495cabfc07c11839562c3
AmirrezaAsk <raskarpour@gmail.com>
2022-03-22 23:40:55 +0430

    fzf functions for fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,10 @@ set -x GOPATH "$HOME"
 set -x PYTHONBINS "$HOME/.local/bin"
 set -x EDITOR 'nvim'
 set -x GOPRIVATE 'gitlab.snapp.ir'
-set -x DOTFILES "~/src/github.com/amirrezaask/dotfiles"
-set -x SNAPP "~/src/gitlab.snapp.ir"
-set -x OSS "~/src/github.com/amirrezaask"
-set -x GOLOBBY "~/src/github.com/golobby"
+set -x DOTFILES "$HOME/src/github.com/amirrezaask/dotfiles"
+set -x SNAPP "$HOME/src/gitlab.snapp.ir"
+set -x OSS "$HOME/src/github.com/amirrezaask"
+set -x GOLOBBY "$HOME/src/github.com/golobby"
 set -x PATH "$GOPATH/bin:/opt/homebrew/bin:$ELIXIR/bin:$HOME/.luarocks/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/Postman:$HOME/.local/elixir-ls/:$HOME/.cache/rebar3/bin:$PATH:$DOTFILES/bin:$PLAN9/bin:$HOME/.composer/vendor/bin"
 
 function freenet
