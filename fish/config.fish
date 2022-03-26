1a3673d82c8e5111ef21fb1f0f05366a68f8dd70
AmirrezaAsk <raskarpour@gmail.com>
2022-03-26 21:10:15 +0430

    Kitty mac updates, zig fmt on save

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ set -x DOTFILES "$HOME/src/github.com/amirrezaask/dotfiles"
 set -x SNAPP "$HOME/src/gitlab.snapp.ir"
 set -x OSS "$HOME/src/github.com/amirrezaask"
 set -x GOLOBBY "$HOME/src/github.com/golobby"
-set -x PATH "$GOPATH/bin:/opt/homebrew/bin:$ELIXIR/bin:$HOME/.luarocks/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/Postman:$HOME/.local/elixir-ls/:$HOME/.cache/rebar3/bin:$PATH:$DOTFILES/bin:$PLAN9/bin:$HOME/.composer/vendor/bin"
+set -x PATH "$GOPATH/bin:/opt/homebrew/bin:$ELIXIR/bin:$HOME/.luarocks/bin:$HOME/.config/composer/vendor/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/Postman:$HOME/.local/elixir-ls/:$HOME/.cache/rebar3/bin:$PATH:$HOME/.composer/vendor/bin"
 
 function freenet
     echo $VPN_PASSWORD | sudo openconnect --no-dtls --passwd-on-stdin --user $VPN_USERNAME $VPN_SERVER
@@ -17,6 +17,7 @@ alias lock='i3lock -c000000'
 
 # Git
 alias gs='git status'
+alias gl='git log'
 alias gd='git diff'
 alias gp='git push'
 alias gpl='git pull'
@@ -24,6 +25,8 @@ alias gco='git checkout'
 alias gcb='git checkout -b'
 alias gca='git commit -a'
 alias gc='git commit'
+alias ga='git add'
+alias g='git'
 
 # vim
 alias vim='nvim'
