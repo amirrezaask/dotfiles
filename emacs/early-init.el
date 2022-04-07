6c27a30d0a8a8ae590d2be560ff75a916f8a4738
AmirrezaAsk <raskarpour@gmail.com>
2022-04-08 00:27:56 +0430

    back to literate format: WIP

---
+++ emacs/early-init.el
@@ -0,0 +1,7 @@
+(tool-bar-mode 0) ;; disable top toolbar
+(scroll-bar-mode 0) ;; disable scroll bar
+(menu-bar-mode -1) ;; Disable menu bar
+
+(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
+
+
