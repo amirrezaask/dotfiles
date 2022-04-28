0f2492cf75314b2f17d4def585696f4cbfe9bbba
AmirrezaAsk <raskarpour@gmail.com>
2022-04-29 04:11:20 +0430

    restore personal emacs config

---
+++ emacs/init.el
@@ -0,0 +1,35 @@
+(defvar amirreza/config-file (expand-file-name "README.org" user-emacs-directory))
+
+(defvar amirreza/packages-file (expand-file-name "packages.el" user-emacs-directory))
+
+(defun amirreza/reload ()
+  "Reload user configuration file"
+  (interactive)
+  (org-babel-load-file amirreza/config-file))
+
+(defun amirreza/sync ()
+  "Sync packages with packages in packages.el"
+  (interactive)
+  (load-file amirreza/packages-file))
+
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")) ;; add melpa repository
+
+;; Initialize emacs package manager
+(package-initialize)
+
+;; helper to install missing packages
+(defun amirreza/packages-install (packages)
+    (mapc (lambda (pkg)
+        (unless (package-installed-p pkg)
+            (package-install pkg)))
+    packages
+    ))
+
+
+
+(amirreza/sync) ;; Sync packages
+
+(amirreza/reload) ;; Load user configuration
+
+
