3b62b4ef0caf0838a4009496998e76e26b4837cc
AmirrezaAsk <raskarpour@gmail.com>
2022-04-29 05:33:01 +0430

    DOOM Emacs FIRST KEY

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -75,4 +75,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(evil-define-key 'normal global-map (kbd "SPC w s") 'persp-switch)
+
+(map! :n ";" 'evil-ex)
