94b77e5014befd168ac93e8a2a894b76c12956bd
amirrezaask <raskarpour@gmail.com>
2022-07-25 06:49:18 +0430

    emacs is backkkk

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -14,7 +14,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "C", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
-  vim.cmd [[ command! Format execute '<cmd>lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
