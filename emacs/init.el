872bb73044df2f48bc0ce7197bd4b0a970b725c5
amirrezaask <raskarpour@gmail.com>
2022-08-09 10:02:27 +0430

    wildly removing stuff

--- emacs/init.el
+++ emacs/init.el
@@ -26,15 +26,13 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p) ;; Show y or n instead of yes or no for question prompts.
 
-(global-set-key (kbd "M-p") (lambda () (interactive) (previous-line 20)))
-(global-set-key (kbd "M-n") (lambda () (interactive) (next-line 20)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (previous-line 10)))
+(global-set-key (kbd "M-n") (lambda () (interactive) (next-line 10)))
 
 (setq echo-keystrokes 0.1) ;; Show keystrokes in minibuffer faster than default.
 
 (setq use-dialog-box nil) ;; Don't use any kind of GUI dialog box.
-
 (setq inhibit-splash-screen 0) ;; Disable Emacs start screen.
-
 (setq ring-bell-function 'ignore) ;; No bell ringing.
 
 (set-terminal-coding-system 'utf-8) ;; default emacs encodings
@@ -58,8 +56,6 @@
 (setq hscroll-step 1) ;; Number of columns to scroll when point is to close to edge.
 (setq hscroll-margin 1) ;; How many columns away from edge to start scrolling.
 
-;; (column-number-mode +1) ;; Show column number in modeline.
-
 (global-display-line-numbers-mode 1) ;; Ensure line numbers globally.
 
 (setq kill-ring-max 15) ;; Capacity of kill-ring.
@@ -71,35 +67,7 @@
 (when (> emacs-major-version 26) (global-tab-line-mode -1)) ;; Disable tab line in Emacs 27+.
 
 ;; Font
-(set-frame-font "JetBrainsMono Nerd Font Mono 16" nil t) ;; Set font
-
-;; Vertico - Consult
-;; (package-install 'vertico)
-;; (vertico-mode 1)
-;; (setq vertico-resize nil
-;;       vertico-count 14
-;;       vertico-cycle t
-;;       completion-in-region-function
-;;       (lambda (&rest args)
-;;         (apply (if vertico-mode
-;;                    #'consult-completion-in-region
-;;                  #'completion--in-region)
-;;               args)))
-
-;; (package-install 'orderless)
-
-;; (setq completion-styles '(basic orderless)
-;;       completion-category-defaults nil
-;;       completion-category-overrides '((file (styles partial-completion))))
-
-(require 'orderless)
-
 (package-install 'gruber-darker-theme)
-
-;;(package-install 'consult)
-
-;; (global-set-key (kbd "C-s") 'consult-line)
-;; (global-set-key (kbd "C-c C-f") 'project-find-file)
 
 (package-install 'exec-path-from-shell)
 (setq exec-path-from-shell-shell-name "zsh")
@@ -156,16 +124,6 @@
                            (setq org-startup-folded t)
                            ))
 
-;; Perspective mode
-(package-install 'perspective)
-(global-set-key (kbd "C-x w s") 'persp-switch)
-(global-set-key (kbd "C-x w n") 'persp-next)
-(global-set-key (kbd "C-x w k") 'persp-kill)
-(setq persp-suppress-no-prefix-key-warning t)
-(persp-mode 1)
-
-
-
 ;; Golang
 (package-install 'go-mode)
 (add-hook 'go-mode-hook (lambda () (add-to-list 'exec-path (concat (getenv "HOME") "/bin"))))
@@ -180,7 +138,6 @@
 
 ;; Rust
 (package-install 'rust-mode)
-(add-hook 'rust-mode-hook (lambda () (interactive) (local-unset-key (kbd "C-c C-f"))))
 
 ;; Ziglang
 (package-install 'zig-mode)
@@ -196,32 +153,16 @@
 (package-install 'docker-compose-mode)
 (package-install 'dockerfile-mode)
 
-(package-install 'which-key)
-
-(which-key-setup-minibuffer)
-
 ;; LSP
+(package-install 'eglot)
+(add-hook 'go-mode-hook #'eglot-ensure)
+(add-hook 'php-mode-hook #'eglot-ensure)
+(add-hook 'rust-mode-hook #'eglot-ensure)
+(add-hook 'zig-mode-hook #'eglot-ensure)
+(add-hook 'python-mode-hook #'eglot-ensure)
+(add-hook 'c-mode-hook #'eglot-ensure)
+(add-hook 'c++-mode-hook #'eglot-ensure)
 
-;; (package-install 'lsp-mode)
-;; (package-install 'consult-lsp)
-
-;; (setq lsp-keymap-prefix "C-c l")
-
-;; (add-hook 'lsp-mode-hook (lambda ()
-;;                            (define-key lsp-mode-map (kbd "M-i") #'consult-lsp-symbols)
-;;                            (define-key lsp-mode-map (kbd "M-'") #'consult-lsp-diagnostics)
-;;                            ))
-
-;; (setq lsp-headerline-breadcrumb-enable nil)
-;; (setq lsp-auto-guess-root t)
-;; ;; LSP hooks
-;; (add-hook 'go-mode-hook #'lsp)
-;; (add-hook 'php-mode-hook #'lsp)
-;; (add-hook 'rust-mode-hook #'lsp)
-;; (add-hook 'zig-mode-hook #'lsp)
-;; (add-hook 'python-mode-hook #'lsp)
-;; (add-hook 'c-mode-hook #'lsp)
-;; (add-hook 'c++-mode-hook #'lsp)
 
 (package-install 'cmake-mode)
 
@@ -241,24 +182,14 @@
                 "  "
                 mode-line-end-spaces))
 
-(add-to-list 'custom-theme-load-path (expand-file-name "lisp" user-emacs-directory))
 
-
-(global-set-key (kbd "C-c b") #'compile)
-(global-set-key (kbd "C-c e") #'async-shell-command)
-
-(global-set-key (kbd "C-c c" ) (lambda ()
-                               (interactive)
-                               (find-file (expand-file-name "init.el" user-emacs-directory))
-                               ))
 
 
 (defun GREP (text)
   (interactive "sPattern: ")
 
   (cond
-   (
-    (not (null (executable-find "rg"))) (compilation-start
+   ((not (null (executable-find "rg"))) (compilation-start
                                           (concat
                                            "rg "
                                            "--line-number "
@@ -268,21 +199,36 @@
                                            )
                                           'grep-mode
                                           ))
-    (vc-backend (buffer-file-name) ) (compilation-start
+    ((vc-backend (buffer-file-name)) (compilation-start
                                       (concat
                                        "git "
-                                       "grep "
+                                       "grep -n "
                                        text
                                        )
                                       'grep-mode
-                                      )
+                                      ))
+    (t (compilation-start (concat "grep -n -RI " text) 'grep-mode))
      
     )
-   
+  )
+
+(defun FIND-FILE ()
+  (interactive)
+  (cond
+   ((vc-backend (buffer-file-name)) (project-find-file))
+   (t (call-interactively 'find-file))
+   )
   )
 
 
-;; theme
+
+(global-set-key (kbd "C-9") #'compile)
+(global-set-key (kbd "C-8") #'async-shell-command)
+(global-set-key (kbd "C-0") 'GREP)
+
+(global-set-key (kbd "C-x C-f") 'FIND-FILE)
+
+;; UI stuff
 (set-face-attribute 'default nil :foreground "#d3b58d" :background "#072626")
 (set-face-attribute 'cursor nil :background "green")
 
@@ -293,14 +239,10 @@
 (set-face-attribute 'font-lock-variable-name-face nil :foreground "#c8d4ec")
 (set-face-attribute 'font-lock-warning-face nil :foreground "$504038")
 (set-face-attribute 'font-lock-constant-face nil :foreground "#7ad0c6")
-
-(set-face-attribute 'grep-highlight-matches nil :foreground "white" :bold t)
-
 (set-face-attribute 'highlight nil :foreground "white")
-
 (set-face-attribute 'mode-line nil :foreground "grey" :background "black") ;; background is the color of text some how.
 (set-face-attribute 'region nil :background "#3c02fa")
 
 (set-face-attribute 'widget-single-line-field nil :background "darkgray")
-
+(set-frame-font "JetBrainsMono Nerd Font Mono 16" nil t) ;; Set font
 
