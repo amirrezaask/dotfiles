50dae2cf472b8274dee8c550ede410f3678d074c
amirrezaask <raskarpour@gmail.com>
2022-08-14 13:20:50 +0430

    remove emacs early init

--- emacs/early-init.el
+++
@@ -1,4 +0,0 @@
-(tool-bar-mode 0) ;; disable top toolbar
-(scroll-bar-mode 0) ;; disable scroll bar
-(menu-bar-mode -1) ;; Disable menu bar
-(setq package-enable-at-startup nil) ;; disable emacs default package manager
