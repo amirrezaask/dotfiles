b097aca2a76ea4db2bac4fc8c5e6bc2b4f67f2de
amirrezaask <raskarpour@gmail.com>
2022-10-30 21:17:15 +0330

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,13 @@
 -- Install packages
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
     use 'eemed/sitruuna.vim'
     use 'sainnhe/sonokai'
+    use 'overcache/NeoSolarized'
     use 'folke/tokyonight.nvim'
     use 'rcticicestudio/nord-vim'
+
     use 'windwp/nvim-spectre'                         -- Search/Replace project wide
     use 'nvim-treesitter/nvim-treesitter'             -- Treesitter syntax highlighting
     use 'nvim-treesitter/nvim-treesitter-textobjects' -- Treesitter text objects
@@ -96,7 +99,7 @@ if vim.version().major >= 0 and vim.version().minor >= 8 then
 end
 
 -- Colorscheme
-vim.cmd [[ colorscheme sonokai ]]
+vim.cmd [[ colorscheme NeoSolarized ]]
 
 vim.g.netrw_banner = false
 vim.g.netrw_winsize = 25
