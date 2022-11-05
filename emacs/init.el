828b5fc38e2faf713b9be1f9417af1eea566a823
amirrezaask <raskarpour@gmail.com>
2022-11-06 00:55:56 +0330

    moving to README.org file

--- emacs/init.el
+++ emacs/init.el
@@ -1,37 +1,3 @@
-(tool-bar-mode 0) ;; disable top toolbar
-(scroll-bar-mode 0) ;; disable scroll bar
-(menu-bar-mode -1) ;; Disable menu bar
-(setq package-enable-at-startup nil) ;; disable emacs default package manager
-
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory)) ;; add my scripts to load path
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(setq backup-by-copying t) ;; Always copy files for backup.
-(setq version-control t) ;; Use version numbers for backup.
-(setq delete-old-versions t) ;; Delete old backup of files.
-(setq kept-new-versions 6) ;; Number of newest versions to keep.
-(setq kept-old-versions 2) ;; Number of old versions to keep.
-(setq create-lockfiles nil) ;; Don't create .# files as lock.
-
-(delete-selection-mode 1)
-
-(blink-cursor-mode -1)
-
-(setq backup-directory-alist ;; all backups should go here (PATTERN . LOCATION)
-      '(("." . "~/.emacs.d/backup")))
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-(setq-default indent-tabs-mode nil ;; Don't insert tabs for indentation.
-              tab-width 4) ;; Width of the TAB character in display.
-
-(setq-default cursor-type 'box)
-
-
-(defalias 'yes-or-no-p 'y-or-n-p) ;; Show y or n instead of yes or no for question prompts.
 
 (global-set-key (kbd "M-p") (lambda () (interactive) (previous-line 10)))
 (global-set-key (kbd "M-n") (lambda () (interactive) (next-line 10)))
@@ -42,36 +8,10 @@
 (setq inhibit-splash-screen 0) ;; Disable Emacs start screen.
 (setq ring-bell-function 'ignore) ;; No bell ringing.
 
-(set-terminal-coding-system 'utf-8) ;; default emacs encodings
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
 
 (setq-default fill-column 80) ;; column number which emacs start to line wrap.
 
-(setq scroll-step 5) ;; When point moves out of screen, number of lines to scroll
-(setq scroll-margin 5) ;; Scroll margin lines, when point arrives at these margins scroll the display.
-(setq scroll-conservatively 101) ;; Number of lines to scroll to bring point back into view.
-(setq scroll-up-aggressively 0.11) ;; When scrolling how much to move the view.
-(setq scroll-down-aggressively 0.01) ;; Same as above.
-(setq auto-window-vscroll nil) ;; Disable changing window-vscroll automatically.
-(setq fast-but-imprecise-scrolling nil) ;; Disable fast scroll since it does not feel good.
-(setq mouse-wheel-scroll-amount '(5
-                                  ((shift) . 10)))
-(setq mouse-wheel-progressive-speed t)
-
-;; Horizontal Scroll
-(setq hscroll-step 1) ;; Number of columns to scroll when point is to close to edge.
-(setq hscroll-margin 1) ;; How many columns away from edge to start scrolling.
-
-(global-display-line-numbers-mode 1) ;; Ensure line numbers globally.
-
 (setq kill-ring-max 15) ;; Capacity of kill-ring.
-
-(show-paren-mode 1) ;; Highlight matching parens
-
-(setq show-paren-delay 0) ;; highlight matching parens instantly.
-
-(when (> emacs-major-version 26) (global-tab-line-mode -1)) ;; Disable tab line in Emacs 27+.
 
 (unless (eq system-type 'windows-nt)
   (package-install 'exec-path-from-shell)
@@ -79,21 +19,6 @@
   (exec-path-from-shell-copy-envs '("GOPROXY" "GOPRIVATE"))
   (exec-path-from-shell-initialize)
 )
-;; Highlight indents for yaml
-(package-install 'highlight-indent-guides)
-(add-hook 'yaml-mode-hook-hook #'highlight-indent-guides)
-(add-hook 'focus-in-hook #'highlight-indent-guides-auto-set-faces)
-(setq highlight-indent-guides-method 'character)
-
-;; VTerm
-
-(when (not (eq 'system-type 'windows-nt))
-  (package-install 'vterm))
-
-;; Wdired
-(add-hook 'dired-mode-hook (lambda ()
-                             (define-key dired-mode-map (kbd "C-c C-e") 'wdired-change-to-wdired-mode)
-                             ))
 
 ;; Completion
 (package-install 'vertico)
@@ -259,9 +184,11 @@
 (package-install 'ace-window)
 (global-set-key (kbd "C-x o") 'ace-select-window)
 
+
+
+;; UI stuff
 (package-install 'ef-themes)
 (package-install 'gruber-darker-theme)
-;; UI stuff
 
 (set-face-attribute 'default nil :foreground "#d3b58d" :background "#072626")
 (set-face-attribute 'cursor nil :background "green")
