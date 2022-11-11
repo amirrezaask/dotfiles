6d54647afb5b6feeaa6aca2730e289b2e7cfd108
amirrezaask <raskarpour@gmail.com>
2022-11-12 00:03:09 +0330

    ui, basics modules

---
+++ nvim/lua/modules/ui/plugins.lua
@@ -0,0 +1,8 @@
+local use = require"core.plugin_manager".use
+
+use 'eemed/sitruuna.vim'
+use 'sainnhe/sonokai'
+use 'folke/tokyonight.nvim'
+use 'shaunsingh/solarized.nvim'
+use 'shaunsingh/nord.nvim'
+use 'overcache/NeoSolarized'
