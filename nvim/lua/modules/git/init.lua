9c741c32cffdb2d273f74637f64b13dfed29d8f6
amirrezaask <raskarpour@gmail.com>
2022-11-12 01:49:04 +0330

    Adds Neogit

--- nvim/lua/modules/git/init.lua
+++ nvim/lua/modules/git/init.lua
@@ -1,1 +1,2 @@
 require('gitsigns').setup()
+require"neogit".setup()
--- nvim/lua/modules/git/plugins.lua
+++ nvim/lua/modules/git/plugins.lua
@@ -4,3 +4,4 @@ use 'tpope/vim-fugitive'
 use 'junegunn/gv.vim'
 use 'cohama/agit.vim'
 use "lewis6991/gitsigns.nvim"
+use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
