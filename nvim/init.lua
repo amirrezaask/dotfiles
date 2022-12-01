22e829c462e4d47cfefb16d5b5f4c2ff82aa0fa7
amirrezaask <raskarpour@gmail.com>
2022-12-01 09:28:57 +0330

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,3 @@
--- Jasvim
--- Core libraries and helpers are in $HOME/.config/nvim/lua/core/*.lua
--- Configurations for all plugins reside in $HOME/.config/nvim/lua/plugins/<plugin name>.lua
--- Language configurations are in $HOME/.config/nvim/lua/langs/<language name>.lua
--- All configurations will load automatically and there is no need to source your stuff in your init.lua
--- Also some plugins are grouped under one group name forexample all git related plugins are in git.lua
--- _G in lua is global namespace, we use it for less linter errors but in lua anything without `local` keyword is global.
-
 -- Core functionalities and utilities
 require "core"
 
