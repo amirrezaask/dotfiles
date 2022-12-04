2fadc8713377f99a59b0d2942339ac13f26bded2
amirrezaask <raskarpour@gmail.com>
2022-12-04 06:33:38 +0330

    updates

--- nvim/lua/amirrezaask/lsp.lua
+++ nvim/lua/amirrezaask/lsp.lua
@@ -2,6 +2,7 @@ local keymaps = require "amirrezaask.keymaps"
 
 local lsp = {}
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = "rounded"
