f8aa6f3fbf5a02f82e3acd3c160afe6673b13f38
amirrezaask <raskarpour@gmail.com>
2022-12-11 01:09:43 +0330

    automated nvim config loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@
 -- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 --
 --
--- read :h runtimepath to know how these files get sourced but in summary
+-- Read :h runtimepath to know how these files get sourced but in summary
 
 -- 1. init.lua => nothing :)
 -- 2. plugin/*.lua or *.vim => basics + plugin spec
