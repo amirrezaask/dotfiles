be45faaea75dc211b8a7594a2807db4110a23e6c
amirrezaask <raskarpour@gmail.com>
2022-12-10 22:57:13 +0330

    move plugin/packer.lua -> lua/user/packer.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,12 @@
 --
 -- read :h runtimepath to know how these files get sourced but in summary
 -- first init.lua gets sourced by neovim
--- then all plugin/* files get sourced
+-- which will install packer if needed and
+-- also registers all plugins to be installed.
 -- then all after/plugin/* files get sourced
 -- all files under lua/* are just there and need to be sourced manually
+
+-- Some global functions and helpers.
 require "core"
+-- Packer.nvim installation and plugin registeration.
+require "user.packer"
