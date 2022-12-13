e601321dd13e4cd1e5376e9493aed53120edf68d
amirrezaask <raskarpour@gmail.com>
2022-12-13 03:44:50 +0330

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ vim.g.catppuccin_flavour = "macchiato"
 vim.g.gruvbox_contrast_dark = "hard"
 local tokyonight_style = "night"
 
-local colorscheme = "catppuccin"
+local colorscheme = "rose-pine"
 
 local transparent = false
 
