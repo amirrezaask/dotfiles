94c53f112b254fa0fcb6a1a30c1e6bc303e94881
amirrezaask <raskarpour@gmail.com>
2022-12-16 20:36:19 +0330

    One file is better

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,4 @@
 (setq amirreza/font "FiraCode Nerd Font Mono")
-;; (setq amirreza/font "OperatorMono Nerd Font")
-;; (setq amirreza/font "JetBrainsMono Nerd Font Mono")
 
 (setq amirreza/font-size "21")
 
@@ -8,5 +6,62 @@
 (setq amirreza/light-theme 'ef-day)
 
 
+;; Setup package manager.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+
+
+
+(use-package emacs
+  :config
+  (setq backup-by-copying t) ;; Always copy files for backup.
+  (setq version-control t) ;; Use version numbers for backup.
+  (setq delete-old-versions t) ;; Delete old backup of files.
+  (setq kept-new-versions 6) ;; Number of newest versions to keep.
+  (setq kept-old-versions 2) ;; Number of old versions to keep.
+  (setq create-lockfiles nil) ;; Don't create .# files as lock.
+  (setq backup-directory-alist ;; all backups should go here (PATTERN . LOCATION)
+	'(("." . "~/.emacs.d/backup")))
+  (delete-selection-mode 1) ;; When a region of text is selected and then something is typed remove text and replace with what has been typed.
+  (show-paren-mode 1) ;; Highlight matching parens
+  (setq show-paren-delay 0) ;; highlight matching parens instantly.
+  (global-display-line-numbers-mode 1)
+  (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+)
+
+;; Orderless completion matching algorithm
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+
+;; Buffer management UI
+(use-package bufler
+  :bind
+  ("C-x b" . bufler))
+
+;; Better window management facilities
+(use-package ace-window
+  :bind
+  ("C-x o" . ace-window))
+
 ;; Load all configurations
 (load-file (expand-file-name "loader.el" (expand-file-name "lisp" user-emacs-directory)))
+
+
--- .emacs.d/lisp/basics.el
+++ .emacs.d/lisp/basics.el
@@ -1,35 +1,4 @@
-(setq backup-by-copying t) ;; Always copy files for backup.
-(setq version-control t) ;; Use version numbers for backup.
-(setq delete-old-versions t) ;; Delete old backup of files.
-(setq kept-new-versions 6) ;; Number of newest versions to keep.
-(setq kept-old-versions 2) ;; Number of old versions to keep.
-(setq create-lockfiles nil) ;; Don't create .# files as lock.
-
-(setq backup-directory-alist ;; all backups should go here (PATTERN . LOCATION)
-      '(("." . "~/.emacs.d/backup")))
-
-(delete-selection-mode 1) ;; 
-
-(show-paren-mode 1) ;; Highlight matching parens
-
-(setq show-paren-delay 0) ;; highlight matching parens instantly.
-
-(global-display-line-numbers-mode 1)
 
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-
-(straight-use-package 'bufler)
-(global-set-key (kbd "C-x b") 'bufler)
-
-(straight-use-package 'ace-window)
-(global-set-key (kbd "C-x o") 'ace-window)
 
 (provide 'basics)
