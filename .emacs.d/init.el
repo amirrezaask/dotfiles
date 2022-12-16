cbb40503c044e9a4da266a2b41a71eebbe2b1915
amirrezaask <raskarpour@gmail.com>
2022-12-16 20:59:06 +0330

    less is more

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+(setq package-enable-at-startup nil)
+
+(tool-bar-mode 0) ;; disable top toolbar
+(scroll-bar-mode 0) ;; disable scroll bar
+(menu-bar-mode -1) ;; Disable menu bar
+
 (setq amirreza/font "FiraCode Nerd Font Mono")
 
 (setq amirreza/font-size "21")
