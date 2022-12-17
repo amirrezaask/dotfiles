358c7d3684b14174d2cfd6da11a229edb6268693
amirrezaask <raskarpour@gmail.com>
2022-12-17 23:17:26 +0330

    Simpler :Gp command for Git push

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -224,6 +224,9 @@ use {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    vim.api.nvim_create_user_command("Gp", function()
+      vim.cmd.Git "push"
+    end, {})
   end,
 }
 
