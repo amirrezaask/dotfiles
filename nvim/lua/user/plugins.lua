3871a3cb29508be6cabaede0fc5374607cf7a750
amirrezaask <raskarpour@gmail.com>
2022-12-17 23:15:44 +0330

    git signs update

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -208,7 +208,15 @@ use {
 use {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup {}
+    require("gitsigns").setup {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    }
   end,
 }
 
