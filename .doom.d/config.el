6def7d6e45721a9dba52a95040bd9384755d6ac0
amirrezaask <raskarpour@gmail.com>
2022-12-17 06:13:22 +0330

    doom update

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,10 +2,28 @@
 (setq user-full-name "Amirreza Askarpour"
       user-mail-address "raskarpour@gmail.com")
 
-(setq doom-font (font-spec :family "Fira Code" :size 20 :weight 'semi-light)
+;; (setq doom-font (font-spec :family "Fira Code" :size 20 :weight 'semi-light)
+     ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 20 :weight 'semi-light)
      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
 (setq evil-escape-unordered-key-sequence t)
+
+(evil-global-set-key 'normal (kbd "<C-d>") (lambda
+					     ()
+					     (interactive)
+					     (evil-scroll-down)
+					     (evil-scroll-line-to-center)
+					     ))
+
+(evil-global-set-key 'normal (kbd "<C-u>") (lambda
+                                            ()
+                                            (interactive)
+                                            (evil-scroll-up)
+                                            (evil-scroll-line-to-center)
+                                            ))
+
 
 (setq doom-theme 'doom-one)
 
