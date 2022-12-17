e90e351f02515b1f4441146b32f9921fbf249e54
amirrezaask <raskarpour@gmail.com>
2022-12-18 00:23:47 +0330

    updates

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require "lsp-zero"
 lsp.preset "recommended"
 
 lsp.set_preferences {
-  suggest_lsp_servers = true,
+  suggest_lsp_servers = false,
   sign_icons = {
     error = "E",
     warn = "W",
