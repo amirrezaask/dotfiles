0bcbc288711b8d52a253635bb34f22682beac7aa
amirrezaask <raskarpour@gmail.com>
2022-12-19 05:24:32 +0330

    literate config, some cleaning

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,4 +2,6 @@
 (tool-bar-mode 0) ;; disable top toolbar
 (scroll-bar-mode 0) ;; disable scroll bar
 (menu-bar-mode -1) ;; Disable menu bar
+(setq gc-cons-threshold (* 100 1024 1024))
+(setq read-process-output-max (* 1024 1024))
 
