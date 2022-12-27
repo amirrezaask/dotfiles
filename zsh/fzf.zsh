25d9669c90bcabd5975bb904c921784d6ab95bf5
amirrezaask <raskarpour@gmail.com>
2022-12-27 05:00:55 +0330

    Adds zsh configuration

--- zsh/fzf.zsh
+++
@@ -1,7 +0,0 @@
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/Users/amirreza/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/Users/amirreza/.fzf/shell/key-bindings.zsh"
