68e4b1530e7026eda75cd6eaaf0ba2f0fde1aeb7
amirrezaask <raskarpour@gmail.com>
2022-12-31 08:50:09 +0330

    remove nvim and tmux :(

--- nvim/after/plugin/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-require("tokyonight").setup {
-  transparent = _G.transparent,
-}
-vim.keymap.set("n", "<leader>tt", function()
-  vim.cmd.Telescope "colorscheme"
-end)
-pcall(vim.cmd.colorscheme, "tokyonight-night")
--- nvim/after/plugin/fugitive.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-vim.api.nvim_create_user_command("Gp", function()
-  vim.cmd.Git "push"
-end, {})
