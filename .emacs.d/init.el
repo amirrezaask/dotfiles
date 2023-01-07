b6f181c9a8a074e2f4c810dd250857eb785585e1
amirrezaask <raskarpour@gmail.com>
2023-01-07 06:49:46 +0330

    mostly editor experience

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,13 +60,18 @@
 ;; All packages I install
 (setq amirreza/packages
  '(
-   ;; Basic stuff
+   ;; Core
    hydra
    helpful
    gcmh
-   expand-region
    exec-path-from-shell
+   which-key
 
+   ;; Editing
+   multiple-cursors
+   expand-region
+   parinfer-rust-mode
+   
    ;; Window and buffer management
    ace-window
    bufler
@@ -91,6 +96,9 @@
    solarized-theme
    doom-themes
    gruber-darker-theme
+   cyberpunk-theme
+   gruvbox-theme
+   exotica-theme
 
    ;; Autocompletion menu
    (corfu :type git :host github :repo "emacs-straight/corfu" :files ("*" "extensions/*.el" (:exclude ".git")))
@@ -131,6 +139,8 @@
    php-mode ;; PHP
    psysh ;; PHP repl in Emacs
    composer ;; Composer
+
+   racket-mode ;; Racket LISP
 
    apache-mode ;; Apache config syntax
    cmake-mode ;; CMake
@@ -308,6 +318,12 @@
 (show-paren-mode 1) ;; Highlight matching parens
 (setq display-line-numbers-type 'relative) ;; relative line numbers
 (global-display-line-numbers-mode 1) ;; enable line numbers globaly
+
+(add-hook 'prog-mode-hook 'hs-minor-mode) ;; Enable HideShow minor mode for all programming mdoes.
+(global-set-key (kbd "C-'") 'hs-toggle-hiding) ;; toggle Fold
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this) ;; Insert cursor next
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this) ;; Insert cursor prev
 
 (defun amirreza/up-center ()
   "Move half a page up and also center the cursor."
