51a8f15f008ee7392eac930e9dfd7f7ca0a400f9
amirrezaask <raskarpour@gmail.com>
2023-01-10 18:37:21 +0330

    updates

--- .doom.d/config.el
+++
@@ -1,8 +0,0 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-(setq user-full-name "Amirreza Askarpour"
-      user-mail-address "raskarpour@gmail.com")
-(setq doom-font (font-spec :family "Source Code Pro" :size 20))
-(setq doom-theme 'doom-one)
-(setq display-line-numbers-type 'relative)
-(setq org-directory "~/org/")
-(setq evil-escape-unordered-key-sequence t)
--- .doom.d/packages.el
+++
@@ -1,2 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;(package! some-package)
