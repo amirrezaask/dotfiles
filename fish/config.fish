ff1d5bea778ec805b37a34cea197e666216dcc08
amirrezaask <raskarpour@gmail.com>
2023-04-24 04:41:49 +0330

    updates

---
+++ fish/config.fish
@@ -0,0 +1,45 @@
+set fish_greeting "" # no start greetings
+
+set -gx GO111MODULE 'on'
+set -gx GOPATH "$HOME"
+set -gx GOPRIVATE "gitlab.snapp.ir"
+set -gx GOPROXY 'goproxy.io,direct'
+
+set -gx PATH "/opt/homebrew/bin:$PATH"
+
+set -gx PATH "$HOME/.local/bin:$PATH"
+
+set -gx PATH "$GOROOT/bin:$PATH"
+
+set -gx PATH "$HOME/.config/composer/vendor/bin:$PATH"
+
+set -gx PATH "$GOPATH/bin:$PATH"
+
+set -gx PATH "$HOME/.cargo/bin:$PATH"
+
+eval $(brew shellenv)
+
+set -gx EDITOR 'vim'
+set -gx HOMEBREW_NO_AUTO_UPDATE '1'
+
+set -gx FZF_DEFAULT_OPTS '--height 20%'
+set -gx FZF_DEFAULT_COMMAND 'rg --files'
+
+alias snappvpn='sudo openfortivpn -c ~/snappDC.conf'
+alias gs='git status'
+alias gd='git diff'
+alias gpsup='git push --set-upstream origin $(git_current_branch)'
+
+# git prompt settings
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_dirtystate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_conflictedstate "+"
+set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_cleanstate green --bold
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_branch cyan --dim --italics
--- nvim/init.lua
+++ nvim/init.lua
@@ -233,6 +233,9 @@ require("gruvbox").setup {
   },
 }
 pcall(vim.cmd.colorscheme, "gruvbox")
+-- Transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- ==========================================================================
 -- ========================= Keybindings ====================================
 -- ==========================================================================
