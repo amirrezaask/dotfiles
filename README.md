0e92f63c13e37fa0c4e85c0b6b148ce8b75a98d3
amirrezaask <raskarpour@gmail.com>
2023-05-27 16:58:37 +0330

    updates

---
+++ README.md
@@ -0,0 +1,7 @@
+# AmirrezaAsk Computing Environment
+
+## Screenshots
+![](https://raw.githubusercontent.com/amirrezaask/dotfiles/master/screenshots/screenshot1.png)
+![](https://raw.githubusercontent.com/amirrezaask/dotfiles/master/screenshots/screenshot2.png)
+![](https://raw.githubusercontent.com/amirrezaask/dotfiles/master/screenshots/screenshot3.png)
+![](https://raw.githubusercontent.com/amirrezaask/dotfiles/master/screenshots/screenshot4.png)
---
+++ init.lua
@@ -0,0 +1,462 @@
+----------------------------------------------------------
+---                     Options                         --
+----------------------------------------------------------
+vim.opt.number = true         -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.errorbells = false
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append "@-@"
+vim.opt.updatetime = 50
+vim.opt.guicursor = ""       -- Don't style cursor in different modes, just a box would suffice
+vim.opt.shortmess:append "c" -- Don't pass messages to |ins-completion-menu|.
+vim.opt.shortmess:append "I" -- No Intro message
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.cursorline = false
+vim.opt.sw = 4
+vim.opt.ts = 4
+vim.opt.expandtab = true
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.opt.laststatus = 2
+vim.opt.timeoutlen = 300
+vim.opt.statusline = "%y%=%m%r%h%w%q%F%=L:%l C:%c"
+vim.opt.laststatus = 3
+vim.g.mapleader = " "
+----------------------------------------------------------
+---                     Basic Keymaps                   --
+----------------------------------------------------------
+-- Copy/paste improvements
+vim.keymap.set("n", "Y", "y$", { desc = "Copy line" })                               -- Make yanking act like other operations
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy into clipboard" }) -- Copy to clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy line into clipboard" })
+vim.keymap.set("n", "<leader>p", [["+p]], { desc = "Paste from clipboard" })
+-- Split windows
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>h", "<cmd>split<CR>", { desc = "Split horizontaly" })
+vim.keymap.set({ "n", "i" }, "<C-l>", "<cmd>wincmd l<CR>", { desc = "Move to split right" })
+vim.keymap.set({ "n", "i" }, "<C-h>", "<cmd>wincmd h<CR>", { desc = "Move to split left" })
+vim.keymap.set("n", "<Left>", "<cmd>vertical resize -10<CR>")
+vim.keymap.set("n", "<Right>", "<cmd>vertical resize +10<CR>")
+vim.keymap.set("n", "<C-w>=", "<cmd>wincmd =<CR>")
+-- Tabs
+vim.keymap.set("n", "<C-,>", ':tabp<cr>', { desc = "Previous Tab" })
+vim.keymap.set("n", "<C-.>", ':tabn<cr>', { desc = "Next Tab" })
+vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { desc = "New Tab" })
+-- Simpler exiting insert mode
+vim.keymap.set({ "i" }, "<C-c>", "<esc>")
+vim.keymap.set({ "t" }, "<C-c>", "<C-\\><C-n>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
+-- Quickfix list
+vim.keymap.set({ "n" }, "<C-k>", "<cmd>cprev<CR>", { desc = "Previous quick fix list item" })
+vim.keymap.set({ "n" }, "<C-j>", "<cmd>cnext<CR>", { desc = "Next quick fix list item" })
+-- When moving around always have pointer centered in screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+-- Move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "Q", "<NOP>")
+vim.keymap.set("n", "<CR>", [[ {-> v:hlsearch ? ':nohl<CR>' : '<CR>'}() ]], { expr = true }) -- handy when doing search in a buffer
+
+-- Netrw
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
+----------------------------------------------------------
+---                     Plugins                         --
+----------------------------------------------------------
+-- Installing lazy package manager
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+
+TRANSPARENT = true
+-- Installing and configuring plugins
+require "lazy".setup {
+    {
+        "amirrezaask/themes",
+        {
+            "ellisonleao/gruvbox.nvim", -- Best theme of all time
+            config = function()
+                require("gruvbox").setup {
+                    transparent_mode = TRANSPARENT,
+                    contrast = "hard",
+                    italic = {
+                        strings = false,
+                        comments = false,
+                        operators = false,
+                        folds = false,
+                    },
+                }
+            end,
+        },
+        {
+            "bluz71/vim-nightfly-colors",
+            config = function() vim.nightflyTransparent = true end,
+        },
+        "shaunsingh/nord.nvim",
+        "amirrezaask/sitruuna.vim",
+        "shaunsingh/oxocarbon.nvim",
+        {
+            "rebelot/kanagawa.nvim",
+            config = function()
+                require("kanagawa").setup {
+                    transparent = TRANSPARENT,
+                }
+            end,
+        },
+        {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            config = function()
+                require("catppuccin").setup {
+                    transparent_background = TRANSPARENT,
+                }
+            end,
+        },
+        {
+            "rose-pine/neovim",
+            name = "rose-pine",
+            config = function() require("rose-pine").setup { disable_background = TRANSPARENT } end,
+        },
+        {
+            "folke/tokyonight.nvim",
+            config = function() require("tokyonight").setup { transparent = TRANSPARENT } end,
+        }, -- folkkkkkeeeeee
+    },
+
+    {
+        "fatih/vim-go",
+        config = function()
+        end
+    },
+
+
+    -- Treesitter syntax highlighting and text objects.
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "nvim-treesitter/playground" },
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                -- Setup treesitter text objects module + highlight
+                ensure_installed = { "json", "yaml", "c", "cpp", "lua", "rust", "go", "python", "php" },
+                context_commentstring = { enable = true },
+                highlight = { enable = true, additional_vim_regex_highlighting = false },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                    },
+                },
+            }
+            -- Install all treesitter parsers.
+            pcall(require("nvim-treesitter.install").update { with_sync = true })
+        end,
+    },
+
+
+    "kevinhwang91/nvim-bqf", -- Preview quickfix list item.
+    "tpope/vim-surround",    -- surrounding text objects
+    "windwp/nvim-autopairs", -- Auto insert pairs like () [] {}
+    {
+        "folke/zen-mode.nvim",
+        config = function() require("zen-mode").setup() end,
+    },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                window = {
+                    border = "single",
+                },
+            }
+        end,
+    }, -- Cheat your way through keymapings
+    {
+        "numToStr/Comment.nvim",
+        config = function() require("Comment").setup() end,
+    },
+
+    "fladson/vim-kitty", -- Support Kitty terminal config syntax
+    "towolf/vim-helm",   -- Support for helm template syntax
+    "jansedivy/jai.vim", -- Jai from Jonathan Blow
+    "dag/vim-fish",      -- Vim fish syntax
+    "tpope/vim-sleuth",  -- Heuristically set buffer options
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup {
+                signs = {
+                    add = { text = "+" },
+                    change = { text = "~" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
+                    changedelete = { text = "~" },
+                },
+            }
+            vim.keymap.set("n", "<leader>b", function() require("gitsigns").blame_line { full = true } end,
+                { desc = "Git blame line" })
+            vim.keymap.set("n", "<leader>d", function() require("gitsigns").diffthis "~" end,
+                { desc = "Diff current file with HEAD" })
+        end,
+    }, -- Signs next to line numbers to show git status of a line
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.api.nvim_create_user_command("Gp", function() vim.cmd.Git "push" end, {})
+
+            vim.keymap.set("n", "<leader>P", function() vim.cmd.Git "push" end, { desc = "Git Push" })
+            vim.keymap.set("n", "<leader>g", vim.cmd.Git, { desc = "Git status" })
+        end,
+    }, -- Second best Git client ( first one is emacs magit )
+
+    {
+        "akinsho/git-conflict.nvim",
+        version = "*",
+        config = function() require("git-conflict").setup {} end,
+    },
+    -- Autocompletion popup
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            -- Autocompletion menu using nvim-cmp
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+            local cmp = require "cmp"
+            cmp.setup {
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                snippet = {
+                    expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
+                },
+                mapping = cmp.mapping.preset.insert {
+                    ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            }
+        end,
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+        },
+    },
+    {
+        -- LSP + Mason + Null-ls: Neovim builtin LSP client + Mason for installing langauge servers + Null-Ls to integrate non LSP tools into LSP client
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            -- TODO(amirreza): find a better more cross platform way of joining paths.
+            vim.env.PATH = string.format("%s/mason/bin", vim.fn.stdpath "data") .. vim.env.PATH
+            require("mason").setup {}
+            local lsp_servers = {
+                gopls = {},
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            telemetry = { enable = false },
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                            workspace = {
+                                checkThirdParty = false,
+                                library = vim.api.nvim_get_runtime_file("", true),
+                            },
+                        },
+                    },
+                },
+                rust_analyzer = {},
+                zls = {},
+            }
+            for server, config in pairs(lsp_servers) do
+                require("lspconfig")[server].setup(config)
+            end
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufnr = args.buf
+                    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+                    local buffer = function(desc) return { buffer = bufnr, desc = desc } end
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, buffer "Goto Definition")
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, buffer "Goto Declaration")
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, buffer "Goto Implementation")
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, buffer "Goto References")
+                    vim.keymap.set("n", "R", vim.lsp.buf.rename, buffer "Rename")
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, buffer "Hover")
+                    vim.keymap.set("n", "gl", vim.diagnostic.open_float, buffer "")
+                    vim.keymap.set("n", "[[", vim.diagnostic.goto_prev, buffer "Next Diagnostic")
+                    vim.keymap.set("n", "]]", vim.diagnostic.goto_next, buffer "Previous Diagnostic")
+                    vim.keymap.set("n", "C", vim.lsp.buf.code_action, buffer "Code Actions")
+                    vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, buffer "Signature Help")
+                    vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, buffer "Signature Help")
+                end,
+            })
+            -- Hover and signature help windows have rounded borders
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
+                { border = "rounded" })
+
+            -- LspInfo window have rounded border
+            require("lspconfig.ui.windows").default_options.border = "rounded"
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = { "*.rs", "*.lua" },
+                callback = function(_) vim.lsp.buf.format() end,
+            })
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            {
+                "rcarriga/nvim-dap-ui",
+                config = function()
+                    require "dapui".setup()
+                    vim.keymap.set("n", "<leader>du", ":lua require'dapui'.toggle()<CR>")
+                end
+            },
+            {
+                "leoluz/nvim-dap-go",
+                config = function()
+                    require('dap-go').setup()
+                end
+            },
+            {
+                'theHamsta/nvim-dap-virtual-text',
+                config = function()
+                    require("nvim-dap-virtual-text").setup {}
+                end
+            }
+        },
+        config = function()
+            vim.keymap.set("n", "<F5>", function()
+                -- require "dapui".open()
+                require "dap".continue()
+            end)
+            vim.keymap.set("n", "<F8>", ":lua require'dap'.step_over()<CR>")
+            vim.keymap.set("n", "<F7>", ":lua require'dap'.step_into()<CR>")
+            vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+            vim.keymap.set("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<CR>")
+            vim.keymap.set("n", "<leader>dB",
+                ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+            vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+
+            local dap, dapui = require("dap"), require("dapui")
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+
+    "stevearc/oil.nvim",  -- File manager like a BOSS
+    "pbrisbin/vim-mkdir", -- Automatically create directory if not exists
+    "tpope/vim-eunuch",   -- Helper commands like :Rename, :Move, :Delete, :Remove, ...
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup {
+                renderer = {
+                    icons = {
+                        show = {
+                            file = false,
+                            folder = false,
+                            folder_arrow = false,
+                            git = false,
+                            modified = false,
+                        },
+                    },
+                },
+            }
+
+            vim.keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>")
+        end,
+    }, -- Tree file explorer
+    {
+        -- telescope: Fuzzy finding and searching interface
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-telescope/telescope-ui-select.nvim",
+        },
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {},
+                    },
+                },
+            }                                               -- Best fuzzy finder
+            require("telescope").load_extension "fzf"       -- load fzf awesomnes into Telescope
+            require("telescope").load_extension "ui-select" -- Use telescope for vim.ui.select
+            local no_preview = { previewer = false, layout_config = { height = 0.6, width = 0.9 } }
+            -- local dropdown = require("telescope.themes").get_dropdown
+            -- local ivy = require("telescope.themes").get_ivy
+            local theme = function(opts) return opts end
+            local telescope_builtin = require "telescope.builtin"
+            vim.keymap.set("n", "<C-p>", function() telescope_builtin.git_files(theme(no_preview)) end,
+                { desc = "Telescope Git Files" })
+            vim.keymap.set("n", "<leader><leader>", function() telescope_builtin.find_files(theme(no_preview)) end,
+                { desc = "Telescope Find files" })
+            vim.keymap.set("n", ",,", function() telescope_builtin.current_buffer_fuzzy_find(theme(no_preview)) end,
+                { desc = "Current File Search" })
+            vim.keymap.set("n", "&", function() telescope_builtin.grep_string(theme(no_preview)) end,
+                { desc = "Grep for word at point" })
+            vim.keymap.set(
+                "n",
+                "<leader>o",
+                function() telescope_builtin.treesitter(theme(no_preview)) end,
+                { desc = "Search Symbols In Current File" }
+            )
+            vim.keymap.set("n", "??", function() telescope_builtin.live_grep(theme(no_preview)) end,
+                { desc = "Live Grep" })
+        end,
+    }
+}
+
+
+-- Setting the colorscheme
+vim.cmd.colorscheme "gruvbox"
