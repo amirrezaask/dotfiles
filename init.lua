543dc0dd6d3f39d6e36cd93809d3b5a3f9f4e484
amirrezaask <raskarpour@gmail.com>
2023-05-29 03:20:14 +0330

    remove fzf keybindings

--- init.lua
+++ init.lua
@@ -493,12 +493,10 @@ require "lazy".setup {
     { 'junegunn/fzf.vim',
         dependencies = { 'junegunn/fzf', build = ":execute fzf#install()" },
         config = function()
-            vim.keymap.set("n", "<leader><leader>", ":Files<CR>")
+            vim.g.fzf_preview_window = ''
         end
 
     }
-
-
 }
 
 
