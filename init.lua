b067b21416879134c1894a4d450c5dc23f7bf220
amirrezaask <raskarpour@gmail.com>
2023-06-30 03:32:16 +0330

    updates

--- init.lua
+++ init.lua
@@ -97,54 +97,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 TRANSPARENT = true
+COLORSCHEME = 'tokyonight-night'
 -- Installing and configuring plugins
 require "lazy".setup {
-    {
-
-        -- Colorscheme
-        {
-            "rose-pine/neovim",
-            name = "rose-pine",
-            config = function() require("rose-pine").setup { disable_background = TRANSPARENT } end,
-        },
-    },
-
-    {
-        "fatih/vim-go",
-        config = function()
-            vim.g['go_gopls_enabled'] = 0
-            vim.g['go_code_completion_enabled'] = 0
-            vim.g['go_fmt_autosave'] = 0
-            vim.g['go_imports_autosave'] = 0
-            vim.g['go_mod_fmt_autosave'] = 0
-            vim.g['go_doc_keywordprg_enabled'] = 0
-            vim.g['go_def_mapping_enabled'] = 0
-            vim.g['go_textobj_enabled'] = 0
-            vim.g['go_list_type'] = 'quickfix'
-            vim.g['go_template_autocreate'] = false
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = { "*.go" },
-                callback = function()
-                    local params = vim.lsp.util.make_range_params()
-                    params.context = { only = { "source.organizeImports" } }
-                    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
-                    for _, res in pairs(result or {}) do
-                        for _, r in pairs(res.result or {}) do
-                            if r.edit then
-                                vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
-                            else
-                                vim.lsp.buf.execute_command(r.command)
-                            end
-                        end
-                    end
-
-                    vim.lsp.buf.format()
-                end
-            })
-        end
-    },
-
-
+    -- Colorscheme
+    { "rose-pine/neovim",                name = "rose-pine" },
+    { "folke/tokyonight.nvim" },
+    { "ellisonleao/gruvbox.nvim" },
+    { 'nyoom-engineering/oxocarbon.nvim' },
+    { 'luisiacc/gruvbox-baby' },
     -- Treesitter syntax highlighting and text objects.
     {
         "nvim-treesitter/nvim-treesitter",
@@ -191,16 +152,6 @@ require "lazy".setup {
     "tpope/vim-surround",    -- surrounding text objects
     "tpope/vim-abolish",     -- useful text stuff
     "windwp/nvim-autopairs", -- Auto insert pairs like () [] {}
-    {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup {
-                window = {
-                    border = "single",
-                },
-            }
-        end,
-    }, -- Cheat your way through keymapings
     {
         "numToStr/Comment.nvim",
         config = function() require("Comment").setup() end,
@@ -451,8 +402,6 @@ require "lazy".setup {
             require("telescope").load_extension "fzf"       -- load fzf awesomnes into Telescope
             require("telescope").load_extension "ui-select" -- Use telescope for vim.ui.select
             local no_preview = { previewer = false, layout_config = { height = 0.6, width = 0.9 } }
-            -- local dropdown = require("telescope.themes").get_dropdown
-            -- local ivy = require("telescope.themes").get_ivy
             local theme = function(opts) return opts end
             local telescope_builtin = require "telescope.builtin"
             vim.keymap.set("n", "<C-p>", function() telescope_builtin.git_files(theme(no_preview)) end,
@@ -497,4 +446,8 @@ require "lazy".setup {
 }
 
 -- Setting the colorscheme
-vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme(COLORSCHEME)
+
+if TRANSPARENT then
+    vim.api.nvim_set_hl(0, 'Normal', { bg = nil })
+end
