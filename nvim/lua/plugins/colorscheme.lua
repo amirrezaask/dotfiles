c6c77d08e2959ee296cf0fb82d84b0dd6bf6da4f
amirrezaask <raskarpour@gmail.com>
2023-07-23 19:01:33 +0330

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   { 'navarasu/onedark.nvim', opts = { style = "darker" }},
   { 'marko-cerovac/material.nvim' },
-  { 'rose-pine/neovim', name = "rose-pine" },
+  { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = false } },
   { 'catppuccin/nvim', name = 'catppuccin' },
   { 'amirrezaask/themes', name = "amirreza-themes" }
 }
