ee3e188ef921a0ae873a85133cbd6a5c374aa6da
amirrezaask <raskarpour@gmail.com>
2023-07-26 02:26:32 +0330

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,4 +31,4 @@ require("lazy").setup ("plugins", {
   }
 })
 
-vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme "dracula"
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,5 +3,6 @@ return {
   { 'marko-cerovac/material.nvim' },
   { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = false } },
   { 'catppuccin/nvim', name = 'catppuccin' },
+  { 'Mofiqul/dracula.nvim', name = 'dracula' },
   { 'amirrezaask/themes', name = "amirreza-themes" }
 }
