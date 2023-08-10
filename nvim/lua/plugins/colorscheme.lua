991af415288ec5958f61e850959859ccb8fbba95
amirrezaask <raskarpour@gmail.com>
2023-08-10 22:13:10 +0330

    UPdates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   { 'navarasu/onedark.nvim', opts = { style = "darker" }},
   { 'marko-cerovac/material.nvim' },
-  { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = true } },
+  { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = true, disable_background = true} },
   { "ellisonleao/gruvbox.nvim", opts = { contrast = 'hard' }},
   { 'catppuccin/nvim', name = 'catppuccin' },
   { 'Mofiqul/dracula.nvim', name = 'dracula' },
