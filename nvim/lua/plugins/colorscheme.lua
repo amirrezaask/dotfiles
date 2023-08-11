7d3375325fe0b80935ef1b54ee617f19e3251e30
amirrezaask <raskarpour@gmail.com>
2023-08-11 23:39:38 +0330

    nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-  { 'navarasu/onedark.nvim', opts = { style = "darker" }},
+  { 'navarasu/onedark.nvim', opts = { style = "darker", transparent = true}},
   { 'marko-cerovac/material.nvim' },
   { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = true, disable_background = true} },
   { "ellisonleao/gruvbox.nvim", opts = { contrast = 'hard' }},
