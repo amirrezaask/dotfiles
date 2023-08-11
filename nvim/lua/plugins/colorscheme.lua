c3cbc8b4969bf2e22a9fbed423c34914472ed30c
amirrezaask <raskarpour@gmail.com>
2023-08-12 00:21:17 +0330

    colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
 return {
-  { 'navarasu/onedark.nvim', opts = { style = "darker", transparent = true}},
-  { 'rose-pine/neovim', name = "rose-pine", opts = { disable_italics = true, disable_background = true} },
-  { "ellisonleao/gruvbox.nvim", opts = { contrast = 'hard' }},
-  { 'catppuccin/nvim', name = 'catppuccin' },
+	{ "navarasu/onedark.nvim", opts = { style = "darker", transparent = true } },
+	{ "rose-pine/neovim", name = "rose-pine", opts = { disable_italics = true, disable_background = true } },
+	{
+		"ellisonleao/gruvbox.nvim",
+		opts = {
+			contrast = "hard",
+			italic = {
+				strings = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			},
+			transparent_mode = true,
+		},
+	},
+	{ "catppuccin/nvim", name = "catppuccin", opts = {} },
 }
