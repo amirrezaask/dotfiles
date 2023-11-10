d40d9357988efe80f3a521dbff07a7c1e430fa73
amirrezaask <raskarpour@gmail.com>
2023-11-11 00:54:59 +0330

    dotfiles

--- wezterm.lua
+++ wezterm.lua
@@ -9,7 +9,6 @@ end
 cfg.font = wezterm.font_with_fallback { "Jetbrains Mono", "Fira Code", "Liberation Mono" }
 
 cfg.colors = {}
-cfg.colors.background = '#111111'
 cfg.use_fancy_tab_bar = false
 
 cfg.tab_bar_at_bottom = true
