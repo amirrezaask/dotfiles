960096910b3174c2cfbfdd1d662a0ac3bbb9d185
amirrezaask <raskarpour@gmail.com>
2023-11-13 00:01:39 +0330

    updates

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,13 +2,28 @@ local color_state = 'dark'
 local dark_colorscheme = 'rose-pine'
 local light_colorscheme = 'rose-pine-dawn'
 
-vim.cmd.colorscheme(dark_colorscheme)
+local function colorscheme(name, transparent)
+	vim.cmd.colorscheme(name)
+	vim.cmd([[ hi LineNr guifg=#5eacd3 ]])
+	if transparent then
+		vim.cmd([[
+		hi Normal guibg=none
+		hi NormalNC guibg=none
+		hi NormalFloat guibg=none
+		hi Visual guibg=#49B9C7 guifg=#F6F6F6
+		hi SignColumn guibg=none
+	]])
+	end
+end
+
 function ToggleColorscheme()
 	if color_state == 'dark' then
 		color_state = 'light'
-		vim.cmd.colorscheme(light_colorscheme)
+		colorscheme(light_colorscheme, true)
 	else
 		color_state = 'light'
-		vim.cmd.colorscheme(dark_colorscheme)
+		colorscheme(dark_colorscheme, true)
 	end
 end
+
+colorscheme(dark_colorscheme, false)
