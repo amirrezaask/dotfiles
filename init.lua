48b6c358b01819e3b000c5a819dbda12d7b037a2
amirrezaask <raskarpour@gmail.com>
2024-02-24 07:05:10 +0330

    Disable signs for todo-comments

--- init.lua
+++ init.lua
@@ -242,6 +242,7 @@ require("lazy").setup({
             "folke/todo-comments.nvim",
             dependencies = { "nvim-lua/plenary.nvim" },
             opts = {
+                signs = false
             }
         },
 
