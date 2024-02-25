83d47098f67fc1cbc28693c46b2ab2690a367270
amirrezaask <raskarpour@gmail.com>
2024-02-25 04:40:01 +0330

    Add statusline plugin

---
+++ nvim/lua/amirreza/plugins/statusline.lua
@@ -0,0 +1,10 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    opts = {
+        options = {
+            icons_enabled = false,
+        }
+    }
+}
