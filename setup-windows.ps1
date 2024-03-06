c6f178c07f71f9141b61af55224532283abc5225
amirrezaask <raskarpour@gmail.com>
2024-03-07 03:17:59 +0330

    Windows IS AWESOME

---
+++ setup-windows.ps1
@@ -0,0 +1,10 @@
+echo "You should run this in Administrator powershell."
+
+winget import -i winget-export.json
+
+Remove-Item -Force -Recurse $HOME\Documents\PowerShell
+Remove-Item -Force -Recurse $Env:APPDATA\.emacs.d
+Remove-Item -Force -Recurse $Env:LOCALAPPDATA\nvim
+New-Item -Type SymbolicLink $HOME\Documents\PowerShell -Value C:\w\dotfiles\PowerShell\
+New-Item -Type SymbolicLink $Env:APPDATA\.emacs.d -Value C:\w\dotfiles\emacs\
+New-Item -Type SymbolicLink $Env:LOCALAPPDATA\nvim -Value C:\w\dotfiles\nvim\
