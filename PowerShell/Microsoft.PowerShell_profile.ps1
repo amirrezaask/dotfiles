d8a7842124eb235672bb6ec7c254d0801cb50f2f
amirrezaask <raskarpour@gmail.com>
2024-03-07 03:15:56 +0330

    PowerShell Awesomeness

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,32 @@
+if((Get-Module -ListAvailable -Name PSReadLine) -eq $null) {
+    Install-Module -Scope CurrentUser PSReadLine
+}
+
+if((Get-Module -ListAvailable -Name Terminal-Icons) -eq $null) {
+    Install-Module -Scope CurrentUser Terminal-Icons
+}
+
+if((Get-Module -ListAvailable -Name posh-git) -eq $null) {
+    Install-Module -Scope CurrentUser posh-git
+}
+
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Invoke-Expression (&starship init powershell)
+Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Emacs
 
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
+Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
+Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
+Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+RightArrow ShellForwardWord
+
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-Alias vim nvim
