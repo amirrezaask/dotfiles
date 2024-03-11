261ff9e7ea66a6fb34c4cb77ae8735ba9c673a7d
amirrezaask <raskarpour@gmail.com>
2024-03-12 00:28:44 +0330

    SublimeText fixups

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -7,4 +7,5 @@ Remove-Item -Force -Recurse $Env:APPDATA\.emacs.d
 Remove-Item -Force -Recurse $Env:LOCALAPPDATA\nvim
 New-Item -Type SymbolicLink $HOME\Documents\PowerShell -Value C:\w\dotfiles\PowerShell\
 New-Item -Type SymbolicLink $Env:APPDATA\.emacs.d -Value C:\w\dotfiles\emacs\
+New-Item -Type SymbolicLink '$Env:APPDATA\Sublime Text\Packages\User' -Value C:\w\dotfiles\sublimetext\
 New-Item -Type SymbolicLink $Env:LOCALAPPDATA\nvim -Value C:\w\dotfiles\nvim\
