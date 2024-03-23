1d7283daf8e86dd9189c5e5fbce9a7557ed824f1
amirrezaask <raskarpour@gmail.com>
2024-03-23 08:06:00 +0330

    setup-windows.ps1 update

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -51,5 +51,5 @@ New-Item -Type SymbolicLink $HOME\Documents\PowerShell -Value C:\w\dotfiles\Powe
 New-Item -Type SymbolicLink $Env:APPDATA\.emacs.d -Value C:\w\dotfiles\emacs\
 New-Item -Type SymbolicLink '$Env:APPDATA\Sublime Text\Packages\User' -Value C:\w\dotfiles\sublimetext\
 New-Item -Type SymbolicLink $Env:LOCALAPPDATA\nvim -Value C:\w\dotfiles\nvim\
-New-Item $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json -Type SymbolicLink -Value C:\w\dotfiles\WindowsTerminal\settings.json
-New-Item $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Type SymbolicLink -Value C:\w\dotfiles\WindowsTerminal\settings.json
+New-Item -Type SymbolicLink $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json  -Value C:\w\dotfiles\WindowsTerminal\settings.json
+New-Item -Type SymbolicLink $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Value C:\w\dotfiles\WindowsTerminal\settings.json
