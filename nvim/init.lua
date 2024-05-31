ab8357d3d0fe4519adc5626d3ff49a88a1a0325f
amirrezaask <raskarpour@gmail.com>
2024-06-01 00:26:43 +0330

    Remove luasnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -309,11 +309,11 @@ require("lazy").setup({
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip'
         },
         config = function()
             local cmp_select = { behavior = require("cmp").SelectBehavior.Select }
