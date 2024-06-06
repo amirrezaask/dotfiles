6dbd4023e809ebfa5d120c18a2bd9a3191cb4e44
amirrezaask <raskarpour@gmail.com>
2024-06-07 00:59:08 +0330

    Make nvim our editor

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ function fish_prompt -d "Write out the prompt"
     printf '%s %s%s%s > ' $USER \
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
-
+export EDITOR='nvim'
+export GIT_EDITOR='nvim'
