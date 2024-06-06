9d073c3e3cf09112e2fe01c8432468ed1cd7ee92
amirrezaask <raskarpour@gmail.com>
2024-06-06 08:29:04 +0330

    Some updates

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+export GOPATH="$HOME/go"
+export PATH="$PATH:$GOPATH/bin"
+export PATH="$PATH:$HOME/.local/bin"
+function fish_prompt -d "Write out the prompt"
+    printf '%s %s%s%s > ' $USER \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
---
+++ vscode/extensions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Installing VSCode extensions"
+code --install-extensions bmewburn.vscode-intelephense-client
+code --install-extensions dracula-theme.theme-dracula
+code --install-extensions felipecaputo.git-project-manager
+code --install-extensions github.github-vscode-theme
+code --install-extensions golang.go
+code --install-extensions ms-vscode-remote.remote-wsl
+code --install-extensions rust-lang.rust-analyzer
+code --install-extensions waderyan.gitblame
+code --install-extensions zhuangtongfa.material-theme
