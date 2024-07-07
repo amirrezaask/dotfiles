6a305fbac3dbeba3dcc1981e90e716e8581e6e26
amirrezaask <raskarpour@gmail.com>
2024-07-07 18:58:14 +0330

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,23 @@ local config = {}
 
 config.font_size = 16
 config.font = wezterm.font_with_fallback {
+	'MesloLGL Nerd Font Mono',
 	'Cascadia Mono NF',
 	'Jetbrains Mono',
 	'Meslo',
+}
+
+-- Coolnight theme
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#706b4e",
+	selection_fg = "#f3d9c4",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
 config.window_padding = {
