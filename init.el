b3f1a89a143b64aa2f85c288c735111558c81a7f
amirrezaask <raskarpour@gmail.com>
2024-07-17 15:22:28 +0330

    even more emacs magic with the help of C-u

--- init.el
+++ init.el
@@ -32,11 +32,7 @@
       is-macos (eq system-type 'darwin)
       has-treesitter (>= emacs-major-version 29))
 
-(defun amirreza/edit-init ()
-  (interactive)
-  (find-file INIT-FILE))
-
-(global-unset-key (kbd "C-x C-c"))
+(defun edit-init () "Edit this file." (interactive) (find-file INIT-FILE))
 
 (require 'package)
 (setq package-archives
@@ -60,12 +56,12 @@
 (global-so-long-mode +1)
 (require 'vlf-setup)
 
-(defun amirreza/copy () "Either copy region or the current line." (interactive)
+(defun copy () "Either copy region or the current line." (interactive)
   (if (use-region-p)
       (kill-ring-save (region-beginning) (region-end)) ;; copy active region contents
     (kill-ring-save (line-beginning-position) (line-end-position)))) ;; copy current line
 
-(defun amirreza/cut () "Either cut region or the current line." (interactive)
+(defun cut () "Either cut region or the current line." (interactive)
   (if (use-region-p)
       (kill-region (region-beginning) (region-end)) ;; copy active region contents
     (kill-region (line-beginning-position) (line-end-position)))) ;; copy current line
@@ -76,12 +72,12 @@
 
 (defun home (path) (expand-file-name path (getenv "HOME")))
 (add-to-list 'exec-path (home ".local/bin"))
-(add-to-list 'exec-path "/usr/local/go/bin")
-(add-to-list 'exec-path (home ".cargo/bin"))
-(add-to-list 'exec-path "/opt/homebrew/bin")
-(add-to-list 'exec-path (home "bin"))
-(add-to-list 'exec-path "/usr/local/bin")
 (add-to-list 'exec-path (home "go/bin"))
+(add-to-list 'exec-path (home ".cargo/bin"))
+(add-to-list 'exec-path (home "bin"))
+(add-to-list 'exec-path "/usr/local/go/bin")
+(add-to-list 'exec-path "/opt/homebrew/bin")
+(add-to-list 'exec-path "/usr/local/bin")
 (add-to-list 'exec-path "w:/bin")
 (add-to-list 'exec-path "c:/programs/bin")
 
@@ -90,7 +86,7 @@
   (setenv "PATH" (string-join exec-path ":"))) ;; set emacs process PATH
 
 
-(defun amirreza/vterm-dwim () "Jump to vterm buffer associated with current project or create a new." (interactive)
+(defun vterm-dwim () "Jump to vterm buffer associated with current project or create a new." (interactive)
   (let* ((root (find-project-root-or-default-directory))
 	 (default-directory root)
 	 (buffer-name (format "*vterm-%s*" root)))
@@ -99,7 +95,7 @@
       (vterm buffer-name))))
 
 (setq eshell-visual-subcommands '("git" "diff" "log" "show"))
-(defun amirreza/eshell-dwim () "Jump to eshell buffer associated with current project or create a new." (interactive)
+(defun eshell-dwim () "Jump to eshell buffer associated with current project or create a new." (interactive)
   (let* ((root (find-project-root-or-default-directory))
 	 (default-directory root)
 	 (eshell-buffer-name (format "*eshell-%s*" root)))
@@ -112,32 +108,6 @@
   (define-key minibuffer-mode-map (kbd "C-n") 'minibuffer-next-completion)
   (define-key minibuffer-mode-map (kbd "C-p") 'minibuffer-previous-completion))
 
-;; naysayer
-;; (custom-set-faces
-;;   `(default                          ((t (:foreground \"#debe95\" :background \"#202020\"))))
-;;   `(hl-line                          ((t (:background \"#353535\"))))
-;;   `(vertico-current                  ((t (:background \"#353535\"))))
-;;   `(region                           ((t (:background \"medium blue\"))))
-;;   `(cursor                           ((t (:background \"lightgreen\"))))
-;;   `(font-lock-keyword-face           ((t (:foreground \"#d4d4d4\"))))
-;;   `(font-lock-type-face              ((t (:foreground \"#8cde94\"))))
-;;   `(font-lock-constant-face          ((t (:foreground \"#7ad0c6\"))))
-;;   `(font-lock-variable-name-face     ((t (:foreground \"#c8d4ec\"))))
-;;   `(font-lock-builtin-face           ((t (:foreground \"white\"))))
-;;   `(font-lock-string-face            ((t (:foreground \"gray70\"))))
-;;   `(font-lock-comment-face           ((t (:foreground \"yellow\"))))
-;;   `(font-lock-comment-delimiter-face ((t (:foreground \"yellow\"))))
-;;   `(font-lock-doc-face               ((t (:foreground \"#3fdf1f\"))))
-;;   `(font-lock-function-name-face     ((t (:foreground \"white\"))))
-;;   `(font-lock-doc-string-face        ((t (:foreground \"#3fdf1f\"))))
-;;   `(font-lock-warning-face           ((t (:foreground \"yellow\"))))
-;;   `(font-lock-note-face              ((t (:foreground \"khaki2\" ))))
-;;   `(mode-line                        ((t (:foreground \"black\" :background \"#d3b58d\"))))
-;;   `(mode-line-inactive               ((t (:background \"gray20\" :foreground \"#ffffff\"))))
-;;   `(show-paren-match                 ((t (:background \"mediumseagreen\")))))
-
-
-;; witness
 (custom-set-faces
  `(default                          ((t (:foreground "#d3b58d" :background "#072626"))))
  `(hl-line                          ((t (:background "#0c4141"))))
@@ -161,7 +131,7 @@
  `(mode-line-inactive               ((t (:background "gray20" :foreground "#ffffff"))))
  `(show-paren-match                 ((t (:background "mediumseagreen")))))
 
-(defun find-project-root ()
+(defun find-root ()
   "Try to find project root based on deterministic predicates"
   (cond
    ((eq major-mode 'go-mode)                                (locate-dominating-file default-directory "go.mod"))
@@ -170,16 +140,12 @@
 
 (defun git-repo-p (DIR) (locate-dominating-file DIR ".git"))
 
-(defun find-project-root-or-default-directory () (or (find-project-root) default-directory))
-
-;; $ go install golang.org/x/tools/gopls@latest
-;; $ rustup component add rust-analyzer
-;; $ npm install -g intelephense
+(defun find-root-or-default-directory () (or (find-root) default-directory))
 
 (setq-default c-default-style "linux" c-basic-offset 4) ;; C/C++ code style
 
-(setq corfu-auto nil)
-(global-corfu-mode +1)
+(setq corfu-auto nil) ;; No autocomplete popup
+(global-corfu-mode +1) ;;
 
 (setq eglot-ignored-server-capabilities '(
 					  :hoverProvider
@@ -194,154 +160,111 @@
 					  :colorProvider
 					  :foldingRangeProvider
 					  :executeCommandProvider
-					  :inlayHintProvider
-					  ))
+					  :inlayHintProvider))
 
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs
-	       '(php-mode . ("intelephense" "--stdio"))) ;; Only viable php langauge server
-  )
-
+(with-eval-after-load 'eglot (add-to-list 'eglot-server-programs '(php-mode . ("intelephense" "--stdio"))))
 (defun eglot-organize-imports () (interactive) (eglot-code-actions nil nil "source.organizeImports" t))
-
 (setq eglot-stay-out-of '(project))
 (setq eglot-sync-connect nil)
+(dolist (mode '(go rust php)) (add-hook (intern (concat (symbol-name mode) "-mode-hook")) #'eglot-ensure))
 
-(dolist (mode '(go rust php)) ;; Enable LSP for these languages
-  (add-hook (intern (concat (symbol-name mode) "-mode-hook")) #'eglot-ensure))
+(defun amirreza-compile-buffer-name-function (MODESTR) (let ((dir (find-root-or-default-directory))) (format "*%s-%s*" MODESTR dir)))
+(setq-default compilation-buffer-name-function 'amirreza-compile-buffer-name-function)
 
-(defun amirreza/compile-buffer-name-function (MODESTR)
-  (let ((dir (find-project-root-or-default-directory)))
-    (format "*%s-%s*" MODESTR dir)))
+(defun run-compile () "run `compile`." (interactive)
+       (if (null current-prefix-arg)
+	   (setq --compile-dir (find-root-or-default-directory))
+	 (setq --compile-dir (read-directory-name "Directory: " default-directory)))
 
-(setq-default compilation-buffer-name-function 'amirreza/compile-buffer-name-function)
-
-(defun guess-compile-command (DIR)
-  (let ((default-directory DIR))
-    (cond
-     ((file-exists-p "build.bat") "build.bat")
-     ((file-exists-p "go.mod")    "go build -v ./..."))))
-
-(defun amirreza/compile-in-directory (DIR COMMAND) "Compile in a directory" (interactive (list
-											  (read-directory-name "[Compile] Directory: " (find-project-root-or-default-directory))
-											  (read-shell-command "[Compile] Command: " (guess-compile-command default-directory))))
-  
-  (let* ((default-directory DIR)
-	 (command COMMAND))
-    (setq amirreza/last-compile `(,default-directory ,command))
-    (compilation-start command)))
-
-(defun amirreza/compile-dwim ()
-  (interactive)
-  (cond
-   ((equal (length (find-project-root)) 0) (call-interactively 'amirreza/compile-in-directory)) ;; we are not inside a project so we should ask user for directory.
-   (t (amirreza/compile-in-directory (find-project-root) (read-shell-command "[Compile] Command: " (guess-compile-command (find-project-root-or-default-directory)))))))
-
+       (let* ((default-directory --compile-dir)
+	      (command (read-shell-command "Command: "  (cond ;; guess a command based on the context.
+							 ((file-exists-p "build.bat") "build.bat")
+							 ((file-exists-p "go.mod")    "go build -v ./...")
+							 ((file-exists-p "Makefile")  "make")))))
+	 (compilation-start command)))
 
 (with-eval-after-load 'grep
   (when (executable-find "rg")
     (grep-apply-setting 'grep-command "rg --no-heading --color='never'")
     (grep-apply-setting 'grep-use-null-device nil)))
 
-(defun amirreza/grep-in-directory (DIR)
-  "Run appropriate grep program in directory."
-  (interactive (list
-		(read-directory-name "[Grep] Directory: " (find-project-root-or-default-directory))))
+(defun run-grep ()
+  "Recursive grep in `find-root` result or C-u to choose directory interactively."
+  (interactive)
+  (if (null current-prefix-arg)
+      (setq --grep-dir (find-root-or-default-directory))
+    (setq --grep-dir (read-directory-name "Directory: " default-directory)))
 
-  (let ((default-directory DIR))
-  (cond
-   ((executable-find "rg") (grep (format "rg --no-heading --color='never' %s" (read-string "Ripgrep: "))))
-   ((git-repo-p DIR) (grep (format "git grep --no-color -n %s" (read-string "Git Grep: ")))))))
+  (let ((default-directory --grep-dir))
+    (cond
+     ((executable-find "rg") (grep (format "rg --no-heading --color='never' %s" (read-string "Ripgrep: "))))
+     ((git-repo-p DIR)       (grep (format "git grep --no-color -n %s" (read-string "Git Grep: "))))
+     (t                      (grep (format "grep --color=auto -nH --null -e %s" (read-string "Grep: ")))))))
+
+(defun file-finder ()
+  "Recursive file find starting from `find-root` result or C-u to choose directory interactively."
+  (interactive)
+  (if (null current-prefix-arg)
+      (setq --open-file-dir (find-root-or-default-directory))
+    (setq --open-file-dir (read-directory-name "Directory: " default-directory)))
   
-(defun amirreza/grep-dwim ()
-  "DWIM version of amirreza/grep-in-directory"
-  (interactive)
   (cond
-   ((equal (length (find-project-root)) 0) (call-interactively 'amirreza/grep-in-directory)) ;; we are not inside a project so we should ask user for directory.
-   (t (amirreza/grep-in-directory (find-project-root)))))
+   ((executable-find "rg") (let* ((default-directory --open-file-dir)
+				  (command (format "rg --files"))
+				  (file (completing-read "Ripgrep Files: " (string-split (shell-command-to-string command) "\n" t))))
+			     (find-file file)))
+   
+   ((git-repo-p --open-file-dir) (let*
+				     ((default-directory --open-file-dir)
+				      (command (format "git ls-files"))
+				      (file (completing-read "Git Files: " (string-split (shell-command-to-string command) "\n" t))))
+				   (find-file file)))
+   (t (error "you don't have rg installed and it's not a git repo."))))
 
-(defun amirreza/git-files (DIR)
-  "run git ls-files to get list of files indexed in git."
-  (interactive (list (read-directory-name "[Git Files] Directory: " (find-project-root-or-default-directory))))
-  (unless (executable-find "git") (error "git executable not found."))
-
-  (let* ((default-directory DIR)
-	 (command (format "git ls-files"))
-	 (file (completing-read "Git Files: " (string-split (shell-command-to-string command) "\n" t))))
-    (find-file file)))
-
-(defun amirreza/rg-files (DIR)
-  "run rg --files to get list of files in current project."
-  (interactive (list (read-directory-name "[Ripgrep Files] Directory: " (find-project-root-or-default-directory))))
-  (unless (executable-find "rg") (error "rg executable not found."))
-
-  (let* ((default-directory DIR)
-	 (command (format "rg --files"))
-	 (file (completing-read "Ripgrep Files:" (string-split (shell-command-to-string command) "\n" t)))
-	 )
-    (find-file file)))
-
-
-(defun amirreza/find-file-in-directory (DIR)
-  "Recursive file find starting from 'DIR'."
-  (interactive (list (read-directory-name "DIR: " (find-project-root-or-default-directory))))
-  (cond
-   ((executable-find "rg") (amirreza/rg-files DIR))
-   ((git-repo-p DIR) (amirreza/git-files DIR))
-   (t (call-interactively 'find-file))))
-
-(defun amirreza/find-file-dwim ()
-  "DWIM version of 'amirreza/find-file-in-directory'."
-  (interactive)
-  (cond
-   ((equal (length (find-project-root)) 0) (call-interactively 'amirreza/find-file-in-directory)) ;; we are not inside a project so we should ask user for directory.
-   (t (amirreza/find-file-in-directory (find-project-root)))))
-
-
-(global-set-key (kbd "C-o")                                          'find-file)
-(global-set-key (kbd "C-S-o")                                        'amirreza/grep-dwim)
-(global-set-key (kbd "C-M-o")                                        'amirreza/grep-in-directory)
-(global-set-key (kbd "M-o")                                          'amirreza/find-file-dwim)
-(global-set-key (kbd "C-x i")                                        'amirreza/edit-init)
+(global-unset-key (kbd "C-x C-c"))
+(global-set-key (kbd "C-o")                                          'other-window)
+(global-set-key (kbd "C-q")                                          'file-finder) ;; quick file find.
+(global-set-key (kbd "M-q")                                          'run-grep)    ;; quick grep.
+(global-set-key (kbd "M-o")                                          'find-file)
+(global-set-key (kbd "C-x i")                                        'edit-init)
 (with-eval-after-load 'grep
   (define-key grep-mode-map (kbd "C-c C-p")                          'wgrep-toggle-readonly-area)
   (define-key grep-mode-map (kbd "<f5>")                             'recompile)
   (define-key grep-mode-map (kbd "g")                                'recompile)
-  (define-key grep-mode-map (kbd "G")                                'amirreza/grep-in-directory)
+  (define-key grep-mode-map (kbd "G")                                'grep-in-directory)
   (define-key grep-mode-map (kbd "M-q")                              'previous-buffer)
   (define-key grep-mode-map (kbd "M-n")                              'jump-down)
   (define-key grep-mode-map (kbd "M-p")                              'jump-up)
   (define-key grep-mode-map (kbd "k")                                'kill-compilation))
 
-(global-set-key (kbd "M-m")                                          'amirreza/compile-dwim)
+(global-set-key (kbd "M-m")                                          'compile-dwim)
 (with-eval-after-load 'compile
   (define-key compilation-mode-map (kbd "<f5>")                      'recompile)
   (define-key compilation-mode-map (kbd "M-m")                       'previous-buffer)
-  (define-key compilation-mode-map (kbd "G")                         'amirreza/compile-in-directory)
+  (define-key compilation-mode-map (kbd "G")                         'compile-in-directory)
   (define-key compilation-mode-map (kbd "n")                         'next-line)
   (define-key compilation-mode-map (kbd "p")                         'previous-line)
   (define-key compilation-mode-map (kbd "M-n")                       'jump-down)
   (define-key compilation-mode-map (kbd "M-p")                       'jump-up)
   (define-key compilation-mode-map (kbd "k")                         'kill-compilation))
-(global-set-key (kbd "M-j")                                          'amirreza/eshell-dwim)
-(global-set-key (kbd "C-M-j")                                        'amirreza/vterm-dwim)
+(global-set-key (kbd "M-j")                                          'eshell-dwim)
+(global-set-key (kbd "C-M-j")                                        'vterm-dwim)
 
 (global-set-key (kbd "C-.")                                          'next-buffer)
 (global-set-key (kbd "C-,")                                          'previous-buffer)
 
-(global-set-key (kbd "C-x g")                                        'magit)                     ;; Git Client
+(global-set-key (kbd "C-x g")                                        'magit)            
 
 (with-eval-after-load 'replace
-  (define-key query-replace-map (kbd "<return>")                     'act))                      ;; in replace mode <return> applies.
-(global-set-key (kbd "C-q")                                          'set-mark-command)          ;; select
-(global-set-key (kbd "C-w")                                          'amirreza/cut)              ;; sane cut/kill
-(global-set-key (kbd "M-w")                                          'amirreza/copy)             ;; sane copy
-(global-set-key (kbd "M-[")                                          'kmacro-start-macro)        ;; start macro
-(global-set-key (kbd "M-]")                                          'kmacro-end-or-call-macro)  ;; end macro
-(global-set-key (kbd "M-\\")                                         'kmacro-end-and-call-macro) ;; call keyboard macro.
-(global-set-key (kbd "C-z")                                          'undo)                      ;; Sane undo key
-(global-set-key (kbd "M-r")                                          'replace-string)            ;; replace strings ( no regex )
-(global-set-key (kbd "C-M-r")                                        'query-replace)             ;; replace using regex
+  (define-key query-replace-map (kbd "<return>")                     'act))             
+(global-set-key (kbd "C-w")                                          'cut)              
+(global-set-key (kbd "M-w")                                          'copy)             
+(global-set-key (kbd "M-[")                                          'kmacro-start-macro)       
+(global-set-key (kbd "M-]")                                          'kmacro-end-or-call-macro) 
+(global-set-key (kbd "M-\\")                                         'kmacro-end-and-call-macro)
+(global-set-key (kbd "C-z")                                          'undo)                     
+(global-set-key (kbd "M-r")                                          'replace-string)           
+(global-set-key (kbd "C-M-r")                                        'query-replace)            
 
 (global-set-key (kbd "C-M-n")                                        'mc/mark-next-like-this-word)
 (global-set-key (kbd "C-M-p")                                        'mc/mark-previous-like-this-word)
@@ -378,4 +301,3 @@
 (with-eval-after-load 'rect
   (define-key rectangle-mark-mode-map (kbd "C-i")                    'string-insert-rectangle)
   (define-key rectangle-mark-mode-map (kbd "C-r")                    'string-rectangle))
-
