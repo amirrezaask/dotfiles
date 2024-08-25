e5383a9a08189d3935b91ac68e8eaee703d8cf49
amirrezaask <raskarpour@gmail.com>
2024-08-25 04:59:32 +0330

    updates

--- .zshrc
+++ .zshrc
@@ -21,7 +21,6 @@ fi
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="robbyrussell"
-# ZSH_THEME="powerlevel10k/powerlevel10k"
 
 plugins=(git)
 
--- config.fish
+++ config.fish
@@ -8,6 +8,10 @@ function fish_prompt -d "Write out the prompt"
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
+if ! command -v starship &>/dev/null
+   curl -sS https://starship.rs/install.sh | sh
+end
+
 if command -v emacs &>/dev/null # use emacs as editor
     export EDITOR='emacs'
     export GIT_EDITOR='emacs'
@@ -35,5 +39,7 @@ end
 
 function fish_greeting
 end
+
+starship init fish | source
 
 alias svpn='sudo openfortivpn  --otp (totpgen ADS)'
