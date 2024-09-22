847e9a200aa6ea5d18db10870c11bd5f1407a8f8
amirrezaask <raskarpour@gmail.com>
2024-09-23 02:53:20 +0330

    Tmux and neovim

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@
 -- Minimal, fast configuration for neovim.
 
 TRANSPARENT = os.getenv('NVIM_TRANSPARENT') or false
-COLORSCEHEME = os.getenv('NVIM_COLORSCHEME') or "gruvbox-material"
+COLORSCEHEME = os.getenv('NVIM_COLORSCHEME') or "catppuccin-mocha"
 IS_WINDOWS = vim.fn.has("win32") == 1
 
 -- Lazy: Plugin manager
@@ -32,6 +32,68 @@ require("lazy").setup({
         opts = {
             transparent_background = TRANSPARENT,
             no_italic = true,
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+            color_overrides = {
+                latte = {
+                    rosewater = "#c14a4a",
+                    flamingo = "#c14a4a",
+                    red = "#c14a4a",
+                    maroon = "#c14a4a",
+                    pink = "#945e80",
+                    mauve = "#945e80",
+                    peach = "#c35e0a",
+                    yellow = "#b47109",
+                    green = "#6c782e",
+                    teal = "#4c7a5d",
+                    sky = "#4c7a5d",
+                    sapphire = "#4c7a5d",
+                    blue = "#45707a",
+                    lavender = "#45707a",
+                    text = "#654735",
+                    subtext1 = "#73503c",
+                    subtext0 = "#805942",
+                    overlay2 = "#8c6249",
+                    overlay1 = "#8c856d",
+                    overlay0 = "#a69d81",
+                    surface2 = "#bfb695",
+                    surface1 = "#d1c7a3",
+                    surface0 = "#e3dec3",
+                    base = "#f9f5d7",
+                    mantle = "#f0ebce",
+                    crust = "#e8e3c8",
+                },
+                mocha = {
+                    rosewater = "#ea6962",
+                    flamingo = "#ea6962",
+                    red = "#ea6962",
+                    maroon = "#ea6962",
+                    pink = "#d3869b",
+                    mauve = "#d3869b",
+                    peach = "#e78a4e",
+                    yellow = "#d8a657",
+                    green = "#a9b665",
+                    teal = "#89b482",
+                    sky = "#89b482",
+                    sapphire = "#89b482",
+                    blue = "#7daea3",
+                    lavender = "#7daea3",
+                    text = "#ebdbb2",
+                    subtext1 = "#d5c4a1",
+                    subtext0 = "#bdae93",
+                    overlay2 = "#a89984",
+                    overlay1 = "#928374",
+                    overlay0 = "#595959",
+                    surface2 = "#4d4d4d",
+                    surface1 = "#404040",
+                    surface0 = "#292929",
+                    base = "#1d2021",
+                    mantle = "#191b1c",
+                    crust = "#141617",
+                },
+            },
         }
     },
     {
