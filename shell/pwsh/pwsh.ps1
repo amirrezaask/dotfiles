227d709c2a3a01a1f3ab739a2bdb69cf95e704b0
AmirrezaAsk <raskarpour@gmail.com>
2024-12-12 03:16:56 +0330

    Update powershell Config

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -1,7 +1,3 @@
-$ENV:STARSHIP_CONFIG = "D:\projects\dotfiles\shell\starship\starship.toml"
-Invoke-Expression (&starship init powershell)
-
-# Import-Module posh-git
 Import-Module PSReadLine
 Import-Module Terminal-Icons
 
@@ -11,6 +7,3 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-
-
-# oh-my-posh init pwsh | Invoke-Expression
