6398508a4b01cdeb838e4f3d7bdc48bc35b154f0
AmirrezaAsk <raskarpour@gmail.com>
2024-12-12 03:24:24 +0330

    Updates

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -1,9 +1,10 @@
 Import-Module PSReadLine
-Import-Module Terminal-Icons
-
 Set-PSReadLineOption -EditMode Emacs
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+
+Invoke-Expression (&starship init powershell)
