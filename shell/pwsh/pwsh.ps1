8388d8631a2b3767679e76c2e902e931aa6d5e66
AmirrezaAsk <raskarpour@gmail.com>
2024-12-20 16:54:42 +0330

    Update powershell

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -7,7 +7,7 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) 
-{
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
-}
+# if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) 
+# {
+#   oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
+# }
