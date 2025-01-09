b1d1941a38c72be1062318d4f024bcfb4019f356
amirrezaask <raskarpour@gmail.com>
2025-01-09 04:59:40 +0330

    updates

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -5,7 +5,7 @@
 -- Minimal, fast configuration for neovim.
 
 TRANSPARENT = os.getenv('NVIM_TRANSPARENT') or false
-COLORSCEHEME = os.getenv('NVIM_COLORSCHEME') or "gruvbox-material"
+COLORSCEHEME = os.getenv('NVIM_COLORSCHEME') or "tokyonight"
 IS_WINDOWS = vim.fn.has("win32") == 1
 
 vim.opt.wrap = true        -- Wrap long lines
