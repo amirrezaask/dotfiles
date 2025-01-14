c18b1432007d53097bb8917a89ce9d9479f0971d
amirrezaask <raskarpour@gmail.com>
2025-01-14 23:44:34 +0330

    Updates

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -4,7 +4,7 @@
 -- /_/ |_/_/_/_/_/_/ /_/  \__//__/\_,_/_/ |_/___/_/\_\
 -- Minimal, fast configuration for neovim.
 
-TRANSPARENT = os.getenv('NVIM_TRANSPARENT') or false
+TRANSPARENT = os.getenv('NVIM_TRANSPARENT') or true
 COLORSCEHEME = os.getenv('NVIM_COLORSCHEME') or "tokyonight"
 IS_WINDOWS = vim.fn.has("win32") == 1
 
