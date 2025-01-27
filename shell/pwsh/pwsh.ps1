73f9eec4f3ff370591ddf474319d864d36ccd1ee
AmirrezaAsk <raskarpour@gmail.com>
2025-01-27 22:34:23 +0330

    remove starship from powershell

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -3,25 +3,14 @@ Import-Module posh-git
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-
-
 $PSStyle.FileInfo.Directory = ""
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) 
+# if (Get-Command "starship" -ErrorAction SilentlyContinue) 
 # {
-#   oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
+#   $ENV:STARSHIP_CONFIG = "D:\src\dotfiles\shell\starship\starship.toml"
+#   $ENV:STARSHIP_DISTRO = "者 Windows"
+#   Invoke-Expression (&starship init powershell)
 # }
-if (Get-Command "starship" -ErrorAction SilentlyContinue) 
-{
-  $ENV:STARSHIP_CONFIG = "D:\src\dotfiles\shell\starship\starship.toml"
-  $ENV:STARSHIP_DISTRO = "者 Windows"
-  Invoke-Expression (&starship init powershell)
-}
 
 New-Alias -Name gvim -Value neovide
 New-Alias -Name vim -Value nvim
