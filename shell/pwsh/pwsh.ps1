22fd967f55bc96e024bd66bd046035439e7f967a
AmirrezaAsk <raskarpour@gmail.com>
2025-01-28 02:24:58 +0330

    Some powershell

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -1,4 +1,8 @@
 Import-Module PSReadLine
-# Import-Module posh-git
 Set-PSReadLineOption -EditMode Emacs
 $PSStyle.FileInfo.Directory = ""
+
+if (Get-Command "starship" -ErrorAction SilentlyContinue) 
+{
+  Invoke-Expression (&starship init powershell)
+}
