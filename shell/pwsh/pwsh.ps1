89ad7e97ce5226a9e81415ea4257a9d3c855f6f2
AmirrezaAsk <raskarpour@gmail.com>
2025-01-28 02:27:18 +0330

    Disable powershell beep

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -1,5 +1,7 @@
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
+
 $PSStyle.FileInfo.Directory = ""
 
 if (Get-Command "starship" -ErrorAction SilentlyContinue) 
