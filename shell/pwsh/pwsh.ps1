cac68f1f15063a2d7cdd54b4115191951b953b4c
AmirrezaAsk <raskarpour@gmail.com>
2025-01-28 02:18:05 +0330

    Disable posh-git

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -1,16 +1,4 @@
 Import-Module PSReadLine
-Import-Module posh-git
+# Import-Module posh-git
 Set-PSReadLineOption -EditMode Emacs
-# Set-PSReadLineOption -PredictionSource History
-# Set-PSReadLineOption -PredictionViewStyle ListView
 $PSStyle.FileInfo.Directory = ""
-
-# if (Get-Command "starship" -ErrorAction SilentlyContinue) 
-# {
-#   $ENV:STARSHIP_CONFIG = "D:\src\dotfiles\shell\starship\starship.toml"
-#   $ENV:STARSHIP_DISTRO = "者 Windows"
-#   Invoke-Expression (&starship init powershell)
-# }
-
-New-Alias -Name gvim -Value neovide
-New-Alias -Name vim -Value nvim
