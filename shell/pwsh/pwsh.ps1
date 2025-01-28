a6c1a834fdb57ada4bc191e948faf5326787e751
AmirrezaAsk <raskarpour@gmail.com>
2025-01-28 21:19:07 +0330

    OMP

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -4,7 +4,9 @@ Set-PSReadlineOption -BellStyle None
 
 $PSStyle.FileInfo.Directory = ""
 
-if (Get-Command "starship" -ErrorAction SilentlyContinue) 
-{
-  Invoke-Expression (&starship init powershell)
-}
+# if (Get-Command "starship" -ErrorAction SilentlyContinue) 
+# {
+#   Invoke-Expression (&starship init powershell)
+# }
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
