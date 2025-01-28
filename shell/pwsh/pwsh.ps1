ba5235fa6536f90a312e43489783ca76ed2eee42
AmirrezaAsk <raskarpour@gmail.com>
2025-01-29 00:46:02 +0330

    Simpler prompt

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -4,9 +4,6 @@ Set-PSReadlineOption -BellStyle None
 
 $PSStyle.FileInfo.Directory = ""
 
-# if (Get-Command "starship" -ErrorAction SilentlyContinue) 
-# {
-#   Invoke-Expression (&starship init powershell)
-# }
-
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
+function prompt {
+  "$($executionContext.SessionState.Path.CurrentLocation)> ";
+}
