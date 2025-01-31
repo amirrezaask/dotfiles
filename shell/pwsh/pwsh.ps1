2f28cadc6fb22b635415877271dd26b1883a7fec
AmirrezaAsk <raskarpour@gmail.com>
2025-01-31 18:48:43 +0330

    OMP

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -4,7 +4,10 @@ Set-PSReadlineOption -BellStyle None
 
 $PSStyle.FileInfo.Directory = ""
 
-function prompt {
-  Write-Host -NoNewLine -ForegroundColor cyan "$($executionContext.SessionState.Path.CurrentLocation)>";
-  return " "
+# function prompt {
+#   Write-Host -NoNewLine -ForegroundColor cyan "$($executionContext.SessionState.Path.CurrentLocation)>";
+#   return " "
+# }
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
 }
