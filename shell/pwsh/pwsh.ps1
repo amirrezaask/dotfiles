21a8047532b4d1860024c6226ee900f3ba526fe7
AmirrezaAsk <raskarpour@gmail.com>
2025-02-03 00:46:33 +0330

    Powershell

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -3,11 +3,10 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadlineOption -BellStyle None
 
 $PSStyle.FileInfo.Directory = ""
-
-# function prompt {
-#   Write-Host -NoNewLine -ForegroundColor cyan "$($executionContext.SessionState.Path.CurrentLocation)>";
-#   return " "
-# }
+function reload() {
+  . $PROFILE
+}
 if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
+  # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
 }
