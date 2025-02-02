374b0f87b447385709be1e906cc3c8a92a79a1d9
AmirrezaAsk <raskarpour@gmail.com>
2025-02-02 23:57:28 +0330

    Change oh my posh theme

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -9,5 +9,5 @@ $PSStyle.FileInfo.Directory = ""
 #   return " "
 # }
 if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
 }
