42a145db1a04419035d9bd720a8e8f5ccb62329d
AmirrezaAsk <raskarpour@gmail.com>
2025-02-03 01:04:35 +0330

    Adds battery function to powershell to show battery report in windows

--- shell/pwsh/pwsh.ps1
+++ shell/pwsh/pwsh.ps1
@@ -7,6 +7,11 @@ function reload() {
   . $PROFILE
 }
 
+function battery() {
+  powercfg /batteryreport
+  start .\battery-report.html
+}
+
 if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
   # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
   oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
