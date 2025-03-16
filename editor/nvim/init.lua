8ed0a927e847b5d6a1e4e9e8b92c2b58eb5a7843
amirrezaask <raskarpour@gmail.com>
2025-03-16 17:45:46 +0330

    Work In Progress

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -2,8 +2,7 @@ require("core.sets")
 require("core.keymaps")
 require("lazy_init")
 
-vim.cmd.colorscheme("gruvbox")
-
+vim.cmd.colorscheme(os.getenv("NVIM_COLORSCHEME") or "rose-pine-moon")
 -- if true then
 -- 	vim.cmd([[
 --         hi! Normal guibg=none
