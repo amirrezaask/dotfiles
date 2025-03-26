5fc63172ed5a9fdcb4ac7598143f9d765ebdca58
amirrezaask <raskarpour@gmail.com>
2025-03-26 21:04:25 +0330

    Work In Progress 2025-03-26 21:04:25

--- scripts/configure-neovim.sh
+++ scripts/configure-neovim.sh
@@ -2,8 +2,6 @@
 SCRIPT_PATH=`dirname $0`
 DOTFILES_PATH=`dirname $SCRIPT_PATH`
 
-
-exit 0
 RELEASE="master"
 
 if ! test -d "$HOME/.neovim"; then
@@ -15,9 +13,6 @@ git pull
 git checkout "{$RELEASE}"
 make CMAKE_BUILD_TYPE=Release
 popd
-
-git submodule update --init --recursive # download my neovim configuration
-
 mkdir -p $HOME/.config
 
 ln -s $DOTFILES_PATH/editor/nvim $HOME/.config/nvim
