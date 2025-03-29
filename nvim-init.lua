be33ac3a00e75089e935d84e49c78e99faaf7961
amirrezaask <raskarpour@gmail.com>
2025-03-30 03:19:13 +0330

    Trimming the tree

--- nvim-init.lua
+++ nvim-init.lua
@@ -125,20 +125,6 @@ require("lazy").setup({
             end, {})
         end
     },
-    { -- Git Client
-        "tpope/vim-fugitive",
-        config = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = "fugitive",
-                callback = function(ev)
-                    vim.keymap.set("n", "P", "<cmd>Git push<CR>", { buffer = ev.buf })
-                    vim.keymap.set("n", "p", "<cmd>Git pull --rebase<CR>", { buffer = ev.buf })
-                    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-                    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
-                end,
-            })
-        end,
-    },
     { -- Mason: Install lsp servers/formatters/etc.
         "williamboman/mason-lspconfig.nvim",
         opts = {},
