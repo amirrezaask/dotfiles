1d34465db30a71a2b4f608c32961d946462cd503
amirrezaask <raskarpour@gmail.com>
2025-04-03 17:08:39 +0330

    Work In Progress 2025-04-03 17:08:39

--- nvim-init.lua
+++ nvim-init.lua
@@ -18,6 +18,7 @@ require("paq")({
 	{ "rose-pine/neovim", as = "rose-pine" },
 	{ "catppuccin/nvim", as = "catppuccin" },
 	"amirrezaask/sitruuna.nvim",
+	"amirrezaask/nvim-terminal",
 })
 
 local TRANSPARENT = true
@@ -25,7 +26,8 @@ local TRANSPARENT = true
 require("rose-pine").setup({ dark_variant = "moon", styles = { italic = false, transparency = TRANSPARENT } })
 require("catppuccin").setup({ transparent_background = TRANSPARENT })
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("sitruuna")
+
 vim.cmd([[
     hi Normal guibg=none
     hi NormalFloat guibg=none
@@ -87,6 +89,9 @@ vim.keymap.set("n", "<M-Left>", "<c-w>5>")
 vim.keymap.set("n", "<M-Right>", "<c-w>5<")
 vim.keymap.set("n", "<M-Up>", "<C-W>+")
 vim.keymap.set("n", "<M-Down>", "<C-W>-")
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-w><C-w>", "<cmd>wincmd w<cr>")
+vim.keymap.set({ "n", "t" }, "<C-j>", require("nvim-terminal")("bottom"))
 -- [[ Quick fix list
 local qflist = false
 vim.keymap.set("n", "<C-q>", function()
@@ -174,6 +179,7 @@ lspconfig.lua_ls.setup({
 		},
 	},
 })
+
 require("conform").setup({
 	format_on_save = function()
 		if vim.tbl_contains({ "php" }, vim.bo.filetype) then
@@ -243,83 +249,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.lsp.completion.enable(true, args.data.client_id, args.buf, { wutotrigger = false }) -- setup completion menu
 	end,
 })
-
--- [[ Terminal
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
-vim.keymap.set("t", "<C-w><C-w>", "<cmd>wincmd w<cr>")
-local terminal_state = { buf = -1, win = -1, last_tab = -1 }
-local terminal_location = "float"
-vim.keymap.set({ "n", "t" }, "<c-j>", function()
-	if terminal_location == "float" or terminal_location == "bottom" then
-		if vim.api.nvim_buf_is_valid(terminal_state.buf) and vim.api.nvim_win_is_valid(terminal_state.win) then
-			vim.api.nvim_win_hide(terminal_state.win)
-			return
-		end
-	end
-
-	if terminal_location == "tab" then
-		local current_win = vim.api.nvim_get_current_win()
-		if vim.wo[current_win].winbar == "Terminal" then
-			vim.api.nvim_set_current_tabpage(terminal_state.last_tab)
-			return
-		end
-		for _, tab_id in ipairs(vim.api.nvim_list_tabpages()) do
-			local win_id = vim.api.nvim_tabpage_get_win(tab_id)
-			local buf_id = vim.api.nvim_win_get_buf(win_id)
-			if vim.wo[win_id].winbar == "Terminal" and vim.bo[buf_id].buftype == "terminal" then
-				terminal_state.last_tab = vim.api.nvim_get_current_tabpage()
-				vim.api.nvim_set_current_tabpage(tab_id)
-				vim.cmd.startinsert()
-				return
-			end
-		end
-		terminal_state.last_tab = vim.api.nvim_get_current_tabpage()
-		vim.cmd.tabnew()
-		local win_id = vim.api.nvim_get_current_win()
-		vim.wo[win_id].winbar = "Terminal"
-		vim.cmd.term()
-		vim.cmd.startinsert()
-		return
-	end
-
-	if not vim.api.nvim_buf_is_valid(terminal_state.buf) then
-		print("creating floating term buffer")
-		terminal_state.buf = vim.api.nvim_create_buf(false, true)
-	end
-
-	if terminal_location == "float" then
-		local height = math.floor(vim.o.lines * 0.8)
-		local width = math.floor(vim.o.columns * 0.8)
-
-		local row = math.floor((vim.o.lines - height) / 2)
-		local col = math.floor((vim.o.columns - width) / 2)
-
-		terminal_state.win = vim.api.nvim_open_win(terminal_state.buf, true, {
-			relative = "editor",
-			width = width,
-			height = height,
-			row = row,
-			col = col,
-			style = "minimal",
-			border = "rounded",
-		})
-	elseif terminal_location == "bottom" then
-		local width = vim.o.columns
-		local height = math.floor(vim.o.lines * 0.45)
-		terminal_state.win = vim.api.nvim_open_win(terminal_state.buf, true, {
-			split = "below",
-			width = width,
-			height = height,
-		})
-	else
-		vim.error("Invalid location for terminal")
-		return
-	end
-
-	if vim.api.nvim_get_option_value("buftype", { buf = terminal_state.buf }) ~= "terminal" then
-		vim.cmd.term()
-	end
-
-	vim.cmd.startinsert()
-end)
--- ]]
