97c146889e847bf1dbdfd51b12e8e1db0a392d3f
amirrezaask <raskarpour@gmail.com>
2025-04-13 00:14:46 +0330

    updates

--- nvim-init.lua
+++ nvim-init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.o.wrap = true
 vim.o.breakindent = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undofile = true
@@ -15,10 +15,11 @@ vim.o.timeoutlen = 300
 vim.o.updatetime = 250
 vim.o.number = true
 vim.o.clipboard = "unnamedplus"
-vim.o.ignorecase = true; vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.completeopt = "fuzzy,menu,noinsert,noselect,popup"
 vim.o.statusline = "[%l:%c]%=%m%r%q%h%f%=%y"
-vim.o.winborder = 'rounded'
+vim.o.winborder = "rounded"
 vim.o.cursorline = true
 local map = vim.keymap.set
 map("n", "Y", "^v$y", { desc = "Copy whole line" })
@@ -31,99 +32,125 @@ map("n", "N", "Nzz")
 map("i", "jk", "<ESC>")
 map("i", "kj", "<ESC>")
 map("n", "<CR>", [[ {-> v:hlsearch ? ':nohl<CR>' : '<CR>'}() ]], { expr = true })
-map("n", "j", "gj"); vim.keymap.set("n", "k", "gk")
+map("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
 map("t", "<C-w><C-w>", "<cmd>wincmd w<cr>")
 map("n", "<leader>i", ":edit $MYVIMRC<CR>")
 map("n", "<C-q>", function()
-    local wins = vim.api.nvim_list_wins()
-    local has_qf_open = false
-    for _, win in ipairs(wins) do
-        local buf = vim.api.nvim_win_get_buf(win)
-        if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "quickfix" then
-            has_qf_open = true
-        end
-    end
-    if has_qf_open then
-        vim.cmd([[ cclose ]])
-    else
-        vim.cmd([[ copen ]])
-    end
+	local wins = vim.api.nvim_list_wins()
+	local has_qf_open = false
+	for _, win in ipairs(wins) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "quickfix" then
+			has_qf_open = true
+		end
+	end
+	if has_qf_open then
+		vim.cmd([[ cclose ]])
+	else
+		vim.cmd([[ copen ]])
+	end
 end, { desc = "Toggle Quickfix list" })
 vim.keymap.set("n", "{", "<cmd>cprev<CR>")
 vim.keymap.set("n", "}", "<cmd>cnext<CR>")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
-    { "folke/snacks.nvim", dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = { picker = { enabled = true, sources = { files = { layout = { preview = false } }, git_files = { layout = { preview = false } } } } } },
-    { "folke/tokyonight.nvim" },
-    { "rose-pine/neovim",        name = "rose-pine" },
-    { "neovim/nvim-lspconfig" },
-    { "williamboman/mason.nvim", opts = { ensure_installed = { "gopls" } } },
-    { "saghen/blink.cmp", version = "1.*",
-        opts = { keymap = { preset = "enter" }, cmdline = { enabled = false } } },
-    { "nvim-treesitter/nvim-treesitter", config = function() require("nvim-treesitter.configs").setup { ensure_installed = { "lua", "go", "gomod", "php" }, highlight = { enable = true } } end },
+	{
+		"folke/snacks.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			picker = {
+				enabled = true,
+				sources = { files = { layout = { preview = false } }, git_files = { layout = { preview = false } } },
+			},
+		},
+	},
+	{ "folke/tokyonight.nvim" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "neovim/nvim-lspconfig" },
+	{ "stevearc/conform.nvim" },
+	{ "williamboman/mason.nvim", opts = { ensure_installed = { "gopls" } } },
+	{
+		"saghen/blink.cmp",
+		version = "1.*",
+		opts = { keymap = { preset = "enter" }, cmdline = { enabled = false } },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup {
+				ensure_installed = { "lua", "go", "gomod", "php" },
+				highlight = { enable = true },
+			}
+		end,
+	},
 }
 
 vim.cmd.colorscheme(vim.env.NVIM_COLORSCHEME or "tokyonight-night")
 
-for _, lsp in ipairs({ "gopls", "intelephense", "rust_analyzer", "zls" }) do require("lspconfig")[lsp].setup {} end
+for _, lsp in ipairs({ "gopls", "intelephense", "rust_analyzer", "zls" }) do
+	require("lspconfig")[lsp].setup {}
+end
 require("lspconfig").lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function()
-                local bufnr = vim.api.nvim_get_current_buf()
-                for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
-                    if client.supports_method("textDocument/formatting") then
-                        vim.lsp.buf.format({ bufnr = bufnr })
-                    end
-                    if client.supports_method("textDocument/codeAction") then
-                        vim.lsp.buf.code_action({
-                            context = { only = { "source.organizeImports" } },
-                            apply = true,
-                        })
-                    end
-                end
-            end,
-        })
-        map("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, { buffer = args.buf })
-        map("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, { buffer = args.buf })
-        map("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf })
-        map("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-        map("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
-        map("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
-        map("n", "gi", vim.lsp.buf.implementation, { buffer = args.buf })
-        map("n", "R", vim.lsp.buf.rename, { buffer = args.buf })
-        map("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
-        map("n", "C", vim.lsp.buf.code_action, { buffer = args.buf })
-        map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, { buffer = args.buf })
-        map("n", "<leader>l", vim.diagnostic.open_float, { buffer = args.buf })
-        map("n", "<leader>q", vim.diagnostic.setloclist, { buffer = args.buf })
-    end,
+	callback = function(args)
+		map("n", "[[", function()
+			vim.diagnostic.jump({ count = -1 })
+		end, { buffer = args.buf })
+		map("n", "]]", function()
+			vim.diagnostic.jump({ count = 1 })
+		end, { buffer = args.buf })
+		map("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf })
+		map("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
+		map("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
+		map("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
+		map("n", "gi", vim.lsp.buf.implementation, { buffer = args.buf })
+		map("n", "R", vim.lsp.buf.rename, { buffer = args.buf })
+		map("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+		map("n", "C", vim.lsp.buf.code_action, { buffer = args.buf })
+		map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, { buffer = args.buf })
+		map("n", "<leader>l", vim.diagnostic.open_float, { buffer = args.buf })
+		map("n", "<leader>q", vim.diagnostic.setloclist, { buffer = args.buf })
+	end,
+})
+
+require("conform").setup({
+	format_on_save = function()
+		if vim.tbl_contains({ "php" }, vim.bo.filetype) then
+			return false
+		end
+		return {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		}
+	end,
+	formatters_by_ft = { lua = { "stylua", lsp_format = "fallback" }, go = { "goimports", "gofmt" } },
 })
 
 Snacks = require("snacks")
 P = Snacks.picker
 map("n", "<leader><leader>", P.files, {})
 map("n", "<C-p>", P.git_files, {})
-map("n", "<leader>fd", function() P.files { cwd = "~/.dotfiles" } end, {})
+map("n", "<leader>fd", function()
+	P.files { cwd = "~/.dotfiles" }
+end, {})
 map("n", "??", P.grep, {})
 map("v", "??", P.grep_word, {})
 map("n", "<leader>h", P.help, {})
@@ -131,3 +158,4 @@ map("n", "<leader>d", P.diagnostics_buffer, {})
 map("n", "<leader>D", P.diagnostics, {})
 map("n", "<leader>o", P.lsp_symbols, {})
 map("n", "<leader>O", P.lsp_workspace_symbols, {})
+map({ "n", "t" }, "<C-j>", Snacks.terminal.toggle, {})
