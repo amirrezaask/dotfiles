c776e73dcda2a48ac08ef666e97502d0ef817081
amirrezaask <raskarpour@gmail.com>
2025-04-12 03:34:44 +0330

    Work In Progress 2025-04-12 03:34:44

--- nvim-init.lua
+++ nvim-init.lua
@@ -1,18 +1,29 @@
-vim.g.mapleader = " " -- <leader> key for keymaps mapped to <Space>
-vim.o.wrap = true; vim.o.breakindent = true -- Line wrapping
-vim.o.swapfile = false; vim.o.backup = false; vim.o.undofile = true 
-vim.o.splitbelow = true; vim.o.splitright = true
-vim.o.showmode = false -- don't show --INSERT-- in command line.
-vim.o.sw = 4; vim.o.ts = 4; vim.o.expandtab = true -- TABs and indentation
-vim.o.timeoutlen = 300; vim.o.updatetime = 250 -- Milliseconds to wait for CursorHold autocmds to fire.
-vim.o.number = true -- Line numbers
-vim.o.clipboard = "unnamedplus" -- Clipboard
+vim.g.mapleader = " "
+vim.o.wrap = true
+vim.o.breakindent = true
+vim.o.signcolumn = 'yes'
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.showmode = false
+vim.o.sw = 4
+vim.o.ts = 4
+vim.o.expandtab = true
+vim.o.timeoutlen = 300
+vim.o.updatetime = 250
+vim.o.number = true
+vim.o.clipboard = "unnamedplus"
 vim.o.ignorecase = true; vim.o.smartcase = true
 vim.o.completeopt = "fuzzy,menu,noinsert,noselect,popup"
-vim.o.statusline = "%l:%c %m%r%q%h%f"
+vim.o.statusline = "[%l:%c] %m%r%q%h%f"
 vim.o.winborder = 'rounded'
 vim.keymap.set("n", "Y", "^v$y", { desc = "Copy whole line" })
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]]); vim.keymap.set("i", "<C-c>", "<esc>"); vim.keymap.set("i", "jk", "<ESC>"); vim.keymap.set("i", "kj", "<ESC>") -- Exiting insert mode
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+vim.keymap.set("i", "<C-c>", "<esc>")
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
 vim.keymap.set("n", "Q", "<cmd>q<CR>")
 vim.keymap.set("n", "<CR>", [[ {-> v:hlsearch ? ':nohl<CR>' : '<CR>'}() ]], { expr = true })
 vim.keymap.set("n", "j", "gj"); vim.keymap.set("n", "k", "gk")
@@ -35,6 +46,7 @@ vim.keymap.set("n", "<C-q>", function()
 end, { desc = "Toggle Quickfix list" })
 vim.keymap.set("n", "{", "<cmd>cprev<CR>")
 vim.keymap.set("n", "}", "<cmd>cnext<CR>")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -52,79 +64,56 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
-	{ "folke/tokyonight.nvim" },
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-
-			for _, lsp in ipairs({ "gopls", "ols", "intelephense", "rust_analyzer", "zls", "lua_ls" }) do lspconfig[lsp].setup {} end
-			lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(args)
-					local bufnr = args.buf
-					vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-
-					local map = function(mode, key, fn, desc) vim.keymap.set(mode, key, fn, { buffer = bufnr, desc = "LSP: " .. desc }) end
-
-					map("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, "Diagnostics: Next")
-					map("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, "Diagnostics: Previous")
-					map("n", "C-]", vim.lsp.buf.definition, "[g]oto definition")
-					map("n", "gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
-					map("n", "gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
-					map("n", "gi", vim.lsp.buf.references, "[g]oto [i]mplementation")
-					map("n", "gr", vim.lsp.buf.implementation, "[g]oto [r]eferences")
-					map("n", "R", vim.lsp.buf.rename, "Rename")
-					map("n", "K", vim.lsp.buf.hover, "Hover")
-					map("n", "C", vim.lsp.buf.code_action, "Code Actions")
-					map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, "Signature Help")
-					map("n", "<leader>l", vim.diagnostic.open_float, "Diagnostics: Open float window")
-					map("n", "<leader>q", vim.diagnostic.setloclist, "Set Local list")
-					vim.diagnostic.config({
-						enabled = true,
-						virtual_text = false,
-					})
-
-					vim.keymap.set("i", "<CR>", function()
-						return vim.fn.pumvisible() == 1 and "<C-y>" or "<CR>"
-					end, { expr = true, noremap = true })
-
-					vim.lsp.completion.enable(true, args.data.client_id, args.buf, { wutotrigger = false }) -- setup completion menu
-				end,
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim", config = function()
-            require("mason").setup({ ensure_installed = { "gopls" } })
-			local process_path = os.getenv("PATH")
-			vim.fn.setenv("PATH", process_path .. ":" .. vim.fn.stdpath("data") .. "/mason/bin")
-		end,
-	},
-	{ "saghen/blink.cmp", version = "1.*", opts = { keymap = { preset = "enter" }, cmdline = { enabled = false } } },
-	{ "nvim-treesitter/nvim-treesitter", config = function() require("nvim-treesitter.configs").setup { ensure_installed = { "lua", "go", "gomod", "php" }, highlight = { enable = true } } end },
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local fzflua = require "fzf-lua"
-			fzflua.setup { winopts = { preview = { hidden = true } } }
-			vim.keymap.set("n", "<leader><leader>", fzflua.files, {})
-			vim.keymap.set("n", "<C-p>", fzflua.git_files, {})
-			vim.keymap.set("n", "<leader>fd", function()
-				fzflua.files { cwd = "~/.dotfiles" }
-			end, {})
-			vim.keymap.set("n", "??", fzflua.grep, {})
-			vim.keymap.set("n", "<leader>fb", fzflua.buffers, {})
-			vim.keymap.set("n", "<leader>h", fzflua.helptags, {})
-			vim.keymap.set("n", "<leader>d", fzflua.diagnostics_document, {})
-			vim.keymap.set("n", "<leader>D", fzflua.diagnostics_workspace, {})
-			vim.keymap.set("n", "<leader>o", fzflua.lsp_document_symbols, {})
-			vim.keymap.set("n", "<leader>O", fzflua.lsp_workspace_symbols, {})
-		end,
-	},
+    "folke/tokyonight.nvim",
+    { "rose-pine/neovim", name = "rose-pine" },
+	"neovim/nvim-lspconfig", "williamboman/mason.nvim" ,
+    { "saghen/blink.cmp", version = "1.*",  },
+    { "nvim-treesitter/nvim-treesitter" },
+	{ "ibhagwan/fzf-lua" },
 }
 
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("rose-pine")
+
+require("blink.cmp").setup  { keymap = { preset = "enter" }, cmdline = { enabled = false } }
+
+require("nvim-treesitter.configs").setup { ensure_installed = { "lua", "go", "gomod", "php" }, highlight = { enable = true } }
+
+local lspconfig = require("lspconfig")
+
+for _, lsp in ipairs({ "gopls", "ols", "intelephense", "rust_analyzer", "zls" }) do lspconfig[lsp].setup {} end
+lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
+
+vim.api.nvim_create_autocmd("LspAttach", { callback = function(args)
+        local bufnr = args.buf
+        local map = function(mode, key, fn, desc) vim.keymap.set(mode, key, fn, { buffer = bufnr, desc = "LSP: " .. desc }) end
+
+        map("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, "Diagnostics: Next")
+        map("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, "Diagnostics: Previous")
+        map("n", "C-]", vim.lsp.buf.definition, "[g]oto definition")
+        map("n", "gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
+        map("n", "gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+        map("n", "gi", vim.lsp.buf.references, "[g]oto [i]mplementation")
+        map("n", "gr", vim.lsp.buf.implementation, "[g]oto [r]eferences")
+        map("n", "R", vim.lsp.buf.rename, "Rename")
+        map("n", "K", vim.lsp.buf.hover, "Hover")
+        map("n", "C", vim.lsp.buf.code_action, "Code Actions")
+        map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, "Signature Help")
+        map("n", "<leader>l", vim.diagnostic.open_float, "Diagnostics: Open float window")
+        map("n", "<leader>q", vim.diagnostic.setloclist, "Set Local list")
+    end,
+})
+
+local fzflua = require "fzf-lua"
+fzflua.setup { files = { previewer = false } }
+vim.keymap.set("n", "<leader><leader>", fzflua.files, {})
+vim.keymap.set("n", "<C-p>", fzflua.git_files, {})
+vim.keymap.set("n", "<leader>fd", function() fzflua.files { cwd = "~/.dotfiles" } end, {})
+vim.keymap.set("n", "??", fzflua.live_grep, {})
+vim.keymap.set("n", "<leader>h", fzflua.helptags, {})
+vim.keymap.set("n", "<leader>d", fzflua.diagnostics_document, {})
+vim.keymap.set("n", "<leader>D", fzflua.diagnostics_workspace, {})
+vim.keymap.set("n", "<leader>o", fzflua.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>O", fzflua.lsp_workspace_symbols, {})
+
+require("mason").setup({ ensure_installed = { "gopls" } })
+vim.fn.setenv("PATH", os.getenv("PATH") .. ":" .. vim.fn.stdpath("data") .. "/mason/bin")
