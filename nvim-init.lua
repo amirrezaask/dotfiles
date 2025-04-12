0a04db7883996c58a3b670104effb389de93e8e9
amirrezaask <raskarpour@gmail.com>
2025-04-12 03:40:30 +0330

    Work In Progress 2025-04-12 03:40:30

--- nvim-init.lua
+++ nvim-init.lua
@@ -8,8 +8,8 @@ vim.o.undofile = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.showmode = false
-vim.o.sw = 4
-vim.o.ts = 4
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.timeoutlen = 300
 vim.o.updatetime = 250
@@ -19,17 +19,22 @@ vim.o.ignorecase = true; vim.o.smartcase = true
 vim.o.completeopt = "fuzzy,menu,noinsert,noselect,popup"
 vim.o.statusline = "[%l:%c] %m%r%q%h%f"
 vim.o.winborder = 'rounded'
-vim.keymap.set("n", "Y", "^v$y", { desc = "Copy whole line" })
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
-vim.keymap.set("i", "<C-c>", "<esc>")
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("i", "kj", "<ESC>")
-vim.keymap.set("n", "Q", "<cmd>q<CR>")
-vim.keymap.set("n", "<CR>", [[ {-> v:hlsearch ? ':nohl<CR>' : '<CR>'}() ]], { expr = true })
-vim.keymap.set("n", "j", "gj"); vim.keymap.set("n", "k", "gk")
-vim.keymap.set("t", "<C-w><C-w>", "<cmd>wincmd w<cr>")
-vim.keymap.set("n", "<leader>i", ":edit $MYVIMRC<CR>")
-vim.keymap.set("n", "<C-q>", function()
+local map = vim.keymap.set
+map("n", "Y", "^v$y", { desc = "Copy whole line" })
+map("t", "<esc>", [[<C-\><C-n>]])
+map("i", "<C-c>", "<esc>")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
+map("n", "Q", "<cmd>q<CR>")
+map("n", "<CR>", [[ {-> v:hlsearch ? ':nohl<CR>' : '<CR>'}() ]], { expr = true })
+map("n", "j", "gj"); vim.keymap.set("n", "k", "gk")
+map("t", "<C-w><C-w>", "<cmd>wincmd w<cr>")
+map("n", "<leader>i", ":edit $MYVIMRC<CR>")
+map("n", "<C-q>", function()
 	local wins = vim.api.nvim_list_wins()
 	local has_qf_open = false
 	for _, win in ipairs(wins) do
@@ -72,7 +77,7 @@ require("lazy").setup {
 	{ "ibhagwan/fzf-lua" },
 }
 
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("tokyonight-night")
 
 require("blink.cmp").setup  { keymap = { preset = "enter" }, cmdline = { enabled = false } }
 
@@ -84,22 +89,19 @@ for _, lsp in ipairs({ "gopls", "ols", "intelephense", "rust_analyzer", "zls" })
 lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
 
 vim.api.nvim_create_autocmd("LspAttach", { callback = function(args)
-        local bufnr = args.buf
-        local map = function(mode, key, fn, desc) vim.keymap.set(mode, key, fn, { buffer = bufnr, desc = "LSP: " .. desc }) end
-
-        map("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, "Diagnostics: Next")
-        map("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, "Diagnostics: Previous")
-        map("n", "C-]", vim.lsp.buf.definition, "[g]oto definition")
-        map("n", "gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
-        map("n", "gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
-        map("n", "gi", vim.lsp.buf.references, "[g]oto [i]mplementation")
-        map("n", "gr", vim.lsp.buf.implementation, "[g]oto [r]eferences")
-        map("n", "R", vim.lsp.buf.rename, "Rename")
-        map("n", "K", vim.lsp.buf.hover, "Hover")
-        map("n", "C", vim.lsp.buf.code_action, "Code Actions")
-        map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, "Signature Help")
-        map("n", "<leader>l", vim.diagnostic.open_float, "Diagnostics: Open float window")
-        map("n", "<leader>q", vim.diagnostic.setloclist, "Set Local list")
+        map("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, { buffer = args.buf })
+        map("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, { buffer = args.buf })
+        map("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf })
+        map("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
+        map("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
+        map("n", "gi", vim.lsp.buf.references, { buffer = args.buf })
+        map("n", "gr", vim.lsp.buf.implementation, { buffer = args.buf })
+        map("n", "R", vim.lsp.buf.rename, { buffer = args.buf })
+        map("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+        map("n", "C", vim.lsp.buf.code_action, { buffer = args.buf })
+        map({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, { buffer = args.buf })
+        map("n", "<leader>l", vim.diagnostic.open_float, { buffer = args.buf })
+        map("n", "<leader>q", vim.diagnostic.setloclist, { buffer = args.buf })
     end,
 })
 
