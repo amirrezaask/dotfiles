bef88ce656c84b099e04ac799783bd18a35e6b7a
amirrezaask <raskarpour@gmail.com>
2025-04-16 04:51:29 +0330

    Updates ?

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,4 +25,15 @@ require("lazy").setup({ import = "plugins.spec" }, {
     },
 })
 
+function Transparent()
+    vim.cmd [[
+        hi Normal guibg=none
+        hi NormalFloat guibg=none
+        hi LineNr guibg=none
+        hi SignColumn guibg=none
+        hi WinBorder guibg=none
+    ]]
+end
+
 vim.cmd.colorscheme(vim.env.NVIM_COLORSCHEME or "gruvbuddy")
+Transparent()
