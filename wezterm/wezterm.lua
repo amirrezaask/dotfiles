65f5c88aacdd531803120798f8a56b6e0bfc10e5
amirrezaask <raskarpour@gmail.com>
2025-04-27 22:17:23 +0330

    2025-04-27 22:17:23

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 local config = wezterm.config_builder()
-config.color_scheme = 'tokyonight_night'
+-- #1DA457
+
 config.tab_bar_at_bottom = true
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.use_fancy_tab_bar = false
+
 return config
