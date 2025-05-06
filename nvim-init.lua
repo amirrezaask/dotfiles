2d010b53cd8c002a37990da3e6a74a0b388e1adc
amirrezaask <raskarpour@gmail.com>
2025-05-07 00:31:06 +0330

    clean up neovim

--- nvim-init.lua
+++ nvim-init.lua
@@ -32,8 +32,103 @@ function _G.titlestring()
 end
 
 vim.o.titlestring = "%M%{v:lua.titlestring()}" -- Set title of the terminal.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+vim.keymap.set("n", "Y", "^v$y", { desc = "Copy whole line" })
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+vim.keymap.set("i", "<C-c>", "<esc>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n" }, "<C-j>", "<C-w>j") -- Window navigation
+vim.keymap.set({ "n" }, "<C-k>", "<C-w>k") -- Window navigation
+vim.keymap.set({ "n" }, "<C-h>", "<C-w>h") -- Window navigation
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>l") -- Window navigation
+
+vim.keymap.set({ "t" }, "<C-j>", "<C-\\><C-n><C-w>j", { noremap = true }) -- Window navigation
+vim.keymap.set({ "t" }, "<C-k>", "<C-\\><C-n><C-w>k", { noremap = true }) -- Window navigation
+vim.keymap.set({ "t" }, "<C-h>", "<C-\\><C-n><C-w>h", { noremap = true }) -- Window navigation
+vim.keymap.set({ "t" }, "<C-l>", "<C-\\><C-n><C-w>l", { noremap = true }) -- Window navigation
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
+vim.keymap.set(
+  "n",
+  "<CR>",
+  function() -- Taken from https://github.com/tjdevries/config.nvim/blob/master/plugin/keymaps.lua#L16
+    if vim.v.hlsearch == 1 then
+      vim.cmd.nohl()
+      return ""
+    else
+      ---@diagnostic disable-next-line: undefined-field
+      return vim.keycode "<CR>"
+    end
+  end,
+  { expr = true }
+)
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "{", "<cmd>cprev<CR>")
+vim.keymap.set("n", "}", "<cmd>cnext<CR>")
+vim.keymap.set({ "n", "t" }, "<M-k>", "<cmd>wincmd q<CR>")
+vim.keymap.set("n", "<C-q>", function()
+  local wins = vim.api.nvim_list_wins()
+  for _, win in ipairs(wins) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "quickfix" then
+      vim.cmd.cclose()
+      return
+    end
+  end
+  vim.cmd.copen()
+end)
+
+local function ftplugin(filetype, callback)
+  local ft = vim.api.nvim_create_augroup("ftplugin_" .. filetype, {})
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetype,
+    group = ft,
+    callback = callback,
+  })
+end
+
+ftplugin("go", function()
+  vim.bo[0].sw = 4
+  vim.bo[0].ts = 4
+  vim.bo[0].expandtab = false
+  vim.bo[0].shiftwidth = 4
+end)
+
+ftplugin("lua", function()
+  vim.bo[0].sw = 2
+  vim.bo[0].ts = 2
+  vim.bo[0].expandtab = true
+  vim.bo[0].shiftwidth = 2
+end)
+
+ftplugin("ocaml", function()
+  vim.bo[0].sw = 2
+  vim.bo[0].ts = 2
+  vim.bo[0].expandtab = true
+  vim.bo[0].shiftwidth = 2
+end)
+
+ftplugin("php", function()
+  vim.bo[0].sw = 4
+  vim.bo[0].ts = 4
+  vim.bo[0].expandtab = true
+  vim.bo[0].shiftwidth = 4
+end)
+
+ftplugin("sh", function()
+  vim.bo[0].sw = 2
+  vim.bo[0].ts = 2
+  vim.bo[0].expandtab = true
+  vim.bo[0].shiftwidth = 2
+end)
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -48,28 +143,48 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
 vim.o.rtp = vim.o.rtp .. "," .. lazypath
 
 require("lazy").setup({
-  { "amirrezaask/nvim-gruvbuddy.lua" }, -- Colorscheme, inspired by great @tjdevries.
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      styles = {
-        italic = false,
-        transparency = true,
+    "amirrezaask/nvim-gruvbuddy.lua", -- Colorscheme, inspired by great @tjdevries.
+    dependencies = {
+
+      {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        opts = {
+          styles = {
+            italic = false,
+            transparency = true,
+          },
+        },
       },
+      { "folke/tokyonight.nvim" },
     },
-  }, -- Colorscheme.
-  { "folke/tokyonight.nvim" }, -- Colorscheme.
+
+    config = function()
+      vim.g.gruvbuddy_style = "default"
+      vim.cmd.colorscheme("tokyonight-moon")
+    end,
+  },
 
   {
     "amirrezaask/nvim-statusline.lua",
+    enabled = false,
     config = function()
       require("statusline").setup()
+    end,
+  },
+
+  {
+    "echasnovski/mini.nvim",
+
+    config = function()
+      require("mini.ai").setup()
+      require("mini.comment").setup()
+      require("mini.move").setup() -- M-h M-j M-k M-l move code blocks around.
+      require("mini.statusline").setup()
     end,
   },
 
@@ -125,7 +240,7 @@ require("lazy").setup({
       cmdline = { enabled = false },
     },
   }, -- Blazingly fast autocomplete
-  {
+  { -- Autoformat/fixes
     "stevearc/conform.nvim",
     config = function()
       require("conform").setup({
@@ -144,10 +259,9 @@ require("lazy").setup({
         end,
       })
     end,
-  }, -- Autoformat/fixes
+  },
 
-  -- Fuzzy Finders
-  {
+  { -- Collection of plugins by folkee.
     "folke/snacks.nvim",
     config = function()
       require("snacks").setup {
@@ -195,7 +309,7 @@ require("lazy").setup({
       vim.lsp.buf.references = SnacksPicker.lsp_references
       vim.lsp.buf.type_definition = SnacksPicker.lsp_type_definitions
     end,
-  }, -- Plugin collection but i use the picker only.
+  },
 
   { -- Git signs
     "lewis6991/gitsigns.nvim",
@@ -213,175 +327,14 @@ require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = "all",
-        highlight = { enable = true },
-        auto_install = true,
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-            },
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
-            },
-            include_surrounding_whitespace = true,
-          },
-        },
-      }
-    end,
+    main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    opts = {
+      ensure_installed = "all",
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true, disable = { "ruby" } },
+    },
   },
 
   { "folke/lazydev.nvim", opts = { library = {} } }, -- Better neovim development support.
 })
-
-vim.keymap.set("n", "Y", "^v$y", { desc = "Copy whole line" })
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
-vim.keymap.set("i", "<C-c>", "<esc>")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set({ "n" }, "<C-j>", "<C-w>j") -- Window navigation
-vim.keymap.set({ "n" }, "<C-k>", "<C-w>k") -- Window navigation
-vim.keymap.set({ "n" }, "<C-h>", "<C-w>h") -- Window navigation
-vim.keymap.set({ "n" }, "<C-l>", "<C-w>l") -- Window navigation
-
-vim.keymap.set({ "t" }, "<C-j>", "<C-\\><C-n><C-w>j", { noremap = true }) -- Window navigation
-vim.keymap.set({ "t" }, "<C-k>", "<C-\\><C-n><C-w>k", { noremap = true }) -- Window navigation
-vim.keymap.set({ "t" }, "<C-h>", "<C-\\><C-n><C-w>h", { noremap = true }) -- Window navigation
-vim.keymap.set({ "t" }, "<C-l>", "<C-\\><C-n><C-w>l", { noremap = true }) -- Window navigation
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("i", "kj", "<ESC>")
-vim.keymap.set(
-  "n",
-  "<CR>",
-  function() -- Taken from https://github.com/tjdevries/config.nvim/blob/master/plugin/keymaps.lua#L16
-    if vim.v.hlsearch == 1 then
-      vim.cmd.nohl()
-      return ""
-    else
-      ---@diagnostic disable-next-line: undefined-field
-      return vim.keycode "<CR>"
-    end
-  end,
-  { expr = true }
-)
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-vim.keymap.set("n", "{", "<cmd>cprev<CR>")
-vim.keymap.set("n", "}", "<cmd>cnext<CR>")
-vim.keymap.set({ "n", "t" }, "<M-k>", "<cmd>wincmd q<CR>")
-vim.keymap.set("n", "<C-q>", function()
-  local wins = vim.api.nvim_list_wins()
-  for _, win in ipairs(wins) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "quickfix" then
-      vim.cmd.cclose()
-      return
-    end
-  end
-  vim.cmd.copen()
-end)
-
-local function toggle_bottom_terminal() -- Toggle terminal at the bottom of the screen, why install a plugin for this ?
-  -- We have a valid buffer showing the terminal
-  if not vim.g.bottom_terminal_buffer or not vim.api.nvim_buf_is_valid(vim.g.bottom_terminal_buffer) then
-    vim.g.bottom_terminal_buffer = vim.api.nvim_create_buf(false, true)
-  end
-  for _, win in ipairs(vim.api.nvim_list_wins()) do -- Toggle if a window showing terminal is open
-    local win_buf = vim.api.nvim_win_get_buf(win)
-    if win_buf == vim.g.bottom_terminal_buffer then
-      vim.api.nvim_win_hide(win)
-      return
-    end
-  end
-
-  local win = vim.api.nvim_open_win(vim.g.bottom_terminal_buffer, true, {
-    win = -1,
-    split = "below",
-    height = math.floor(vim.o.lines * 0.45),
-    width = vim.o.columns,
-  })
-  vim.wo[win].winfixheight = true
-  vim.wo[win].list = false
-  vim.wo[win].scrolloff = 0
-  vim.wo[win].number = false
-  vim.wo[win].relativenumber = false
-
-  if vim.bo[vim.g.bottom_terminal_buffer].buftype ~= "terminal" then
-    vim.cmd.term()
-    vim.g.bottom_terminal_buffer = vim.api.nvim_get_current_buf()
-  end
-
-  vim.cmd.startinsert()
-end
-vim.keymap.set({ "n", "t" }, "<C-s>", toggle_bottom_terminal, { desc = "Toggle bottom terminal" })
-
-vim.g.gruvbuddy_style = "default"
-
-vim.cmd.colorscheme("tokyonight-moon")
-
--- FileType Settings
-local function ftplugin(filetype, callback)
-  local ft = vim.api.nvim_create_augroup("ftplugin_" .. filetype, {})
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = filetype,
-    group = ft,
-    callback = callback,
-  })
-end
-
-ftplugin("go", function()
-  vim.bo[0].sw = 4
-  vim.bo[0].ts = 4
-  vim.bo[0].expandtab = false
-  vim.bo[0].shiftwidth = 4
-end)
-
-ftplugin("lua", function()
-  vim.bo[0].sw = 2
-  vim.bo[0].ts = 2
-  vim.bo[0].expandtab = true
-  vim.bo[0].shiftwidth = 2
-end)
-
-ftplugin("ocaml", function()
-  vim.bo[0].sw = 2
-  vim.bo[0].ts = 2
-  vim.bo[0].expandtab = true
-  vim.bo[0].shiftwidth = 2
-end)
-
-ftplugin("php", function()
-  vim.bo[0].sw = 4
-  vim.bo[0].ts = 4
-  vim.bo[0].expandtab = true
-  vim.bo[0].shiftwidth = 4
-end)
-
-ftplugin("sh", function()
-  vim.bo[0].sw = 2
-  vim.bo[0].ts = 2
-  vim.bo[0].expandtab = true
-  vim.bo[0].shiftwidth = 2
-end)
