298f47961351e7abcae27333d8ad83f162a2d193
amirrezaask <raskarpour@gmail.com>
2025-05-09 02:14:38 +0330

    Automated WIP Commit: 2025-05-09 02:14:38

--- README.md
+++ README.md
@@ -11,8 +11,35 @@ make configure
 
 ## Current setup
 - Neovim
-- Fish
+- Zsh
 - Ghostty
+
+## Neovim
+```
+                            *Amirreza Ask's Neovim*
+╭-----------------------------------------------------------------------------╮
+|  Project                               |  LSP                               |
+| ------------------------------------   | ---------------------------------  |
+| <leader>pf -> Project file             |  gd -> Goto to definition          |
+| <leader>pg -> Project grep             |  gr -> Goto to references          |
+| <leader>pw -> Project word             |  gi -> Goto to implementation      |
+| <leader>pW -> Project word with input  |  R  -> Execute rename              |
+| <leader>ps -> Project (LSP) symbols    |  C  -> Execute code action         |
+| <leader>pd -> Project Diagnostics      |  K  -> Toggle Hover over symbol    |
+|                                        |  L  -> Toggle line diagnostic      |
+|-----------------------------------------------------------------------------|
+|                              General / Navigation                           |
+| --------------------------------------------------------------------------- |
+|                         <C-p>      -> Git files                             |
+|                         <C-j/k/h/l> -> Navigate splits                      |
+|                         { / }      -> Quickfix prev/next                    |
+|                         <C-s>      -> Toggle terminal                       |
+|                         -          -> Open file explorer                    |
+|                         <M-j>      -> Move code down                        |
+|                         <M-k>      -> Move code up                          |
+|                         <C-c>/jk/kj -> Escape insert mode                   |
+╰-----------------------------------------------------------------------------╯
+```
 
 ## License
 MIT
--- nvim-init.lua
+++ nvim-init.lua
@@ -1,27 +1,3 @@
---                                                              *Amirreza Ask's Neovim*
---                                  ╭-----------------------------------------------------------------------------╮
---                                  |  Project                               |  LSP                               |
---                                  | ------------------------------------   | ---------------------------------  |
---                                  | <leader>pf -> Project file             |  gd -> Goto to definition          |
---                                  | <leader>pg -> Project grep             |  gr -> Goto to references          |
---                                  | <leader>pw -> Project word             |  gi -> Goto to implementation      |
---                                  | <leader>pW -> Project word with input  |  R  -> Execute rename              |
---                                  | <leader>ps -> Project (LSP) symbols    |  C  -> Execute code action         |
---                                  | <leader>pd -> Project Diagnostics      |  K  -> Toggle Hover over symbol    |
---                                  |                                        |  L  -> Toggle line diagnostic      |
---                                  |-----------------------------------------------------------------------------|
---                                  |                              General / Navigation                           |
---                                  | --------------------------------------------------------------------------- |
---                                  |                         <C-p>      -> Git files                             |
---                                  |                         <C-j/k/h/l> -> Navigate splits                      |
---                                  |                         { / }      -> Quickfix prev/next                    |
---                                  |                         <C-s>      -> Toggle terminal                       |
---                                  |                         -          -> Open file explorer                    |
---                                  |                         <M-j>      -> Move code down                        |
---                                  |                         <M-k>      -> Move code up                          |
---                                  |                         <C-c>/jk/kj -> Escape insert mode                   |
---                                  ╰-----------------------------------------------------------------------------╯
-
 local map = vim.keymap.set
 local autocmd = vim.api.nvim_create_autocmd
 local o, g = vim.o, vim.g
