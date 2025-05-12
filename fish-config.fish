c50e626aca9f16ba1ecf4f743df0e8b87b479c83
amirrezaask <raskarpour@gmail.com>
2025-05-12 04:08:53 +0330

    Automated Commit: Work In Progress 2025-05-12 04:08:53

--- fish-config.fish
+++ fish-config.fish
@@ -115,3 +115,7 @@ function set_system_background
         return 1
     end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
