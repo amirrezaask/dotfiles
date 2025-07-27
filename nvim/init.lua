113b0d2560d00874bc5b0d53584994d51c96eed8
amirrezaask <raskarpour@gmail.com>
2025-07-28 02:00:59 +0330

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,20 @@
-vim.g.mapleader = " " -- <leader> in keybindings means Space.
-vim.o.wrap = true -- Wrap long lines.
-vim.o.signcolumn = "no" -- No need for my code jump around because some plugin wants to show a sign in the left.
-vim.o.breakindent = true -- Indent wrapped lines.
-vim.o.swapfile = false -- Disable swapfile.
-vim.o.clipboard = "unnamedplus" -- Copy/Cut/Paste to system clipboard
-vim.o.tabstop = 4 -- Indent Size
-vim.o.shiftwidth = 4 -- Default indent size
-vim.o.shiftwidth = 4 -- Default indent size
-vim.o.ignorecase = true -- Search case insensitive...
-vim.o.smartcase = true -- ... but not if it contains caps
-vim.o.formatoptions = "jcql" -- See :help fo-table
-vim.o.inccommand = "split" -- Show partial commands in the command line
-vim.o.winborder = "rounded" -- All created windows in neovim will follow this setting for borders.
-vim.o.timeoutlen = 500 -- Time in milliseconds to wait for a mapped sequence to complete.
+vim.g.mapleader = " "
+vim.o.wrap = true
+vim.o.signcolumn = "no"
+vim.o.breakindent = true
+vim.o.swapfile = false
+vim.o.clipboard = "unnamedplus"
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.shiftwidth = 4
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.formatoptions = "jcql"
+vim.o.inccommand = "split"
+vim.o.winborder = "rounded"
+vim.o.timeoutlen = 500
 vim.o.guicursor = ""
-vim.cmd("set completeopt+=noselect") -- No auto selection of completion candidates really annoying default.
+vim.cmd("set completeopt+=noselect")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -56,22 +56,22 @@ require("oil").setup()
 Fzf = require("fzf-lua")
 Fzf.setup({ "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } })
 Fzf.register_ui_select()
-vim.keymap.set("n", "<leader><leader>", Fzf.files, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>j", Fzf.live_grep, { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>k", Fzf.grep, { desc = "Grep word" })
-vim.keymap.set("n", "<leader>K", Fzf.grep_cword, { desc = "Grep <cword>" })
-vim.keymap.set("v", "<leader>K", Fzf.grep_visual, { desc = "Grep visualy selected text" })
-vim.keymap.set("n", "<leader>l", Fzf.lsp_document_symbols, { desc = "LSP Document Symbols" })
-vim.keymap.set("n", "<leader>L", Fzf.lsp_live_workspace_symbols, { desc = "LSP Workspace Symbols" })
+vim.keymap.set("n", "<leader><leader>", Fzf.files)
+vim.keymap.set("n", "<leader>j", Fzf.live_grep)
+vim.keymap.set("n", "<leader>k", Fzf.grep)
+vim.keymap.set("n", "<leader>K", Fzf.grep_cword)
+vim.keymap.set("v", "<leader>K", Fzf.grep_visual)
+vim.keymap.set("n", "<leader>l", Fzf.lsp_document_symbols)
+vim.keymap.set("n", "<leader>L", Fzf.lsp_live_workspace_symbols)
 
 vim.lsp.enable({ "gopls", "intelephense", "lua_ls" })
-vim.api.nvim_create_autocmd("LspAttach", { -- Lsp keybindings
+vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client ~= nil and client:supports_method("textDocument/completion") then
 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 		end
-		vim.keymap.set("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf }) -- extend default vim
+		vim.keymap.set("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf })
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = args.buf })
