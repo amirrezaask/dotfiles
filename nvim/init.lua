16f0afa987520f9cf34e375d44fa704ebbbde3e0
amirrezaask <raskarpour@gmail.com>
2025-07-28 01:43:51 +0330

    Automated WIP Commit: 2025-07-28 01:43:51

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,8 @@
--- Goals:
--- 		* No external package unless necessary
--- 		* Stick to defaults
--- 		* Minimum number of new keys
--- 		* Prefer muscle memory to mnemonic for new keys ( vim way )
-
--- j k l play a key part in my neovim experience
---
--- 		<leader> j -> Find File
--- 		<leader> J -> Live Grep
--- 		<leader> k -> Grep ( with input )
--- 		<leader> K -> Grep Word/<CWORD>
--- 		<leader> l -> LSP Document Symbols
--- 		<leader> L -> LSP Workspace Symbols
-
--- 		<C-j>      -> Goto Definition
--- 		<C-k>      -> Goto References
--- 		<C-l>      -> Goto Implementations
-
 vim.g.mapleader = " " -- <leader> in keybindings means Space.
 vim.o.wrap = true -- Wrap long lines.
 vim.o.signcolumn = "no" -- No need for my code jump around because some plugin wants to show a sign in the left.
 vim.o.breakindent = true -- Indent wrapped lines.
 vim.o.swapfile = false -- Disable swapfile.
-vim.o.undofile = true -- Store undo history on disk
-vim.o.splitbelow = true -- Split windows below the current windows
-vim.o.splitright = true -- Split windows right to the current windows
 vim.o.clipboard = "unnamedplus" -- Copy/Cut/Paste to system clipboard
 vim.o.tabstop = 4 -- Default indent size
 vim.o.shiftwidth = 4 -- Default indent size
@@ -35,6 +13,7 @@ vim.o.inccommand = "split" -- Show partial commands in the command line
 vim.o.winborder = "rounded" -- All created windows in neovim will follow this setting for borders.
 vim.o.timeoutlen = 500 -- Time in milliseconds to wait for a mapped sequence to complete.
 vim.o.guicursor = ""
+vim.cmd("set completeopt+=noselect") -- No auto selection of completion candidates really annoying default.
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -49,42 +28,33 @@ vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", "{", "<cmd>cprev<CR>")
 vim.keymap.set("n", "}", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<leader>i", ":edit $MYVIMRC<CR>")
-vim.cmd([[ command! W w ]])
-vim.cmd([[ command! Q q ]])
+vim.keymap.set("n", "<leader>w", ":write <CR>")
 vim.keymap.set("n", "[[", function() vim.diagnostic.jump({ count = -1 }) end, {})
 vim.keymap.set("n", "]]", function() vim.diagnostic.jump({ count = 1 }) end, {})
 vim.keymap.set("n", "L", vim.diagnostic.open_float, {})
 
--- Colors
-vim.cmd([[ 
+-- -- Colors
+vim.cmd([[
 	hi Normal guibg=none
 	hi! link StatusLine  Normal
 	hi! link NormalFloat Normal
 ]])
 
-local paq_installed, paq = pcall(require, "paq")
-if not paq_installed then
-	vim.fn.system(
-		'git clone --depth=1 https://github.com/savq/paq-nvim.git "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim'
-	)
-end
-
-paq({
-	"savq/paq-nvim",
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	"ibhagwan/fzf-lua",
-	"neovim/nvim-lspconfig",
-	{ "saghen/blink.cmp", branch = "v1.1.1" },
-	"stevearc/conform.nvim",
-	"stevearc/oil.nvim",
+vim.pack.add({
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/vague2k/vague.nvim" },
 })
 
--- https://github.com/junegunn/fzf
+require("conform").setup({ formatters_by_ft = { lua = { "stylua" }, go = { "goimports" } }, format_on_save = {} })
+require("nvim-treesitter.configs").setup { ensure_installed = { "go", "php" }, highlight = { enable = true }, auto_install = true }
+require("oil").setup()
+
 Fzf = require("fzf-lua")
-Fzf.setup({
-	"fzf-vim",
-	keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } },
-})
+Fzf.setup({ "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } })
 Fzf.register_ui_select()
 vim.keymap.set("n", "<leader><leader>", Fzf.files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>j", Fzf.live_grep, { desc = "Live Grep" })
@@ -93,39 +63,18 @@ vim.keymap.set("n", "<leader>K", Fzf.grep_cword, { desc = "Grep <cword>" })
 vim.keymap.set("v", "<leader>K", Fzf.grep_visual, { desc = "Grep visualy selected text" })
 vim.keymap.set("n", "<leader>l", Fzf.lsp_document_symbols, { desc = "LSP Document Symbols" })
 vim.keymap.set("n", "<leader>L", Fzf.lsp_live_workspace_symbols, { desc = "LSP Workspace Symbols" })
-vim.lsp.buf.definition = Fzf.lsp_definitions
-vim.lsp.buf.implementation = Fzf.lsp_implementations
-vim.lsp.buf.references = Fzf.lsp_references
-vim.lsp.buf.type_definition = Fzf.lsp_type_definitions
 
--- Treesitter enables richer syntax highlight
-require("nvim-treesitter.configs").setup {
-	ensure_installed = { "go", "php" },
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true, disable = { "ruby" } },
-}
-
--- Langauge Server Protocol
-require("lspconfig").gopls.setup {} -- go install golang.org/x/tools/gopls@latest
-require("lspconfig").intelephense.setup {} -- npm install -g intelephense
-require("lspconfig").lua_ls.setup { -- https://github.com/LuaLS/lua-language-server/releases/latest
-	settings = {
-		Lua = {
-			diagnostics = { enable = true, globals = { "vim" } },
-			workspace = { library = { vim.fn.expand("$VIMRUNTIME/lua") }, maxPreload = 10000, preloadFileSize = 10000 },
-		},
-	},
-}
-
+vim.lsp.enable({ "gopls", "intelephense", "lua_ls" })
 vim.api.nvim_create_autocmd("LspAttach", { -- Lsp keybindings
 	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client ~= nil and client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
 		vim.keymap.set("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf }) -- extend default vim
-
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = args.buf })
-
 		vim.keymap.set("n", "<C-j>", vim.lsp.buf.definition, { buffer = args.buf })
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.references, { buffer = args.buf })
 		vim.keymap.set("n", "<C-l>", vim.lsp.buf.implementation, { buffer = args.buf })
@@ -135,27 +84,3 @@ vim.api.nvim_create_autocmd("LspAttach", { -- Lsp keybindings
 		vim.keymap.set({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, { buffer = args.buf })
 	end,
 })
-vim.diagnostic.config({ virtual_text = true })
-
--- Autocomplete
-require("blink.cmp").setup({
-	keymap = { preset = "enter" },
-	cmdline = { enabled = false },
-	completion = {
-		list = { selection = { preselect = false } },
-		menu = { draw = { columns = { { "label", "label_description", gap = 1 } } } }, -- No icons
-	},
-	sources = { default = { "lsp", "path", "snippets" } },
-})
-
--- Autoformat on save
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" }, -- github.com/JohnnyMorganz/StyLua/releases/latest
-		go = { "goimports" }, -- go install golang.org/x/tools/cmd/goimports@latest
-	},
-	format_on_save = {},
-})
-
--- Manage files using vim text editing power.
-require("oil").setup()
