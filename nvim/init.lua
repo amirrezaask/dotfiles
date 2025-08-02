bad7f0d641f2c3d61df7a03a0107532891f51abe
amirrezaask <raskarpour@gmail.com>
2025-08-03 02:34:47 +0330

    some vimscript can hurt

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,9 @@ vim.g.mapleader = " "
 vim.o.signcolumn = "yes"
 vim.o.swapfile = false
 vim.o.clipboard = "unnamedplus"
-vim.o.tabstop = 4;
+vim.o.tabstop = 4
 vim.o.shiftwidth = 0
-vim.o.ignorecase = true;
+vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.formatoptions = "jcql"
 vim.o.inccommand = "split"
@@ -13,13 +13,16 @@ vim.o.scrolloff = 5
 vim.o.splitkeep = 'topline'
 vim.o.showmode = false
 vim.o.linebreak = true
-vim.o.completeopt="menuone,noselect,noinsert,fuzzy"
+vim.o.completeopt = "menuone,noselect,noinsert,fuzzy"
 vim.o.cursorline = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.laststatus = 3
 vim.cmd [[ set wildoptions+=fuzzy ]]
 
-vim.cmd [[ 
-	hi! link StatusLine Normal 
-	hi! Visual guibg=fg guifg=bg 
+vim.cmd [[
+	hi! link StatusLine Normal
+	hi! Visual guibg=fg guifg=bg
 ]]
 
 vim.cmd [[ autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150 } ]]
@@ -37,46 +40,57 @@ vim.pack.add {
 	"https://github.com/ibhagwan/fzf-lua",                -- Fuzzy Finder
 	"https://github.com/nvim-treesitter/nvim-treesitter", -- Syntax Highlighting
 	"https://github.com/neovim/nvim-lspconfig",           -- LSP
-	"https://github.com/stevearc/conform.nvim",           -- Autoformat
 	"https://github.com/stevearc/oil.nvim",               -- File manager
 }
 
-require("conform").setup({ formatters_by_ft = { go = { "goimports" } }, format_on_save = {} })
 require("nvim-treesitter.configs").setup { highlight = { enable = true }, auto_install = true }
 require("oil").setup()
 
-local FzfLua = require("fzf-lua")
-FzfLua.setup { "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } }
-FzfLua.register_ui_select()
+F = require("fzf-lua")
+F.setup { "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } }
+F.register_ui_select()
 
-vim.keymap.set("n", "<leader><leader>", FzfLua.files)
-vim.keymap.set("n", "<leader>j",        FzfLua.live_grep)
-vim.keymap.set("n", "<leader>k",        FzfLua.grep_cword)
-vim.keymap.set("v", "<leader>k",        FzfLua.grep_visual)
+vim.cmd [[
+	nnoremap <leader><leader> <cmd>lua F.files()<CR>
+	nnoremap <leader>j        <cmd>lua F.live_grep()<CR>
+	nnoremap <leader>k        <cmd>lua F.grep_cword()<CR>
+	vnoremap <leader>k        <cmd>lua F.grep_cword()<CR>
+]]
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-		vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
-		vim.keymap.set("n", "C-]", vim.lsp.buf.definition, { buffer = args.buf })
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = args.buf })
-		vim.keymap.set("n", "R", vim.lsp.buf.rename, { buffer = args.buf })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
-		vim.keymap.set("n", "C", vim.lsp.buf.code_action, { buffer = args.buf })
-		vim.keymap.set({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, { buffer = args.buf })
-		vim.keymap.set("n", "L", vim.diagnostic.open_float, {})
-		vim.keymap.set("n", "<leader>l", FzfLua.lsp_workspace_symbols, {buffer = args.buf })
-		vim.keymap.set("n", "<leader>o", FzfLua.diagnostics_document, {buffer = args.buf })
-		vim.keymap.set("n", "<leader>O", FzfLua.diagnostics_workspace, {buffer = args.buf })
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		vim.cmd [[
+			nnoremap <buffer> <C-]>          <cmd>lua vim.lsp.buf.definition() <CR>
+			nnoremap <buffer> gd             <cmd>lua vim.lsp.buf.definition() <CR>
+			nnoremap <buffer> gr             <cmd>lua F.lsp_references() <CR>
+			nnoremap <buffer> gi             <cmd>lua F.lsp_implementations() <CR>
+			nnoremap <buffer> R              <cmd>lua vim.lsp.buf.rename()<CR>
+			nnoremap <buffer> K              <cmd>lua vim.lsp.buf.hover()<CR>
+			nnoremap <buffer> C              <cmd>lua vim.lsp.buf.code_action()<CR>
+			nnoremap <buffer> L              <cmd>lua vim.diagnostic.open_float()<CR>
+		    nnoremap <buffer> <leader>l      <cmd>lua F.lsp_workspace_symbols()<CR>
+			nnoremap <buffer> <C-s>          <cmd>lua vim.lsp.buf.signature_help()<CR>
+			inoremap <buffer> <C-s>          <cmd>lua vim.lsp.buf.signature_help()<CR>
+		]]
+	end
+})
+
+-- Go Autoformat
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern  = '*.go',
+	callback = function()
+		vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' }, }, apply = true, })
+		vim.lsp.buf.format()
+		vim.cmd('write')
 	end,
 })
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
-vim.lsp.config('lua_ls', { settings = { Lua = { runtime = { version = 'LuaJIT', path = runtime_path }, diagnostics = { globals = {'vim'}} , workspace = {library = vim.api.nvim_get_runtime_file('', true)}}}})
+vim.lsp.config('lua_ls',
+	{ settings = { Lua = { runtime = { version = 'LuaJIT', path = runtime_path }, diagnostics = { globals = { 'vim' } }, workspace = { library = vim.api.nvim_get_runtime_file('', true) } } } })
 
 vim.lsp.enable({ "gopls", "intelephense", "lua_ls" })
-
