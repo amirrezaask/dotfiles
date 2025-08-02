f595d503056e07d3deea6b8ada033ad35f376b53
amirrezaask <raskarpour@gmail.com>
2025-08-03 01:04:25 +0330

    Automated WIP Commit: 2025-08-03 01:04:25

--- fish/config.fish
+++ fish/config.fish
@@ -72,3 +72,10 @@ end
 function feat
 	git checkout -b "feat-$argv[1]"
 end
+
+
+if ! command -v starship &> /dev/null
+	curl -sS https://starship.rs/install.sh | fish
+end
+
+starship init fish | source
