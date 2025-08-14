7c5fa139683e5591e1bade163f22782d39c41a5a
Amirreza Askarpour <raskarpour@gmail.com>
2025-08-15 01:19:51 +0330

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ vim.o.formatoptions = "jcql"
 vim.o.inccommand = "split"
 vim.o.winborder = "rounded"
 vim.o.scrolloff = 5
-vim.o.splitkeep = 'topline'
+vim.o.splitkeep = "topline"
 vim.o.linebreak = true
 vim.o.completeopt = "menuone,noselect,noinsert,fuzzy"
 vim.o.splitbelow = true
@@ -20,7 +20,7 @@ vim.o.splitright = true
 vim.o.laststatus = 3
 vim.opt.wildoptions:append("fuzzy")
 vim.o.cursorline = true
-vim.cmd [[ autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150 } ]]
+vim.cmd([[ autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150 } ]])
 local map = vim.keymap.set
 
 map("i", "jk", "<esc>")
@@ -31,8 +31,24 @@ map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzz")
 map("n", "N", "Nzz")
 map("n", "<leader>i", ":edit $MYVIMRC<CR>")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.pack.add {
+require("lazy").setup({
 	"https://github.com/ibhagwan/fzf-lua",
 	"https://github.com/scottmckendry/cyberdream.nvim",
 	"https://github.com/folke/tokyonight.nvim",
@@ -40,33 +56,32 @@ vim.pack.add {
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/stevearc/oil.nvim",
-	{ src = 'https://github.com/saghen/blink.cmp', version="v1.6.0" }
-}
+	{ "https://github.com/saghen/blink.cmp", version = "v1.6.0" },
+})
 
-
-require"cyberdream".setup { transparent = true }
+require("cyberdream").setup({ transparent = true })
 
 vim.cmd.colorscheme("cyberdream")
 
-require("nvim-treesitter.configs").setup { highlight = { enable = true }, auto_install = true }
+require("nvim-treesitter.configs").setup({ highlight = { enable = true }, auto_install = true })
 
-require("oil").setup {}
+require("oil").setup({})
 
-require("blink.cmp").setup {
-    keymap = { preset = 'default' },
-}
-require("fzf-lua").setup { "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } }
-vim.cmd [[
+require("blink.cmp").setup({
+	keymap = { preset = "default" },
+})
+require("fzf-lua").setup({ "fzf-vim", keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } })
+vim.cmd([[
 	nnoremap <leader><leader> <cmd>lua FzfLua.files()<CR>
 	nnoremap <leader>j        <cmd>lua FzfLua.live_grep()<CR>
 	nnoremap <leader>k        <cmd>lua FzfLua.grep_cword()<CR>
 	vnoremap <leader>k        <cmd>lua FzfLua.grep_cword()<CR>
-]]
+]])
 
-vim.lsp.buf.references       = FzfLua.lsp_references
-vim.lsp.buf.definition       = FzfLua.lsp_definitions
-vim.lsp.buf.implementation   = FzfLua.lsp_implementations
-vim.lsp.buf.document_symbol  = FzfLua.lsp_document_symbols
+vim.lsp.buf.references = FzfLua.lsp_references
+vim.lsp.buf.definition = FzfLua.lsp_definitions
+vim.lsp.buf.implementation = FzfLua.lsp_implementations
+vim.lsp.buf.document_symbol = FzfLua.lsp_document_symbols
 vim.lsp.buf.workspace_symbol = FzfLua.lsp_workspace_symbols
 
 vim.diagnostic.config({ virtual_text = true })
@@ -80,15 +95,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- if client:supports_method('textDocument/completion') then
 		--   vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
 		-- end
-		map("n", "gd", vim.lsp.buf.definition, {buffer = args.buf})
-		map("n", "L", vim.diagnostic.open_float, {buffer = args.buf})
-		map("n", "<leader>O", FzfLua.lsp_workspace_symbols, {buffer = args.buf})
-	end
+		map("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
+		map("n", "L", vim.diagnostic.open_float, { buffer = args.buf })
+		map("n", "<leader>O", FzfLua.lsp_workspace_symbols, { buffer = args.buf })
+	end,
 })
-vim.lsp.config('lua_ls', { settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file('', true) } } } })
+vim.lsp.config("lua_ls", { settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } } })
 vim.lsp.enable({ "gopls", "intelephense", "lua_ls" })
 
-vim.api.nvim_create_autocmd('BufEnter', { -- Go related stuff
+vim.api.nvim_create_autocmd("BufEnter", { -- Go related stuff
 	pattern = "*.go",
 	callback = function(args)
 		vim.o.makeprg = "golangci-lint run --tests=false --show-stats=false"
@@ -96,9 +111,9 @@ vim.api.nvim_create_autocmd('BufEnter', { -- Go related stuff
 		vim.api.nvim_create_autocmd("BufWritePre", { -- Autoformat + imports
 			buffer = args.buf,
 			callback = function()
-				vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' }, }, apply = true, })
+				vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
 				vim.lsp.buf.format()
 			end,
 		})
-	end
+	end,
 })
