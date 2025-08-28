1bbe7c5cb39c8f52f294f907b56c5ef127b751a6
AmirrezaAskarpour <raskarpour@gmail.com>
2025-08-29 01:01:45 +0330

    Automated WIP Commit: 2025-08-29 01:01:45

---
+++ base/base.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Base directory: $BASE_DIR"
+
+# Detect the Linux distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Cannot determine distribution: /etc/os-release not found."
+    exit 1
+fi
+
+case "$DISTRO" in
+    "ubuntu")
+        echo "Installing Ubuntu packages"
+        # Ubuntu-specific commands
+		bash "$BASE_DIR/ubuntu/ubuntu.sh"
+        ;;
+    "fedora")
+        echo "Installing Fedora packages"
+        # Fedora-specific commands
+		bash "$BASE_DIR/fedora/fedora.sh"
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        exit 1
+        ;;
+esac
+
+if !command -v starship &> /dev/null; then
+	curl -sS https://starship.rs/install.sh | sh
+fi
+
