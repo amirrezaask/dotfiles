f97af7fdfb37ddf5ff0310054b27ab6bcf3d2b0f
AmirrezaAskarpour <raskarpour@gmail.com>
2025-08-29 01:13:27 +0330

    new themes

--- env/nvim/lua/core/options.lua
+++ env/nvim/lua/core/options.lua
@@ -23,12 +23,13 @@ vim.o.laststatus = 3
 vim.opt.wildoptions:append("fuzzy")
 vim.o.cursorline = true
 vim.diagnostic.config({ virtual_text = true })
+vim.g.lazyvim_check_order = false
 vim.cmd([[ autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150 } ]])
 
 vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = function()
-		vim.cmd([[ 
-			hi Normal guibg=NONE 
+		vim.cmd([[
+			hi Normal guibg=NONE
 			hi NormalFloat guibg=NONE
 			hi SignColumn guibg=NONE
 		]])
---
+++ themes/everforest/neovim.lua
@@ -0,0 +1,10 @@
+return {
+	{ "neanias/everforest-nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "everforest",
+			background = "soft",
+		},
+	},
+}
