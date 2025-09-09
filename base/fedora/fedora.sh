d4dbc79aba35257c0c75540db059e961ba31bf0e
AmirrezaAskarpour <raskarpour@gmail.com>
2025-09-09 20:51:15 +0330

    Automated WIP Commit: 2025-09-09 20:51:15

--- base/fedora/fedora.sh
+++ base/fedora/fedora.sh
@@ -7,6 +7,8 @@ sudo dnf copr -y enable solopasha/hyprland
 sudo dnf copr -y enable markupstart/SwayOSD
 sudo dnf copr -y enable dejan/lazygit
 sudo dnf copr -y enable scottames/ghostty
+sudo dnf copr -y enable azandure/clipse
+sudo dnf copr -y enable lihaohong/yazi
 
 # Install base packages
 sudo dnf -y install \
@@ -33,18 +35,26 @@ sudo dnf -y install \
 	lazygit \
 	ghostty \
 	kitty \
+	clipse \
+	yazi \
 	fzf
 
 
 # Install hyprshot for screenshots
-sudo curl https://raw.githubusercontent.com/Gustash/Hyprshot/refs/heads/main/hyprshot > ~/hyprshot
-sudo mv ~/hyprshot /usr/bin/hyprshot
-sudo chmod +x /usr/bin/hyprshot
+if [[ ! -f /usr/bin/hyprshot ]]; then
+	sudo curl https://raw.githubusercontent.com/Gustash/Hyprshot/refs/heads/main/hyprshot > ~/hyprshot
+	sudo mv ~/hyprshot /usr/bin/hyprshot
+	sudo chmod +x /usr/bin/hyprshot
+fi
 
 # Install cargo
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+if command -v nvim >/dev/null 2>&1; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
 
-. "$HOME/.cargo/env"
+if [[ -f "$HOME/.cargo/env" ]]; then
+	. "$HOME/.cargo/env"
+fi
 
 # manage bluetooth
 cargo install bluetui
--- base/ubuntu/ubuntu.sh
+++ base/ubuntu/ubuntu.sh
@@ -39,14 +39,5 @@ cargo install bluetui
 # To let brightness control work
 sudo usermod -a -G video $USER
 
-# Clone Walker + Elephant
-git clone https://github.com:abenz1267/walker.git
-git clone https://github.com:abenz1267/elephant.git
 
-cd elephant
-go install ./cmd/
-cd ..
-cd walker
-cargo build --release
-sudo cp target/release/walker /usr/local/bin/
-cd ..
+go install github.com/savedra1/clipse@v1.1.0
