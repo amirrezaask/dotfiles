63cb2fb5287904fc92e249ca1992bd6048f49c69
AmirrezaAskarpour <raskarpour@gmail.com>
2025-09-19 01:50:54 +0330

    Some updates

--- configs/nvim/lua/plugins/fzf.lua
+++ configs/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,6 @@
 return {
 	"ibhagwan/fzf-lua",
+	enabled = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("fzf-lua").setup {
---
+++ configs/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+	"folke/snacks.nvim",
+	config = function()
+		require("snacks").setup {
+			terminal = { enabled = true },
+			indent = { enabled = true },
+			picker = { enabled = true },
+			dashboard = { enabled = true },
+			input = { enabled = true },
+			lazygit = { enabled = true },
+		}
+
+
+		local Snacks = require "snacks"
+		local picker = Snacks.picker
+		vim.keymap.set("n", "<leader><leader>", picker.files)
+		vim.keymap.set("n", "<leader>j", picker.grep)
+		vim.keymap.set({ "n", "v" }, "<leader>k", picker.grep_word)
+		vim.keymap.set("n", "<leader>o", picker.lsp_symbols)
+		vim.keymap.set("n", "<leader>O", picker.lsp_workspace_symbols)
+
+		vim.lsp.buf.references = picker.lsp_references
+		vim.lsp.buf.definition = picker.lsp_definitions
+		vim.lsp.buf.implementation = picker.lsp_implementations
+		vim.lsp.buf.document_symbol = picker.lsp_symbols
+		vim.lsp.buf.workspace_symbol = picker.lsp_workspace_symbols
+	end,
+}
