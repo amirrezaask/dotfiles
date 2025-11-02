c0995df28d8683aa9997e9fce429390e10909f17
AmirrezaAskarpour <raskarpour@gmail.com>
2025-11-02 04:45:37 +0330

    Fully Commit to Evil Mode

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -1,22 +1,10 @@
 ;; -*- lexical-binding: t; -*-
+;; Setup facts
 (setq is-windows (eq system-type 'windows-nt)
       is-linux (eq system-type 'gnu/linux)
       is-macos (eq system-type 'darwin))
 
-;; Add directory to exec-path and also set emacs process PATH variable.
-(defun home (path) (expand-file-name path (getenv "HOME")))
-(add-to-list 'exec-path (home ".local/bin"))
-(add-to-list 'exec-path (home "go/bin"))
-(add-to-list 'exec-path (home ".cargo/bin"))
-(add-to-list 'exec-path (home "bin"))
-(add-to-list 'exec-path "/usr/local/go/bin")
-(add-to-list 'exec-path "/opt/homebrew/bin")
-(add-to-list 'exec-path "/usr/local/bin")
-(if is-windows (setenv "PATH" (string-join exec-path ";")) (setenv "PATH" (string-join exec-path ":"))) ;; set emacs process PATH
-
-;;;;
-; Optimize startup performance by adjusting GC and file handler settings.
-;;;;
+;;;; Optimize startup performance by adjusting GC and file handler settings.
 (defvar my/original-gc-cons-threshold gc-cons-threshold)
 (defvar my/original-gc-cons-percentage gc-cons-percentage)
 (defvar my/original-file-name-handler-alist file-name-handler-alist)
@@ -37,16 +25,23 @@
                      (float-time (time-subtract after-init-time before-init-time))
                      gcs-done)))
 
-
-;; Cursor blinking is both distracting and CPU consuming.
-(blink-cursor-mode -1)
-
 ;; Since on macos menubar is a section that is wasted anyway let's just have it.
 (unless is-macos (menu-bar-mode -1))
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+;; Add directory to exec-path and also set emacs process PATH variable.
+(defun home (path) (expand-file-name path (getenv "HOME")))
+(add-to-list 'exec-path (home ".local/bin"))
+(add-to-list 'exec-path (home "go/bin"))
+(add-to-list 'exec-path (home ".cargo/bin"))
+(add-to-list 'exec-path (home "bin"))
+(add-to-list 'exec-path "/usr/local/go/bin")
+(add-to-list 'exec-path "/opt/homebrew/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(if is-windows (setenv "PATH" (string-join exec-path ";")) (setenv "PATH" (string-join exec-path ":"))) ;; set emacs process PATH
 
-(setq-default cursor-type 'bar)
+;; Cursor blinking is both distracting and CPU consuming.
+(blink-cursor-mode -1)
 
 ;;; Stack traces on errors.
 (setq debug-on-error nil)
@@ -93,7 +88,7 @@
        (unless (package-installed-p package) (package-install package)))
 
 (defun ensure-package-vc (package repo) "Same as ensure-package but get's package from a VC backend."
-  (unless (package-installed-p package) (package-vc-install package repo)))
+       (unless (package-installed-p package) (package-vc-install package repo)))
 
 (set-frame-font "Maple Mono-17" nil t)
 
@@ -108,6 +103,7 @@
 
 ;; Highlight current line
 (global-hl-line-mode +1)
+
 ;; better scrolling experience.
 (pixel-scroll-precision-mode +1)
 
@@ -168,53 +164,14 @@
 ;; handy string transformation functions.
 (ensure-package 'string-inflection)
 
-;; Multiple cursors.
-(ensure-package 'multiple-cursors)
-(global-set-key (kbd "C-S-n") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-S-p") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-S-g") 'mc/mark-all-like-this-dwim)
-
-
-
-;; jump-up/down are utility functions that I use to move around code to emulate C-d/u functionality from vim.
-(defun jump-up ()
-  (interactive)
-  (next-line (* -1 (/ (window-height) 2))) (recenter-top-bottom))
-
-(defun jump-down ()
-  (interactive)
-  (next-line (/ (window-height) 2)) (recenter-top-bottom))
-
-(global-set-key (kbd "M-n") 'jump-down)
-(global-set-key (kbd "M-p") 'jump-up)
-
-(defun copy () "Either copy region or the current line."
-       (interactive)
-       (if (use-region-p)
-           (kill-ring-save (region-beginning) (region-end)) ;; copy active region contents
-         (kill-ring-save (line-beginning-position) (line-end-position)))) ;; copy current line
-
-(defun cut () "Either cut region or the current line."
-       (interactive)
-       (if (use-region-p)
-           (kill-region (region-beginning) (region-end)) ;; copy active region contents
-         (kill-region (line-beginning-position) (line-end-position)))) ;; copy current line
-
-(global-set-key (kbd "C-w") 'cut)
-(global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "M-w") 'copy)
-
 (global-set-key (kbd "C-/") 'comment-line) ;; Comment
 (global-set-key (kbd "C-<return>") 'save-buffer)
 
-(global-set-key (kbd "C-;") 'goto-line) ;;
-
-(global-set-key (kbd "C-SPC") 'set-mark-command) ;; Visual selection
-
 (global-set-key (kbd "M-RET") 'indent-buffer) ;; Format buffer
 
-(global-set-key (kbd "M-q") 'quoted-insert)
 
+(global-set-key   (kbd "M--") 'text-scale-decrease)
+(global-set-key   (kbd "M-=") 'text-scale-increase)
 
 ;; Themes
 (setq custom-safe-themes t)
@@ -234,25 +191,16 @@
 (setq vertico-count 20)
 (vertico-mode +1)
 
-(ensure-package 'go-mode)
-(ensure-package 'php-mode)
-
-(add-hook 'go-mode-hook #'eglot-ensure)
-(add-hook 'php-mode-hook #'eglot-ensure)
-
-      
 ;; magit is emacs git client.
 (ensure-package 'magit)
 (global-set-key (kbd "C-x g") 'magit)
 
-;; 
-;; by default It uses C-x p acs prefix.
-;; It has functionality to search in project but it's slow, so I use a custom function for that.
 (defun project-grep (&optional EDIT)
   (interactive "P")
   (let ((default-directory (if (project-current) (project-root (project-current)) default-directory)))
     (grep (format "rg --no-heading --color=\"never\" %s" (read-string "Grep: ")))))
 (define-key project-prefix-map (kbd "g") 'project-grep)
+
 (global-set-key (kbd "C-x p g") 'project-grep)
 
 
@@ -275,49 +223,6 @@
   (define-key compilation-mode-map (kbd "k") 'kill-compilation)
   (define-key compilation-mode-map (kbd "G") (lambda () (interactive) (recompile t))))
 
-
-;; search/replace
-(with-eval-after-load 'replace (define-key query-replace-map (kbd "<return>") 'act))
-(global-set-key (kbd "M-r") 'replace-regexp)
-
-;; macros, i don't use but let's have better keys
-(global-set-key   (kbd "M-[")  'kmacro-start-macro)
-(global-set-key   (kbd "M-]")  'kmacro-end-or-call-macro)
-(global-set-key   (kbd "M-\\") 'kmacro-end-and-call-macro)
-
-;; By default emacs resizes font with C-x -/+ but it's faster this way.
-(global-set-key   (kbd "C--") 'text-scale-decrease)
-(global-set-key   (kbd "C-=") 'text-scale-increase)
-
-
-;; Splits
-;; UX: Favor vertical splits over horizontal ones. Monitors are trending toward
-;;   wide, rather than tall.
-(setq split-width-threshold 160
-      split-height-threshold nil)
-
-
-(defun split-window-right-balance-and-switch () (interactive)
-       (split-window-right)
-       (balance-windows)
-       (other-window 1))
-
-(defun split-window-below-balance-and-switch () (interactive)
-       (split-window-below)
-       (balance-windows)
-       (other-window 1))
-
-(defun delete-window-and-balance () (interactive)
-       (delete-window)
-       (balance-windows))
-
-(global-set-key (kbd "C-x 0") 'delete-window-and-balance)
-(global-set-key (kbd "C-x 1") 'delete-other-windows)
-(global-set-key (kbd "C-x 2") 'split-window-below-balance-and-switch)
-(global-set-key (kbd "C-x 3") 'split-window-right-balance-and-switch)
-
-(defun kill-current-buffer () (interactive) (kill-buffer (current-buffer)))
-
 (defun indent-buffer () "Indent an entire buffer using the default intenting scheme."
        (interactive)
        (save-excursion
@@ -336,11 +241,6 @@
 
 ;; LSP
 (ensure-package 'eglot)
-(with-eval-after-load 'eglot
-  (define-key eglot-mode-map (kbd "C-c C-r") 'eglot-rename)
-  (define-key eglot-mode-map (kbd "M-RET")   'eglot-organize-imports-format)
-  (define-key eglot-mode-map (kbd "C-c C-c") 'eglot-code-actions))
-
 (setq eldoc-echo-area-use-multiline-p nil)
 (setq eglot-ignored-server-capabilities '( ;; Disable fancy LSP features.
                                           :documentHighlightProvider           ;; "Highlight symbols automatically"
@@ -363,12 +263,9 @@
 
 (defun eglot-organize-imports-format () (interactive) (eglot-format) (eglot-organize-imports))
 
-
-;; xref is emacs infrastructure that provides functionality to jump to definition, references, ...
-(global-set-key (kbd "M-.") 'xref-find-definitions)
-(global-set-key (kbd "M-,") 'xref-go-back)
-(global-set-key (kbd "M-?") 'xref-find-references)
-(global-set-key (kbd "M-/") 'xref-find-references)
+(with-eval-after-load 'eglot
+  (define-key eglot-mode-map (kbd "M-RET")   'eglot-organize-imports-format)
+  (define-key eglot-mode-map (kbd "C-c C-c") 'eglot-code-actions))
 
 ;; Installing support for languages, hopefuly emacs will start shipping all needed tressitter parsers soon and I can remove these.
 (ensure-package 'json-mode)
@@ -377,9 +274,11 @@
 (ensure-package 'rust-mode)
 (ensure-package 'php-mode )
 
+(add-hook 'go-mode-hook #'eglot-ensure)
+(add-hook 'php-mode-hook #'eglot-ensure)
+
 ;; Evil section ...
 (ensure-package 'evil)
-(ensure-package 'evil-commentary)
 (setq evil-want-integration t)
 (setq evil-want-keybinding nil)
 (setq evil-want-C-u-scroll t)
@@ -387,15 +286,11 @@
 (setq evil-split-window-below t)
 (evil-mode +1)
 
-(evil-global-set-key 'normal (kbd "SPC o") 'find-file)
-(evil-global-set-key 'normal (kbd "SPC SPC") 'project-find-file)
-(evil-global-set-key 'normal (kbd "SPC j")   'project-grep)
-(evil-global-set-key 'normal (kbd "SPC p f") 'project-find-file)
-(evil-global-set-key 'normal (kbd "SPC p p") 'project-switch-project)
-(evil-global-set-key 'normal (kbd "SPC h v") 'describe-variable)
-(evil-global-set-key 'normal (kbd "SPC h f") 'describe-function)
+(evil-define-key 'normal global-map (kbd "gcc") 'comment-line)
+(evil-define-key 'visual global-map (kbd "gc")  'comment-line)
+(evil-define-key 'normal global-map (kbd "gd")  'xref-find-definitions)
+(evil-define-key 'normal global-map (kbd "grr") 'xref-find-references)
+(evil-define-key 'normal global-map (kbd "SPC SPC") 'project-find-file)
+(with-eval-after-load 'eglot
+  (evil-define-key 'normal eglot-mode-map (kbd "grn") 'eglot-rename))
 
-(evil-commentary-mode)
-
-(ensure-package 'evil-collection)
-(evil-collection-init)
