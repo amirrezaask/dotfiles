49303694d3c361e2d199cb76f259fe2c0cf3a4cd
AmirrezaAskarpour <raskarpour@gmail.com>
2025-11-29 17:12:54 +0330

    wip

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -99,6 +99,18 @@ alias lsa='ls -lah'
 function fish_greeting
 
 end
-
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	if [ $PWD != $HOME ]
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
 # opencode
 fish_add_path /Users/amirrezaask/.opencode/bin
