9c94af1ca8eef96b823ab864388d8d8b8f7c684b
AmirrezaAskarpour <raskarpour@gmail.com>
2026-02-20 22:54:05 +0330

    wip

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -179,7 +179,16 @@ require("lazy").setup({
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("lualine").setup({})
+			require('lualine').setup {
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = { { 'filename', path = 1 } },
+					lualine_x = { 'encoding', 'fileformat', 'filetype' },
+					lualine_y = { 'progress' },
+					lualine_z = { 'location' }
+				},
+			}
 		end,
 	},
 
