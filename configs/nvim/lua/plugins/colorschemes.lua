deb0878b1d611a5d43de53bf801b6a5afafd6131
AmirrezaAskarpour <raskarpour@gmail.com>
2026-02-24 03:26:03 +0330

    wip

--- configs/nvim/lua/plugins/colorschemes.lua
+++ configs/nvim/lua/plugins/colorschemes.lua
@@ -1,24 +1,6 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({ transparent = true })
-		end,
-	},
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{ "catppuccin/nvim", name = "catppuccin" },
-	{ "vague-theme/vague.nvim" },
-	{
-		"eldritch-theme/eldritch.nvim",
-		lazy = true,
-		name = "eldritch",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
+	{ "folke/tokyonight.nvim", priority = 1000, opts = { transparent = true } },
+	{ "rose-pine/neovim", name = "rose-pine", opts = { styles = { transparency = true } } },
+	{ "catppuccin/nvim", name = "catppuccin", opts = { transparent = true } },
+	{ "vague-theme/vague.nvim", opts = { transparent = true } },
 }
