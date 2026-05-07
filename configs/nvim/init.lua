5be0e2e41cf8427e73e4b19c07e0853fcc72f62a
AmirrezaAskarpour <raskarpour@gmail.com>
2026-05-08 03:12:52 +0330

    updates

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -260,6 +260,7 @@ vim.pack.add { "https://github.com/folke/noice.nvim" }
 
 require("noice").setup {
 	lsp = {
+		progress = { enabled = false },
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
