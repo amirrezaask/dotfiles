fb9e383429fa9e671bd9a0bc1d7e789529972f37
AmirrezaAskarpour <raskarpour@gmail.com>
2026-05-09 20:46:35 +0330

    updates

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -437,6 +437,23 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "dmtrKovalenko/fff.nvim",
+    build = function() require("fff.download").download_or_build_binary() end,
+    opts = {
+      debug = {
+        enabled = true,
+        show_scores = true,
+      },
+    },
+    lazy = false, -- the plugin lazy-initialises itself
+    -- keys = {
+    --   { "ff", function() require("fff").find_files() end, desc = "FFFind files" },
+    --   { "fg", function() require("fff").live_grep() end, desc = "LiFFFe grep" },
+    --   { "fz", function() require("fff").live_grep { grep = { modes = { "fuzzy", "plain" } } } end, desc = "Live fffuzy grep" },
+    --   { "fc", function() require("fff").live_grep { query = vim.fn.expand("<cword>") } end, desc = "Search current word" },
+    -- },
+  },
 
   {
     "stevearc/conform.nvim",
