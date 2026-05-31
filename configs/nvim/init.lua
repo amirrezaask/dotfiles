e10c9e629928770be57e264f33b603aec9dfbee5
AmirrezaAskarpour <raskarpour@gmail.com>
2026-06-01 01:12:04 +0330

    Remove unused colorschemes.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -160,17 +160,13 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 -- [colors] {{{
 vim.pack.add {
   "https://github.com/vague-theme/vague.nvim",
-  "https://github.com/sainnhe/everforest",
-  "https://github.com/morhetz/gruvbox",
+  "https://github.com/folke/tokyonight.nvim",
   { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
   { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
   "https://github.com/scottmckendry/cyberdream.nvim",
-  "https://github.com/navarasu/onedark.nvim",
-  "https://github.com/folke/tokyonight.nvim",
 }
 
 require("vague").setup { bold = false, italic = false, transparent = vim.g.transparency }
-require("onedark").setup { style = "darker", transparent = vim.g.transparency }
 require("tokyonight").setup {
   transparent = vim.g.transparency,
   styles = { comments = { italic = false }, keywords = { italic = false } },
@@ -186,38 +182,6 @@ require("rose-pine").setup {
   styles = { bold = false, italic = false, transparency = vim.g.transparency },
 }
 
-vim.g.everforest_background = "hard"
-vim.g.everforest_enable_italic = 0
-vim.g.gruvbox_contrast_dark = "hard"
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "everforest",
-  callback = function()
-    if vim.o.background == "dark" then
-      vim.cmd([[
-        hi! Normal guibg=#1e2326
-        hi! NormalFloat guibg=#1e2326
-        hi! Terminal guibg=#1e2326
-      ]])
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "gruvbox",
-  callback = function()
-    if vim.o.background == "dark" then
-      vim.cmd([[
-        hi! Normal guibg=none guifg=#ffffff
-        hi! NormalFloat guibg=none guifg=#ffffff
-        hi! Terminal guibg=none guifg=#ffffff
-      ]])
-    end
-  end,
-})
-
-vim.g.adwaita_darker = true
-vim.g.adwaita_transparent = vim.g.transparency
 vim.cmd.colorscheme(vim.g.colorscheme)
 -- }}}
 
