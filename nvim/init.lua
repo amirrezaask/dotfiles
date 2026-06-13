bbff2f5b97470d4e95980e34fec6377074d6ca4e
AmirrezaAskarpour <raskarpour@gmail.com>
2026-06-13 23:40:11 +0330

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,7 +103,7 @@ require("gruvbox").setup {
  transparent_mode = vim.g.transparency,
 }
 
-vim.cmd.colorscheme(os.getenv("NVIM_THEME") or "gruvbox")
+vim.cmd.colorscheme(os.getenv("NVIM_THEME") or "vague")
 
 require("nvim-highlight-colors").setup {}
 
